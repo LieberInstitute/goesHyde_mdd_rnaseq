844448cabb77ba49d53fa77ee9aa3a6f77570c77
fsg112 <fgoes1@jhmi.edu>
2020-04-21 14:41:26 -0400

    dropping mismatching samples

---
+++ data/drop_flagged_samples.R
@@ -0,0 +1,46 @@
+library("sessioninfo")
+library("SummarizedExperiment")
+
+
+## load rse data 
+load("rse_gene_GoesZandi_n1093.rda", verbose=TRUE)
+load("rse_exon_GoesZandi_n1093.rda", verbose=TRUE)
+load("rse_jxn_GoesZandi_n1093.rda", verbose=TRUE)
+load("rse_tx_GoesZandi_n1093.rda", verbose=TRUE)
+
+## Find samples to drop 
+load("../genotype_data/flagged_samples_mddseq.Rdata", verbose = TRUE)
+
+## phenotype data
+
+pd <- colData(rse_gene)
+keep_samples <- !pd$RNum %in% checks$SAMPLE_ID
+table(keep_samples)
+dim(checks)
+length(unique(checks$SAMPLE_ID))
+
+#### subset samples
+dim(rse_gene)
+rse_gene <- rse_gene[, keep_samples]
+dim(rse_gene)
+
+rse_exon <- rse_exon[, keep_samples]
+rse_jxn <- rse_jxn[, keep_samples]
+rse_tx <- rse_tx[, keep_samples]
+
+## save data 
+dir.create("without_flagged", showWarnings = FALSE)
+save(rse_gene, file = file.path("without_flagged", "rse_gene_bp_mdd.Rdata"))
+save(rse_exon, file = file.path("without_flagged", "rse_exon_bp_mdd.Rdata"))
+save(rse_jxn, file = file.path("without_flagged", "rse_jxn_bp_mdd.Rdata"))
+save(rse_tx, file = file.path("without_flagged", "rse_tx_bp_mdd.Rdata"))
+
+
+
+
+## Reproducibility information
+print('Reproducibility information:')
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
