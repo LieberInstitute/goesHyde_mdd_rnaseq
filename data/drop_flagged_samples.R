5468859c0a99b56335d59386f8aa1c223cbd3a74
lahuuki <lahuuki@gmail.com>
2020-04-23 14:33:07 -0400

    style drop flagged samples script

--- data/drop_flagged_samples.R
+++ data/drop_flagged_samples.R
@@ -1,14 +1,16 @@
 library("sessioninfo")
 library("SummarizedExperiment")
 
+## style this script
+#styler::style_file("drop_flagged_samples.R", transformers = styler::tidyverse_style(indent_by = 4))
 
-## load rse data 
-load("rse_gene_GoesZandi_n1093.rda", verbose=TRUE)
-load("rse_exon_GoesZandi_n1093.rda", verbose=TRUE)
-load("rse_jxn_GoesZandi_n1093.rda", verbose=TRUE)
-load("rse_tx_GoesZandi_n1093.rda", verbose=TRUE)
+## load rse data
+load("rse_gene_GoesZandi_n1093.rda", verbose = TRUE)
+load("rse_exon_GoesZandi_n1093.rda", verbose = TRUE)
+load("rse_jxn_GoesZandi_n1093.rda", verbose = TRUE)
+load("rse_tx_GoesZandi_n1093.rda", verbose = TRUE)
 
-## Find samples to drop 
+## Find samples to drop
 load("../genotype_data/flagged_samples_mddseq.Rdata", verbose = TRUE)
 
 ## phenotype data
@@ -28,7 +30,7 @@ rse_exon <- rse_exon[, keep_samples]
 rse_jxn <- rse_jxn[, keep_samples]
 rse_tx <- rse_tx[, keep_samples]
 
-## save data 
+## save data
 dir.create("without_flagged", showWarnings = FALSE)
 save(rse_gene, file = file.path("without_flagged", "rse_gene_bp_mdd.Rdata"))
 save(rse_exon, file = file.path("without_flagged", "rse_exon_bp_mdd.Rdata"))
@@ -36,11 +38,11 @@ save(rse_jxn, file = file.path("without_flagged", "rse_jxn_bp_mdd.Rdata"))
 save(rse_tx, file = file.path("without_flagged", "rse_tx_bp_mdd.Rdata"))
 
 
-#sgejobs::job_single('drop_flagged_samples', create_shell = TRUE, memory = '40G', command = "Rscript drop_flagged_samples.R")
+# sgejobs::job_single('drop_flagged_samples', create_shell = TRUE, memory = '40G', command = "Rscript drop_flagged_samples.R")
 
 
 ## Reproducibility information
-print('Reproducibility information:')
+print("Reproducibility information:")
 Sys.time()
 proc.time()
 options(width = 120)
