0bb8aba69f95938bb46d11d3ddcbdb52af1f06c1
fsg112 <fgoes1@jhmi.edu>
2020-04-28 13:49:18 -0400

    added ERCC processing code

--- data/drop_flagged_samples.R
+++ data/drop_flagged_samples.R
@@ -21,6 +21,60 @@ table(keep_samples)
 dim(checks)
 length(unique(checks$SAMPLE_ID))
 
+### process ERCC https://github.com/LieberInstitute/RNAseq-pipeline/blob/master/sh/create_count_objects-human.R#L306-L336
+
+
+##observed kallisto tpm
+erccTPM = sapply(sampIDs, function(x) {
+    read.table(file.path(opt$maindir, "Ercc", x, "abundance.tsv"),
+               header = TRUE)$tpm
+})
+rownames(erccTPM) = read.table(file.path(opt$maindir, "Ercc", sampIDs[1], "abundance.tsv"),
+                               header = TRUE)$target_id
+#check finiteness / change NaNs to 0s
+erccTPM[which(is.na(erccTPM), arr.ind = T)] = 0
+
+#expected concentration
+spikeIns = read.delim(
+    "/users/ajaffe/Lieber/Projects/RNAseq/Ribozero_Compare/ercc_actual_conc.txt",
+    as.is = TRUE,
+    row.names = 2
+)
+##match row order
+spikeIns = spikeIns[match(rownames(erccTPM), rownames(spikeIns)), ]
+
+pdf(
+    file.path(opt$maindir, 'Ercc', 'ercc_spikein_check_mix1.pdf'),
+    h = 12,
+    w = 18
+)
+mypar(4, 6)
+for (i in 1:ncol(erccTPM)) {
+    plot(
+        log2(10 * spikeIns[, "concentration.in.Mix.1..attomoles.ul."] + 1) ~ log2(erccTPM[, i] +
+                                                                                      1),
+        xlab = "Kallisto log2(TPM+1)",
+        ylab = "Mix 1: log2(10*Concentration+1)",
+        main = colnames(erccTPM)[i],
+        xlim = c(min(log2(erccTPM + 1)), max(log2(erccTPM + 1)))
+    )
+    abline(0, 1, lty = 2)
+}
+dev.off()
+
+mix1conc = matrix(
+    rep(spikeIns[, "concentration.in.Mix.1..attomoles.ul."]),
+    nc = ncol(erccTPM),
+    nr = nrow(erccTPM),
+    byrow = FALSE
+)
+logErr = (log2(erccTPM + 1) - log2(10 * mix1conc + 1))
+metrics$ERCCsumLogErr = colSums(logErr)
+
+
+
+
+
 #### subset samples
 dim(rse_gene)
 rse_gene <- rse_gene[, keep_samples]
