09c3206dd92895e47da877fef8a45c6214b73bfc
lahuuki <lahuuki@gmail.com>
2020-05-28 13:36:49 -0400

    Fix path to save PC data

--- eqtl/genomewide/genomewide_run_eqtls_amyg.R
+++ eqtl/genomewide/genomewide_run_eqtls_amyg.R
@@ -116,7 +116,7 @@ kTx <- num.sv(log2(txTpm + 1), mod, vfilter = 50000)
 txPCs <- pcaTx$x[, 1:kTx]
 
 # is this how we want to save this?
-save(genePCs, exonPCs, jxnPCs, txPCs, file = here("rdas", "pcs_4features_amyg.rda"))
+save(genePCs, exonPCs, jxnPCs, txPCs, file = here("eqtl", "genomewide", "rdas", "pcs_4features_amyg.rda"))
 
 covsGene <- SlicedData$new(t(cbind(mod[, -1], genePCs)))
 covsExon <- SlicedData$new(t(cbind(mod[, -1], exonPCs)))
