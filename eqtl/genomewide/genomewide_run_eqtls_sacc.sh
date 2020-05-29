1bfb0eb97f7332801f36f97f5fa2f11d94a75694
lahuuki <lahuuki@gmail.com>
2020-05-29 09:35:16 -0400

    Update sh file

--- eqtl/genomewide/genomewide_run_eqtls_sacc.sh
+++ eqtl/genomewide/genomewide_run_eqtls_sacc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=100G,h_vmem=100G,h_fsize=100G
+#$ -l mem_free=150G,h_vmem=150G,h_fsize=100G
 #$ -N genomewide_run_eqtls_sacc
 #$ -o logs/genomewide_run_eqtls_sacc.txt
 #$ -e logs/genomewide_run_eqtls_sacc.txt
