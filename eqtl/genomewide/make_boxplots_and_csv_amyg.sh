1e95d78d8302d89106dae1adf7b9a4409fb2cfee
lahuuki <lahuuki@gmail.com>
2020-06-05 11:10:24 -0400

    Add sh file

---
+++ eqtl/genomewide/make_boxplots_and_csv_amyg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=150G,h_vmem=150G,h_fsize=100G
+#$ -N make_boxplots_and_csv_amyg
+#$ -o logs/make_boxplots_and_csv_amyg.txt
+#$ -e logs/make_boxplots_and_csv_amyg.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript make_boxplots_and_csv_amyg.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
