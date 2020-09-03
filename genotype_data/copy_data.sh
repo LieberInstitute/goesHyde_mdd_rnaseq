00178ee649eb2103da08f01c18d0da3c1d3de9c9
lahuuki <lahuuki@gmail.com>
2020-09-03 15:39:08 -0400

    Add sh files to copy data and make bed files

---
+++ genotype_data/copy_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/merged_batches_topmed/MDD_samples/* .
---
+++ genotype_data/make_bed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+module load plink
+plink2 --vcf topmed_mdd_602sample_090120_maf005.vcf.gz --make-bed --out topmed_mdd_602sample_090120_maf005
