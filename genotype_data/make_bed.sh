d505c96612346b34c6e6c924670d4aed773f4a45
lahuuki <lahuuki@gmail.com>
2020-09-08 13:52:16 -0400

    Use plink not plink2

--- genotype_data/make_bed.sh
+++ genotype_data/make_bed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 module load plink
-plink2 --vcf topmed_mdd_602sample_090120_maf005.vcf.gz --make-bed --out topmed_mdd_602sample_090120_maf005
+plink --vcf topmed_mdd_602sample_090120_maf005.vcf.gz --make-bed --out topmed_mdd_602sample_090120_maf005
