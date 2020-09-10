f73c1b3a4d954d617fda6864d324417de5a80d98
lahuuki <lahuuki@gmail.com>
2020-09-10 16:35:24 -0400

    Copy unimputed data

--- genotype_data/copy_data.sh
+++ genotype_data/copy_data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/merged_batches_topmed/MDD_samples/* .
+# cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/merged_batches_topmed/MDD_samples/* .
+cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/unimputed_genotypes/mdd_unimputed_genotypes/* . 
