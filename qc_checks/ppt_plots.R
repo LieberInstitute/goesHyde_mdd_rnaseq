1ac0267d95e2a6c856e3c41c2b714cf9a3b438a9
lahuuki <lahuuki@gmail.com>
2020-09-10 11:24:45 -0400

    Copy ERCC script from drop_flagged_samples.R

--- qc_checks/ppt_plots.R
+++ qc_checks/ppt_plots.R
@@ -497,6 +497,77 @@ legend("topleft", paste0("Plate ", levels(factor(pd$Plate))),
 )
 dev.off()
 
+### process ERCC for MDD  https://github.com/LieberInstitute/RNAseq-pipeline/blob/master/sh/create_count_objects-human.R#L306-L336
+
+##observed kallisto tpm
+sampIDs <- pd$RNum[pd$Experiment=="psychENCODE_MDD"]
+opt <- list("maindir"="/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/preprocessed_data")
+erccTPM = sapply(sampIDs, function(x) {
+    read.table(file.path(opt$maindir, "Ercc", x, "abundance.tsv"),
+               header = TRUE)$tpm
+})
+rownames(erccTPM) = read.table(file.path(opt$maindir, "Ercc", sampIDs[1], "abundance.tsv"),
+                               header = TRUE)$target_id
+#check finiteness / change NaNs to 0s
+erccTPM[which(is.na(erccTPM), arr.ind = T)] = 0
+
+#expected concentration
+spikeIns = read.delim(
+    "/users/ajaffe/Lieber/Projects/RNAseq/Ribozero_Compare/ercc_actual_conc.txt",
+    as.is = TRUE,
+    row.names = 2
+)
+##match row order
+spikeIns = spikeIns[match(rownames(erccTPM), rownames(spikeIns)), ]
+
+pdf(
+    file.path('ercc_spikein_check_mix1_drop_flagged_samples.pdf'),
+    h = 12,
+    w = 18
+)
+rafalib::mypar(4, 6)
+for (i in 1:ncol(erccTPM)) {
+    plot(
+        log2(10 * spikeIns[, "concentration.in.Mix.1..attomoles.ul."] + 1) ~ log2(erccTPM[, i] +
+                                                                                      1),
+        xlab = "Kallisto log2(TPM+1)",
+        ylab = "Mix 1: log2(10*Concentration+1)",
+        main = colnames(erccTPM)[i],
+        xlim = c(min(log2(erccTPM + 1)), max(log2(erccTPM + 1)))
+    )
+    abline(0, 1, lty = 2)
+}
+dev.off()
+
+mix1conc = matrix(
+    rep(spikeIns[, "concentration.in.Mix.1..attomoles.ul."]),
+    nc = ncol(erccTPM),
+    nr = nrow(erccTPM),
+    byrow = FALSE
+)
+logErr = (log2(erccTPM + 1) - log2(10 * mix1conc + 1))
+pd$ERCCsumLogErr <- NA
+pd$ERCCsumLogErr[pd$Experiment == "GoesMDD"] = colSums(logErr)
+
+## extract ERCC for BP project samples
+metrics <- read.csv("/dcl01/lieber/ajaffe/lab/zandiHyde_bipolar_rnaseq/preprocessed_data/read_and_alignment_metrics_zandiHyde_Bipolar_LIBD.csv")
+sampIDs_BP <- pd$RNum[pd$Experiment=="ZandiBPD"]
+m_bp <- match(sampIDs_BP, metrics$RNum)
+table(is.na(m_bp))
+pd$ERCCsumLogErr[pd$Experiment == "ZandiBPD"] <- metrics$ERCCsumLogErr[m_bp]
+
+##explore ERCCsumLogErr  (expect no NA)
+summary(pd$ERCCsumLogErr)
+tapply(pd$ERCCsumLogErr, pd$Experiment, summary)
+
+# $GoesMDD
+#     Min.  1st Qu.   Median     Mean  3rd Qu.     Max.
+# -64.9605 -18.6245 -14.9598 -15.8102 -11.8254  -0.4339
+#
+# $ZandiBPD
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# -36.992  -4.586   3.903  10.526  29.208  47.363
+
 # sgejobs::job_single('ppt_plots', create_shell = TRUE, queue= 'bluejay', memory = '50G', command = "Rscript ppt_plots.R")
 
 ## Reproducibility information
