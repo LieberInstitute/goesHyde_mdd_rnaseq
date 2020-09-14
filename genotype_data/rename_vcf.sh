dbad1bc217537f2f13f452b30580133741998f3e
lahuuki <lahuuki@gmail.com>
2020-09-14 09:34:00 -0400

    rename vcf files

---
+++ genotype_data/rename_vcf.sh
@@ -0,0 +1,8 @@
+mv 1M_unimputed_mdd.vcf.gz Illumina_1M_mdd.vcf.gz
+mv Illumina_HumanHap650Yv3_A_unimputed_mdd.vcf.gz Illumina_HumanHap650Yv3_A_mdd.vcf.gz
+mv Illumina_HumanOmni2-5-8-v1-1_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.1_mdd.vcf.gz
+mv Illumina_HumanOmni5-Quad_unimputed_mdd.vcf.gz Illumina_Omni5-Quad_mdd.vcf.gz
+mv Macrogen_11062018_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.3._mdd.vcf.gz
+mv Tao_omni2.5-8_v1.5_56_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.5_56_mdd.vcf.gz
+mv Tao_Omni2.5v1.3_8_unimputed_mdd.vcf.gz Illumina_Omni2.5_v1.3_8_mdd.vcf.gz
+mv Tao_omni2.5v1.4_136_unimputed_mdd.vcf.gz Illumina_Omni2.5_v1.4_136_mdd.vcf.gz
