ebdbb6c22608489a39c9dd349de19e511e038ac7
lahuuki <lahuuki@gmail.com>
2020-10-02 16:11:53 -0400

    Update to work with overlap samples #6

--- qc_checks/qc_checks.R
+++ qc_checks/qc_checks.R
@@ -17,14 +17,17 @@ library(here)
 
 ## load phenotype and alignment data
 load(here("data","rse_gene_raw_GoesZandi.rda"),
-    verbose = TRUE
+     verbose = TRUE
 )
 pd <- colData(rse_gene) %>% as.data.frame()
+table(pd$overlap)
+# FALSE  TRUE 
+# 1123    20 
 
 metricCols <-c("RNum", "Experiment", "numReads", "Plate", "ERCCsumLogErr")
 pd_mdd <- read.csv(here("preprocessed_data","read_and_alignment_metrics_goesHyde_MDD.csv"),
-    stringsAsFactors = FALSE,
-    row.names = 1) %>%
+                   stringsAsFactors = FALSE,
+                   row.names = 1) %>%
     mutate(Experiment = "psychENCODE_MDD",
            ERCCsumLogErr = NA) %>%
     rename(RNum = SAMPLE_ID) %>%
@@ -41,7 +44,7 @@ pd_bp <- read.csv(
 pd_both <- rbind(pd_mdd, pd_bp)
 
 pd <- left_join(pd, pd_both, by = c("RNum", "Experiment"))
-rownames(pd) <- pd$RNum
+rownames(pd) <- paste0(pd$RNum,"_",pd$Experiment)
 
 #### ERCC data ####
 ### https://github.com/LieberInstitute/RNAseq-pipeline/blob/master/sh/create_count_objects-human.R#L306-L336
@@ -207,7 +210,7 @@ pd$dropMetrics[pd$overallMapRate < 0.5 |
 
 table(pd$dropMetrics)
 # FALSE  TRUE
-# 1133     7
+# 1133     10
 
 ####################################
 
@@ -397,7 +400,7 @@ Dmat <- t(Xmat) %*% Xmat
 guess <-
     apply(yExprs1000, 2, function(x)
         solve(Dmat, t(Xmat) %*% x))[2,]
-stopifnot(identical(names(guess), paste0(rownames(pd), "_", pd$Experiment)))
+stopifnot(identical(names(guess), rownames(pd)))
 pd$guess <- guess
 
 ### plot
@@ -443,37 +446,35 @@ pd[which(pd$BrainRegion == "sACC" & pd$guess < .3), info_cols]
 pd[which(pd$BrainRegion == "Amygdala" & pd$guess > 0.9), info_cols]
 
 
-# > pd[which(pd$BrainRegion=="sACC" & pd$guess<.3),info_cols]
-# BrNum      Experiment AgeDeath Sex Race PrimaryDx BrainRegion dropMetrics        guess
-# R14179 Br1469 psychENCODE_MDD 28.57000   M CAUC   Control        sACC       FALSE -0.021303859
-# R17520 Br1635 psychENCODE_MDD 52.31000   M CAUC       MDD        sACC       FALSE  0.231191685
-# R17527 Br1675 psychENCODE_MDD 32.10000   M CAUC       MDD        sACC       FALSE -0.056853078
-# R17547 Br1754 psychENCODE_MDD 25.98000   F CAUC       MDD        sACC       FALSE  0.018389137
-# R17579 Br6099 psychENCODE_MDD 26.66381   M CAUC       MDD        sACC       FALSE  0.153008741
-# R17894 Br8017 psychENCODE_MDD 49.29500   M CAUC       MDD        sACC       FALSE  0.261345113
-# R17941 Br8133 psychENCODE_MDD 62.36813   M CAUC       MDD        sACC       FALSE -0.104237322
-# R17951 Br2333 psychENCODE_MDD 62.99000   F CAUC   Control        sACC       FALSE -0.063507534
-# R17952 Br5549 psychENCODE_MDD 67.47000   M CAUC       MDD        sACC       FALSE  0.008696151
-# R18430 Br3863 psychENCODE_MDD 69.33904   M CAUC   Control        sACC       FALSE -0.122722764
-# R18458 Br8313 psychENCODE_MDD 55.41672   M CAUC   Control        sACC       FALSE  0.134877418
-# R19186 Br1475 psychENCODE_MDD 43.30000   M CAUC       MDD        sACC       FALSE  0.066271916
-# R14110 Br1562  psychENCODE_BP 36.15000   M CAUC   Bipolar        sACC       FALSE  0.281349529
-# R14175 Br1752  psychENCODE_BP 39.20000   F CAUC   Bipolar        sACC       FALSE  0.061872883
-# R14182 Br1558  psychENCODE_BP 23.43000   F CAUC   Control        sACC       FALSE  0.266600470
-# R14228 Br2333  psychENCODE_BP 62.99000   F CAUC   Control        sACC       FALSE  0.035792461
-# R14235 Br1661  psychENCODE_BP 51.15000   M CAUC   Bipolar        sACC       FALSE  0.274285966
-# R14269 Br5555  psychENCODE_BP 21.34000   M CAUC   Control        sACC       FALSE  0.134646892
-# R14295 Br2071  psychENCODE_BP 40.62000   F CAUC   Bipolar        sACC       FALSE  0.128219700
-# > pd[which(pd$BrainRegion=="Amygdala" & pd$guess>0.9),info_cols]
+# BrNum      Experiment AgeDeath Sex Race PrimaryDx BrainRegion dropMetrics       guess
+# R14179_psychENCODE_MDD Br1469 psychENCODE_MDD 28.57000   M CAUC   Control        sACC       FALSE -0.01452763
+# R17520_psychENCODE_MDD Br1635 psychENCODE_MDD 52.31000   M CAUC       MDD        sACC       FALSE  0.20640366
+# R17527_psychENCODE_MDD Br1675 psychENCODE_MDD 32.10000   M CAUC       MDD        sACC       FALSE -0.03900185
+# R17547_psychENCODE_MDD Br1754 psychENCODE_MDD 25.98000   F CAUC       MDD        sACC       FALSE  0.02626940
+# R17579_psychENCODE_MDD Br6099 psychENCODE_MDD 26.66381   M CAUC       MDD        sACC       FALSE  0.12403490
+# R17894_psychENCODE_MDD Br8017 psychENCODE_MDD 49.29500   M CAUC       MDD        sACC       FALSE  0.25591506
+# R17941_psychENCODE_MDD Br8133 psychENCODE_MDD 62.36813   M CAUC       MDD        sACC       FALSE -0.09759443
+# R17952_psychENCODE_MDD Br5549 psychENCODE_MDD 67.47000   M CAUC       MDD        sACC       FALSE  0.01866621
+# R18430_psychENCODE_MDD Br3863 psychENCODE_MDD 69.33904   M CAUC   Control        sACC       FALSE -0.12428720
+# R18458_psychENCODE_MDD Br8313 psychENCODE_MDD 55.41672   M CAUC   Control        sACC       FALSE  0.14767064
+# R19186_psychENCODE_MDD Br1475 psychENCODE_MDD 43.30000   M CAUC       MDD        sACC       FALSE  0.04996062
+# R14110_psychENCODE_BP  Br1562  psychENCODE_BP 36.15000   M CAUC   Bipolar        sACC       FALSE  0.27638628
+# R14175_psychENCODE_BP  Br1752  psychENCODE_BP 39.20000   F CAUC   Bipolar        sACC       FALSE  0.06713448
+# R14179_psychENCODE_BP  Br1469  psychENCODE_BP 28.57000   M CAUC   Control        sACC       FALSE -0.03493316
+# R14182_psychENCODE_BP  Br1558  psychENCODE_BP 23.43000   F CAUC   Control        sACC       FALSE  0.26839632
+# R14228_psychENCODE_BP  Br2333  psychENCODE_BP 62.99000   F CAUC   Control        sACC       FALSE  0.03937358
+# R14235_psychENCODE_BP  Br1661  psychENCODE_BP 51.15000   M CAUC   Bipolar        sACC       FALSE  0.27786712
+# R14269_psychENCODE_BP  Br5555  psychENCODE_BP 21.34000   M CAUC   Control        sACC       FALSE  0.13650520
+# R14295_psychENCODE_BP  Br2071  psychENCODE_BP 40.62000   F CAUC   Bipolar        sACC       FALSE  0.14280649
+# > pd[which(pd$BrainRegion == "Amygdala" & pd$guess > 0.9), info_cols]
 # BrNum      Experiment AgeDeath Sex Race PrimaryDx BrainRegion dropMetrics    guess
-# R17496 Br1469 psychENCODE_MDD 28.57000   M CAUC   Control    Amygdala       FALSE 1.075980
-# R18938 Br6293 psychENCODE_MDD 27.85763   F CAUC       MDD    Amygdala       FALSE 0.903392
-# R14081 Br5611  psychENCODE_BP 63.82000   F CAUC   Bipolar    Amygdala       FALSE 1.010363
-# R15072 Br5939  psychENCODE_BP 55.90965   M CAUC   Bipolar    Amygdala       FALSE 1.058777
+# R17496_psychENCODE_MDD Br1469 psychENCODE_MDD 28.57000   M CAUC   Control    Amygdala       FALSE 1.073050
+# R14081_psychENCODE_BP  Br5611  psychENCODE_BP 63.82000   F CAUC   Bipolar    Amygdala       FALSE 1.014568
+# R15072_psychENCODE_BP  Br5939  psychENCODE_BP 55.90965   M CAUC   Bipolar    Amygdala       FALSE 1.055168
 
 ## Br1469 labels got switched
-pd["R14179", "BrainRegion"] <- "Amygdala"
-pd["R17496", "BrainRegion"] <- "sACC"
+pd[which(pd$RNum == "R14179"), "BrainRegion"] <- "Amygdala"
+pd[which(pd$RNum == "R17496"), "BrainRegion"] <- "sACC"
 
 ## Drop the rest
 dropInd <- which((pd$BrainRegion == "sACC" & pd$guess < .3) |
@@ -484,6 +485,9 @@ pd$dropRegion[dropInd] <- TRUE
 table(pd$dropRegion)
 # FALSE  TRUE
 # 1119    21
+## With overlap controls included
+# FALSE  TRUE 
+# 1126    19
 
 ######################################################## .
 gia <- read.csv(here("synapse","genotypeInferredAncestry.csv"))
@@ -497,93 +501,90 @@ table(pd$Race, pd$geneticRace)
 # AA ambiguous   AS CAUC HISP
 # AA      5         0    0    0    0
 # AS      0         0    2    0    0
-# CAUC    0         2    0 1127    0
+# CAUC    0         2    0 1130    0
 # HISP    0         0    0    1    3
 
 table(pd$dropRace)
-# FALSE  TRUE
-# 1128    12
+# FALSE  TRUE 
+# 1131    12 
 
 
 ######################################################## .
-info_cols <-
-    c(  "RNum",
-        "BrNum",
-        "Experiment",
-        "AgeDeath",
+temp_info_cols <-
+    c(  "BrNum",
+        # "AgeDeath",
         "Sex",
         "Race",
         "PrimaryDx",
         "BrainRegion",
         "RIN",
-        "Plate",
+        # "Plate",
         "numReads",
-        "ERCCsumLogErr",
+        # "ERCCsumLogErr",
         "geneticRace",
         "dropMetrics",
         "dropRegion",
         "dropRace"
     )
 pd[which(pd$dropMetrics == TRUE |
-             pd$dropRegion == TRUE | pd$dropRace == TRUE), info_cols]
+             pd$dropRegion == TRUE | pd$dropRace == TRUE), temp_info_cols]
 
-#         BrNum AgeDeath Sex Race PrimaryDx BrainRegion RIN Plate dropMetrics dropRegion dropRace
-# R17520 Br1635 52.31000   M CAUC       MDD        sACC 6.6     6       FALSE       TRUE    FALSE
-# R17527 Br1675 32.10000   M CAUC       MDD        sACC 6.8     2       FALSE       TRUE    FALSE
-# R17538 Br1723 55.70000   F CAUC       MDD    Amygdala 5.3     1        TRUE      FALSE    FALSE
-# R17547 Br1754 25.98000   F CAUC       MDD        sACC 5.7     5       FALSE       TRUE    FALSE
-# R17579 Br6099 26.66381   M CAUC       MDD        sACC 6.9     7       FALSE       TRUE    FALSE
-# R17616 Br1986 25.35000   M CAUC       MDD    Amygdala 6.7     5       FALSE      FALSE     TRUE
-# R17794 Br5763 53.89000   M CAUC       MDD        sACC 7.1     1        TRUE      FALSE    FALSE
-# R17839 Br5965 30.12183   M CAUC       MDD    Amygdala 5.6     4        TRUE      FALSE    FALSE
-# R17880 Br6227 18.62822   M HISP   Control        sACC 7.4     5       FALSE      FALSE     TRUE
-# R17894 Br8017 49.29500   M CAUC       MDD        sACC 7.2     3       FALSE       TRUE    FALSE
-# R17914 Br8053 47.00616   M CAUC   Control    Amygdala 5.5     1        TRUE      FALSE    FALSE
-# R17941 Br8133 62.36813   M CAUC       MDD        sACC 5.7     1       FALSE       TRUE    FALSE
-# R17951 Br2333 62.99000   F CAUC   Control        sACC 5.0     4       FALSE       TRUE    FALSE
-# R17952 Br5549 67.47000   M CAUC       MDD        sACC 5.6     5       FALSE       TRUE    FALSE
-# R17963 Br5526 68.85000   M CAUC       MDD    Amygdala 5.4     4        TRUE      FALSE    FALSE
-# R18425 Br6285 64.82000   M   AS   Control    Amygdala 6.8     3       FALSE      FALSE     TRUE
-# R18426 Br6285 64.82000   M   AS   Control        sACC 7.2     3       FALSE      FALSE     TRUE
-# R18430 Br3863 69.33904   M CAUC   Control        sACC 6.5     7       FALSE       TRUE    FALSE
-# R18431 Br3872 59.24162   F HISP   Control    Amygdala 5.8     4       FALSE      FALSE     TRUE
-# R18432 Br3872 59.24162   F HISP   Control        sACC 5.9     4       FALSE      FALSE     TRUE
-# R18458 Br8313 55.41672   M CAUC   Control        sACC 5.5     4       FALSE       TRUE    FALSE
-# R18459 Br6123 30.76249   F   AA   Control    Amygdala 6.6     3       FALSE      FALSE     TRUE
-# R18460 Br6123 30.76249   F   AA   Control        sACC 7.1     4       FALSE      FALSE     TRUE
-# R18461 Br5146 47.01000   F   AA   Control    Amygdala 6.8     4       FALSE      FALSE     TRUE
-# R18462 Br5146 47.01000   F   AA   Control        sACC 7.6     4       FALSE      FALSE     TRUE
-# R18824 Br3877 51.85775   M CAUC       MDD    Amygdala 6.4     6       FALSE      FALSE     TRUE
-# R18938 Br6293 27.85763   F CAUC       MDD    Amygdala 7.6     5       FALSE       TRUE    FALSE
-# R19186 Br1475 43.30000   M CAUC       MDD        sACC 8.9     4       FALSE       TRUE    FALSE
-# R13935 Br0922 51.35000   M CAUC   Control    Amygdala 6.2    NA        TRUE      FALSE    FALSE
-# R14081 Br5611 63.82000   F CAUC   Bipolar    Amygdala 7.0    NA       FALSE       TRUE    FALSE
-# R14110 Br1562 36.15000   M CAUC   Bipolar        sACC 7.1    NA       FALSE       TRUE    FALSE
-# R14175 Br1752 39.20000   F CAUC   Bipolar        sACC 7.5    NA       FALSE       TRUE    FALSE
-# R14182 Br1558 23.43000   F CAUC   Control        sACC 6.9    NA       FALSE       TRUE    FALSE
-# R14210 Br1148 29.97000   M   AA   Control        sACC 7.9    NA       FALSE      FALSE     TRUE
-# R14228 Br2333 62.99000   F CAUC   Control        sACC 6.2    NA       FALSE       TRUE    FALSE
-# R14235 Br1661 51.15000   M CAUC   Bipolar        sACC 6.2    NA       FALSE       TRUE    FALSE
-# R14269 Br5555 21.34000   M CAUC   Control        sACC 6.5    NA       FALSE       TRUE    FALSE
-# R14295 Br2071 40.62000   F CAUC   Bipolar        sACC 6.5    NA       FALSE       TRUE    FALSE
-# R14306 Br5435 56.51000   M CAUC   Bipolar        sACC 7.9    NA        TRUE      FALSE    FALSE
-# R15072 Br5939 55.90965   M CAUC   Bipolar    Amygdala 7.5    NA       FALSE       TRUE    FALSE
+# BrNum Sex Race PrimaryDx BrainRegion RIN  numReads geneticRace dropMetrics dropRegion dropRace
+# R17514_psychENCODE_MDD Br1582   F CAUC       MDD    Amygdala 6.7  15252860        CAUC        TRUE      FALSE    FALSE
+# R17520_psychENCODE_MDD Br1635   M CAUC       MDD        sACC 6.6  72006392        CAUC       FALSE       TRUE    FALSE
+# R17527_psychENCODE_MDD Br1675   M CAUC       MDD        sACC 6.8  95710478        CAUC       FALSE       TRUE    FALSE
+# R17538_psychENCODE_MDD Br1723   F CAUC       MDD    Amygdala 5.3  49607375        CAUC        TRUE      FALSE    FALSE
+# R17547_psychENCODE_MDD Br1754   F CAUC       MDD        sACC 5.7 164708040        CAUC       FALSE       TRUE    FALSE
+# R17579_psychENCODE_MDD Br6099   M CAUC       MDD        sACC 6.9  84212528        CAUC       FALSE       TRUE    FALSE
+# R17616_psychENCODE_MDD Br1986   M CAUC       MDD    Amygdala 6.7 325417972   ambiguous       FALSE      FALSE     TRUE
+# R17794_psychENCODE_MDD Br5763   M CAUC       MDD        sACC 7.1 132887932        CAUC        TRUE      FALSE    FALSE
+# R17839_psychENCODE_MDD Br5965   M CAUC       MDD    Amygdala 5.6 111654297        CAUC        TRUE      FALSE    FALSE
+# R17880_psychENCODE_MDD Br6227   M HISP   Control        sACC 7.4 110688364        HISP       FALSE      FALSE     TRUE
+# R17894_psychENCODE_MDD Br8017   M CAUC       MDD        sACC 7.2 159142448        CAUC       FALSE       TRUE    FALSE
+# R17914_psychENCODE_MDD Br8053   M CAUC   Control    Amygdala 5.5 216752444        CAUC        TRUE      FALSE    FALSE
+# R17941_psychENCODE_MDD Br8133   M CAUC       MDD        sACC 5.7  88944792        CAUC       FALSE       TRUE    FALSE
+# R17950_psychENCODE_MDD Br1698   M CAUC       MDD        sACC 6.6  10284026        CAUC        TRUE      FALSE    FALSE
+# R17952_psychENCODE_MDD Br5549   M CAUC       MDD        sACC 5.6 125112170        CAUC       FALSE       TRUE    FALSE
+# R17963_psychENCODE_MDD Br5526   M CAUC       MDD    Amygdala 5.4 192819932        CAUC        TRUE      FALSE    FALSE
+# R18425_psychENCODE_MDD Br6285   M   AS   Control    Amygdala 6.8  63210188          AS       FALSE      FALSE     TRUE
+# R18426_psychENCODE_MDD Br6285   M   AS   Control        sACC 7.2 191881132          AS       FALSE      FALSE     TRUE
+# R18430_psychENCODE_MDD Br3863   M CAUC   Control        sACC 6.5  94282744        CAUC       FALSE       TRUE    FALSE
+# R18431_psychENCODE_MDD Br3872   F HISP   Control    Amygdala 5.8 208135920        HISP       FALSE      FALSE     TRUE
+# R18432_psychENCODE_MDD Br3872   F HISP   Control        sACC 5.9 135427576        HISP       FALSE      FALSE     TRUE
+# R18458_psychENCODE_MDD Br8313   M CAUC   Control        sACC 5.5 226538640        CAUC       FALSE       TRUE    FALSE
+# R18459_psychENCODE_MDD Br6123   F   AA   Control    Amygdala 6.6 102404724          AA       FALSE      FALSE     TRUE
+# R18460_psychENCODE_MDD Br6123   F   AA   Control        sACC 7.1 223245052          AA       FALSE      FALSE     TRUE
+# R18461_psychENCODE_MDD Br5146   F   AA   Control    Amygdala 6.8  89905868          AA       FALSE      FALSE     TRUE
+# R18462_psychENCODE_MDD Br5146   F   AA   Control        sACC 7.6 179353610          AA       FALSE      FALSE     TRUE
+# R18824_psychENCODE_MDD Br3877   M CAUC       MDD    Amygdala 6.4 125328900   ambiguous       FALSE      FALSE     TRUE
+# R19186_psychENCODE_MDD Br1475   M CAUC       MDD        sACC 8.9 313300392        CAUC       FALSE       TRUE    FALSE
+# R13935_psychENCODE_BP  Br0922   M CAUC   Control    Amygdala 6.2 116357154        CAUC        TRUE      FALSE    FALSE
+# R14003_psychENCODE_BP  Br2333   F CAUC   Control    Amygdala 6.4 139167058        CAUC        TRUE      FALSE    FALSE
+# R14081_psychENCODE_BP  Br5611   F CAUC   Bipolar    Amygdala 7.0 133289152        CAUC       FALSE       TRUE    FALSE
+# R14110_psychENCODE_BP  Br1562   M CAUC   Bipolar        sACC 7.1 137413506        CAUC       FALSE       TRUE    FALSE
+# R14175_psychENCODE_BP  Br1752   F CAUC   Bipolar        sACC 7.5 139218132        CAUC       FALSE       TRUE    FALSE
+# R14182_psychENCODE_BP  Br1558   F CAUC   Control        sACC 6.9 137971714        CAUC       FALSE       TRUE    FALSE
+# R14210_psychENCODE_BP  Br1148   M   AA   Control        sACC 7.9 144816192          AA       FALSE      FALSE     TRUE
+# R14228_psychENCODE_BP  Br2333   F CAUC   Control        sACC 6.2 130240626        CAUC       FALSE       TRUE    FALSE
+# R14235_psychENCODE_BP  Br1661   M CAUC   Bipolar        sACC 6.2 123042494        CAUC       FALSE       TRUE    FALSE
+# R14269_psychENCODE_BP  Br5555   M CAUC   Control        sACC 6.5 119019314        CAUC       FALSE       TRUE    FALSE
+# R14295_psychENCODE_BP  Br2071   F CAUC   Bipolar        sACC 6.5 134903940        CAUC       FALSE       TRUE    FALSE
+# R14306_psychENCODE_BP  Br5435   M CAUC   Bipolar        sACC 7.9 186118990        CAUC        TRUE      FALSE    FALSE
+# R15072_psychENCODE_BP  Br5939   M CAUC   Bipolar    Amygdala 7.5 153561278        CAUC       FALSE       TRUE    FALSE
 
 #### Summarize ####
-
 pd$dropSum <-
     rowSums(pd[, c("dropMetrics", "dropRegion", "dropRace")])
-sum(pd$dropSum > 0) # 40
-rownames(pd) <- paste0(pd$RNum, "_", pd$Experiment)
+message("Number of samples to drop: ",sum(pd$dropSum > 0))
 
 ## Save qc_dropping results
-qcresults <- pd[, info_cols]
+qcresults <- pd
 
 ## Update pd
-pd <- pd[-which(pd$dropSum > 0),]
+pd <- pd[-which(pd$dropSum > 0 & !(pd$overlap & pd$Experiment == "psychENCODE_BP")),]
 
 message("Remaining Samples: n", nrow(pd))
-# 1100
+# 1102
 
 table(pd$BrainRegion, pd$PrimaryDx)
 table(pd$Sex, pd$PrimaryDx)
