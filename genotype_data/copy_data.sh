73a6429b7e98a5f5795e4290815edce201c01aa6
lahuuki <lahuuki@gmail.com>
2020-10-08 17:48:19 -0400

    Copy missing data, fix names

--- genotype_data/copy_data.sh
+++ genotype_data/copy_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/merged_batches_topmed/MDD_samples/* .
-cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/unimputed_genotypes/mdd_unimputed_genotypes/* . 
+#cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/unimputed_genotypes/mdd_unimputed_genotypes/* . 
+cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/merged_batches_topmed/SampleFiles/mdd_unimputed_genotypes/1M_unimputed_mdd.vcf.gz . 
+cp /dcl01/lieber/RNAseq/Datasets/BrainGenotyping_2018/merged_batches_topmed/SampleFiles/mdd_unimputed_genotypes/Illumina_HumanOmni2-5-8-v1-3_unimputed_mdd.vcf.gz . 
--- genotype_data/rename_vcf.sh
+++ genotype_data/rename_vcf.sh
@@ -1,8 +1,19 @@
+# 1M
 mv 1M_unimputed_mdd.vcf.gz Illumina_1M_mdd.vcf.gz
+
+# Human Hap 
 mv Illumina_HumanHap650Yv3_A_unimputed_mdd.vcf.gz Illumina_HumanHap650Yv3_A_mdd.vcf.gz
-mv Illumina_HumanOmni2-5-8-v1-1_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.1_mdd.vcf.gz
+
+#Omni5-quad
 mv Illumina_HumanOmni5-Quad_unimputed_mdd.vcf.gz Illumina_Omni5-Quad_mdd.vcf.gz
-mv Macrogen_11062018_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.3._mdd.vcf.gz
+
+# Omni 2.5. *
+mv Illumina_HumanOmni2-5-8-v1-1_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.1_mdd.vcf.gz
 mv Tao_omni2.5-8_v1.5_56_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.5_56_mdd.vcf.gz
-mv Tao_Omni2.5v1.3_8_unimputed_mdd.vcf.gz Illumina_Omni2.5_v1.3_8_mdd.vcf.gz
 mv Tao_omni2.5v1.4_136_unimputed_mdd.vcf.gz Illumina_Omni2.5_v1.4_136_mdd.vcf.gz
+
+# Omni2.5-8_v1.3, threee versions will need to be combined
+mv Macrogen_11062018_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.3_mdd1.vcf.gz
+mv Tao_Omni2.5v1.3_8_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.3_mdd2.vcf.gz
+mv Illumina_HumanOmni2-5-8-v1-3_unimputed_mdd.vcf.gz Illumina_Omni2.5-8_v1.3_mdd3.vcf.gz
+
