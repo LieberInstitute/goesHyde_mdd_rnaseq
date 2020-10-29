80abc665fec6f6fe90dabfe44b193596b53289e2
lahuuki <lahuuki@gmail.com>
2020-10-29 12:51:35 -0400

    Add theme colors, update data input #8

--- deconvolution/deconvo_plots.R
+++ deconvolution/deconvo_plots.R
@@ -1,43 +1,62 @@
+library(SummarizedExperiment)
 library(RColorBrewer)
 library(jaffelab)
 library(here)
 library(dplyr)
 library(reshape2)
+library(ggplot2)
 
-#### plot ####
-# load("prop_sacc.Rdata", verbose = TRUE)
-pd_sacc <- as.data.frame(colData(rse_gene_sACC))
+#### Load Data ####
+load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
+pd <- as.data.frame(colData(rse_gene))
+
+## Bind with qSV table
+load(here("differential_expression" ,"qSV_mat.Rdata"), verbose = TRUE)
+all(rownames(pd)==rownames(qSV_mat))
+pd <- cbind(pd, qSV_mat)
+
+## split by region
+pd_sacc <- pd[pd$BrainRegion == "sACC",]
+pd_amyg <- pd[pd$BrainRegion == "Amygdala",]
+
+source(here("main_colors.R"))
+
+## bind with prop data
+load("prop_sacc.Rdata", verbose = TRUE)
+load("prop_amyg.Rdata", verbose = TRUE)
+
 pd_sacc <- cbind(pd_sacc, est_prop_sacc$Est.prop.weighted)
+pd_amyg <- cbind(pd_amyg, est_prop_amyg$Est.prop.weighted)
 
-bp_dx_sacc <- pd_sacc %>% select(PrimaryDx, colnames(est_prop_sacc$Est.prop.weighted)) %>%
+cells_sacc <- colnames(est_prop_sacc$Est.prop.weighted)
+cells_amyg <- colnames(est_prop_amyg$Est.prop.weighted)
+
+#### cell type boxplot ####
+
+bp_dx_sacc <- pd_sacc %>% select(PrimaryDx, cells_sacc) %>%
   melt(id.vars = "PrimaryDx") %>%
   rename(`Cell Type` = variable, Prop = value) %>%
   ggplot(aes(`Cell Type`, Prop, fill = `PrimaryDx`)) +
   geom_boxplot() +
+  scale_fill_manual(values = mdd_Dx_colors)+
   labs(title = "Distribution of Cell Type Propotions",
        subtitle = "sACC samples - MuSiC defaults")
 
 ggsave(filename = "cellType_boxplot_sACC.png", plot = bp_dx_sacc, width = 10)
 
-# load("prop_sacc.Rdata", verbose = TRUE)
-pd_sacc <- as.data.frame(colData(rse_gene_sACC))
-pd_sacc <- cbind(pd_sacc, est_prop_sacc$Est.prop.weighted)
 
-bp_dx_amyg <- pd_sacc %>% select(PrimaryDx, names(cell_bias$M.S)) %>%
+bp_dx_amyg <- pd_amyg %>% select(all_of(c("PrimaryDx", cells_amyg))) %>%
   melt(id.vars = "PrimaryDx") %>%
   rename(`Cell Type` = variable, Prop = value) %>%
   ggplot(aes(`Cell Type`, Prop, fill = `PrimaryDx`)) +
   geom_boxplot() +
+  scale_fill_manual(values = mdd_Dx_colors) +
   labs(title = "Distribution of Cell Type Propotions",
-       subtitle = "sACC samples - MuSiC defaults")
+       subtitle = "Amygdala samples - MuSiC defaults")
 
-ggsave(filename = "sACC_CellType_boxplot.png", plot = bp_sacc_dx, width = 10)
+ggsave(filename = "cellType_boxplot_amyg.png", plot = bp_dx_amyg, width = 10)
 
 #### check against qSV 1####
-load(here("differential_expression" ,"qSV_mat.Rdata"), verbose = TRUE)
-all(colnames(rse_gene)==rownames(qSV_mat))
-
-pd_sacc <- cbind(pd_sacc, qSV_mat[rownames(pd_sacc),])
 
 scatter_qsv1 <- pd_sacc %>% select(PrimaryDx, colnames(est_prop_sacc_noT$Est.prop.weighted), PC1) %>%
   melt(id.vars = c("PrimaryDx","PC1")) %>% 
