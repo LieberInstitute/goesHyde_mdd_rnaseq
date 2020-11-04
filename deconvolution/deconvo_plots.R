3ab8e398d14c30208b6227d7cc360cc071fa6e77
lahuuki <lahuuki@gmail.com>
2020-11-04 10:50:38 -0500

    Add sge job + repo info, update age and qsv plotting #8

--- deconvolution/deconvo_plots.R
+++ deconvolution/deconvo_plots.R
@@ -9,6 +9,7 @@ library(tidyverse)
 
 source(here("main_colors.R"))
 source("big_little_boxplot.R")
+
 #### Load Data ####
 load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
 pd <- as.data.frame(colData(rse_gene))
@@ -70,25 +71,27 @@ prop_broad_amyg <- melt(est_prop_top40_broad_amyg$Est.prop.weighted) %>%
 
 
 ## Plot
-top40_scatter_sacc <- prop40_sacc %>% 
+top40_scatter_sacc <- prop_sacc %>% 
   ggplot(aes(prop, prop_top40, color = cell_type)) +
   geom_point()+ 
   # geom_smooth(method='lm')+
-  labs(title = "Prop top 40 vs. all genes, sACC")
+  labs(title = "Prop top-40 vs. all genes",
+       subtitle = "sACC")
 
-ggsave(plot = top40_scatter_sacc , filename = "top40_scatter_sacc.png")
-
-top40_scatter_amyg <- prop40_amyg %>% 
+top40_scatter_amyg <- prop_amyg %>% 
   ggplot(aes(prop, prop_top40, color = cell_type)) +
   geom_point()  + 
   # geom_smooth(method='lm')+
-  labs(title = "Prop top 40 vs. all genes, Amygdala")
+  labs(subtitle = "Amygdala")
 
-ggsave(plot = top40_scatter_amyg , filename = "top40_scatter_amyg.png")
+ggsave(plot = top40_scatter_sacc + top40_scatter_amyg , filename = "plots/top40_scatter.png", width = 14)
 
 #### cell type boxplot ####
 ## Add dx to long data
 prop_dx_sacc <- prop_sacc %>% 
+  left_join(pd_sacc %>% select(PrimaryDx) %>% rownames_to_column("sample")) 
+
+prop_dx_amyg <- prop_amyg %>% 
   left_join(pd_sacc %>% select(PrimaryDx) %>% rownames_to_column("sample")) 
 
 prop_broad_dx_sacc <- prop_broad_sacc %>% 
@@ -100,49 +103,63 @@ prop_broad_dx_amyg <-  prop_broad_amyg %>%
 ## specific cell types
 both_regions_bl_boxplot(prop_dx_sacc, prop_dx_amyg, yvar = "prop", 
                         title = "Cell Type RNA-prop - all common genes",
-                        filename = "cellType_boxplots.png")
+                        filename = "plots/cellType_boxplots.png")
 
 both_regions_bl_boxplot(prop_dx_sacc, prop_dx_amyg, yvar = "prop_top40", 
                         title = "Cell Type RNA-prop - all common genes",
-                        filename = "cellType_boxplots_top40.png")
+                        filename = "plots/cellType_boxplots_top40.png")
 
 ## broad cell types
 both_regions_bl_boxplot(prop_broad_dx_sacc, prop_broad_dx_amyg, yvar = "prop", 
-                        title = "Cell Type RNA-prop - top 40 genes", 
-                        filename = "cellType_boxplots_broad.png")
+                        title = "Cell Type RNA-prop -  all common genes - Broad cell types", 
+                        filename = "plots/cellType_boxplots_broad.png")
 
 both_regions_bl_boxplot(prop_broad_dx_sacc, prop_broad_dx_amyg, yvar = "prop_top40", 
-                        title = "Cell Type RNA-prop - top 40 genes", 
-                        filename = "cellType_boxplots_broad_top40.png")
+                        title = "Cell Type RNA-prop - top 40 genes - Broad cell types", 
+                        filename = "plots/cellType_boxplots_broad_top40.png")
 
+## Age scatter
+prop_age_broad_sacc <- prop_broad_sacc %>% 
+  left_join(pd_sacc %>% select(AgeDeath) %>% rownames_to_column("sample")) 
 
-
-age_scatter_sacc <- pd_sacc %>%
-  select(all_of(c("RNum", "AgeDeath", cells_sacc))) %>%
-  melt(id.vars= c("RNum","AgeDeath")) %>%
-  rename(cell_type = variable, prop = value)%>%
+age_scatter_broad_sacc <- prop_age_broad_sacc %>%
   ggplot(aes(AgeDeath, prop, color = cell_type))+
-  geom_point()
+  geom_point() +
+  facet_wrap(~cell_type, scales = "free_y", nrow = 6)
 
-ggsave(filename = "age_vs_celltype_sACC.png", plot = age_scatter_sacc)
+ggsave(filename = "plots/age_vs_celltype_broad_sACC.png", plot = age_scatter_broad_sacc)
 
 #### check against qSV ####
 qsv_names <- colnames(qSV_mat)
 
-prop_vs_qsv_sacc <- pd_sacc %>% select(all_of(c("RNum", cells_sacc))) %>%
-  melt(id.vars= c("RNum")) %>%
-  rename(cell_type = variable, prop = value) %>%
-  left_join(pd_sacc %>% select(all_of(c("RNum", qsv_names))) %>%
-              melt(id.vars= c("RNum")) %>%
-              rename(qSV_name = variable, qSV = value)) 
+qsv_long_sacc <- pd_sacc %>% 
+  rownames_to_column("sample") %>%
+  select(all_of(c("sample", qsv_names))) %>%
+  melt(id.vars= c("sample")) %>%
+  rename(qSV_name = variable, qSV = value) 
 
+qsv_long_amyg <- pd_amyg %>% 
+  rownames_to_column("sample") %>%
+  select(all_of(c("sample", qsv_names))) %>%
+  melt(id.vars= c("sample")) %>%
+  rename(qSV_name = variable, qSV = value) 
 
-scatter_qsv_sacc <- prop_vs_qsv_sacc %>%
+prop_qsv_sacc <- prop_sacc %>%
+  left_join(qsv_long_sacc, by = "sample")
+
+scatter_qsv_sacc <- prop_qsv_sacc %>%
   ggplot(aes(qSV, prop)) +
   geom_point(size = .5) +
   facet_grid(cell_type~qSV_name, scales = "free")+
   theme_bw(base_size = 10)
 
-ggsave(filename = "sACC_CellType_qSV.png", plot = scatter_qsv_sacc, width = 26, height = 10)
+ggsave(filename = "plots/sACC_CellType_qSV.png", plot = scatter_qsv_sacc, width = 26, height = 10)
 
 
+# sgejobs::job_single('deconvo_plots', create_shell = TRUE, queue= 'bluejay', memory = '10G', command = "Rscript deconvo_plots.R")
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
