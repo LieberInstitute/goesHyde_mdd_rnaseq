a35e77823a114835d84ef04c578590f3382f69c0
lahuuki <lahuuki@gmail.com>
2020-11-05 18:13:13 -0500

    add top40 data #8

--- deconvolution/deconvo_heatmap.R
+++ deconvolution/deconvo_heatmap.R
@@ -1,7 +1,10 @@
+
 library(SummarizedExperiment)
 library(SingleCellExperiment)
 library(jaffelab)
 library(here)
+library(pheatmap)
+library(scuttle)
 
 ## Load rse_gene data
 load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
@@ -27,3 +30,37 @@ sce.amy <- sce.amy[,sce.amy$cellType.split != "Ambig.lowNtrxts",]
 sce.amy$cellType.Broad <- ss(as.character(sce.amy$cellType.split), "\\.", 1)
 ## Match rownames
 rownames(sce.amy) <- rowData(sce.amy)$ID
+
+#### Top 40 data ####
+top40_amyg <- read.csv("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/tables/top40genesLists_Amyg-n2_cellType.split_SN-LEVEL-tests_May2020.csv")
+top40_sacc <- read.csv("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/tables/top40genesLists_sACC-n2_cellType_SN-LEVEL-tests_May2020.csv")
+
+## sacc
+top40all_sacc <- unique(unlist(top40_sacc[,grepl("1vAll", colnames(top40_sacc))]))
+length(top40all_sacc)
+# [1] 388
+top40all_ensm_sacc <-  rowData(rse_gene)$ensemblID[rowData(rse_gene)$Symbol %in% top40all_sacc]
+top40all_ensm_sacc <- top40all_ensm_sacc[top40all_ensm_sacc %in% rownames(sce.sacc)]
+length(top40all_ensm_sacc)
+# [1] 332
+
+# filter expression data
+rse_gene_sacc <- rse_gene_sacc[top40all_ensm_sacc,]
+sce.sacc <- sce.sacc[top40all_ensm_sacc,]
+
+##Amyg
+top40all_amyg <- unique(unlist(top40_amyg[,grepl("1vAll", colnames(top40_amyg))]))
+length(top40all_amyg)
+# [1] 441
+top40all_ensm_amyg <-  rowData(rse_gene)$ensemblID[rowData(rse_gene)$Symbol %in% top40all_amyg]
+top40all_ensm_amyg <- top40all_ensm_amyg[top40all_ensm_amyg %in% rownames(sce.amy)]
+length(top40all_ensm_amyg)
+# [1] 376  
+# filter expression data
+rse_gene_amyg <- rse_gene_amyg[top40all_ensm_amyg,]
+sce.amy <- sce.amy[top40all_ensm_amyg,]
+
+#### pseudobulk single cell data ####
+
+pb_sacc <- aggregateAcrossCells(sce.sacc, 
+                                id = colData(sce.sacc)[,c("donor","cellType")])
