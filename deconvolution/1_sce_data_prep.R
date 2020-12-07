7143e3e39d7bc4134c46ecfe80f1a0288736b5b1
lahuuki <lahuuki@gmail.com>
2020-12-07 10:54:29 -0500

    Add bp_length to rowData of saved sce Rdata

--- deconvolution/1_sce_data_prep.R
+++ deconvolution/1_sce_data_prep.R
@@ -1,6 +1,7 @@
 library(SingleCellExperiment)
 library(jaffelab)
 library(here)
+library(GenomicFeatures)
 
 #### Load and filter data ####
 ## Load rse_gene data
@@ -31,6 +32,29 @@ sce.sacc <- sce.sacc[common_genes, ]
 sce.sacc <- sce.sacc[!rowSums(assay(sce.sacc, "counts"))==0, ] 
 dim(sce.sacc)
 # [1] 17785  7004
+
+#### Add bp length for RPKM later #### 
+rd <- rowData(sce.sacc)
+## Import gnomic features
+txdb <- makeTxDbFromGFF("/dcl01/ajaffe/data/lab/singleCell/10x_pilot/premRNA/GRCh38-3.0.0_premrna/genes/genes.gtf")
+g <- genes(txdb)
+e <- exonsBy(txdb, by = "gene")
+
+if (!all(names(e) %in% names(g))) {
+  warning("Dropping exons with gene ids not present in the gene list")
+  e <- e[names(e) %in% names(g)]
+}
+e2 <- disjoin(e)
+g$bp_length <- sum(width(e2))
+summary(g$bp_length)
+
+g_sacc <- g[rownames(sce.sacc),]
+summary(g_sacc$bp_length)
+table(g_sacc$bp_length > 205012)
+
+rowRanges(sce.sacc) <- g_sacc
+all(rownames(rd) == rownames(sce.sacc))
+rowData(sce.sacc)$Symbol <- rd$Symbol
 
 ## Save filtered sce object
 save(sce.sacc, file = here("deconvolution","data","sce.sacc_filtered.Rdata"))
