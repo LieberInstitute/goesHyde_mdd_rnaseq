49f5f0444e25cfbb2969b3b15f49a08445e6b2a8
lahuuki <lahuuki@gmail.com>
2020-12-09 09:10:46 -0500

    Prep amyg data

--- deconvolution/1_sce_data_prep.R
+++ deconvolution/1_sce_data_prep.R
@@ -33,8 +33,34 @@ sce.sacc <- sce.sacc[!rowSums(assay(sce.sacc, "counts"))==0, ]
 dim(sce.sacc)
 # [1] 17785  7004
 
+## Amyg Data
+load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/regionSpecific_Amyg-n2_cleaned-combined_SCE_MNTFeb2020.rda", verbose = TRUE)
+sce.amy$uniqueID <- paste0(sce.amy$donor, "_", sce.amy$Barcode)
+colnames(sce.amy) <- sce.amy$uniqueID
+
+sce.amy <- sce.amy[,sce.amy$cellType != "Ambig.lowNtrxts",]
+sce.amy$cellType <- droplevels(sce.amy$cellType)
+## Add cellType.broad
+sce.amy$cellType.Broad <- ss(as.character(sce.amy$cellType), "\\.", 1)
+sce.amy$cellType.Broad <- factor(sce.amy$cellType.Broad)
+## Match rownames
+rownames(sce.amy) <- rowData(sce.amy)$ID
+table(rownames(sce.amy) %in% rownames(rse_gene))
+# FALSE  TRUE 
+# 15021 18517 
+
+## filter for common between sc and bulk data
+common_genes <- rownames(rse_gene)[rownames(rse_gene) %in% rownames(sce.amy)]
+sce.amy <- sce.amy[common_genes, ]
+
+# Remove 0 genes across all nuclei
+sce.amy <- sce.amy[!rowSums(assay(sce.amy, "counts"))==0, ] 
+dim(sce.amy)
+# [1] 17792  6582
+
 #### Add bp length for RPKM later #### 
-rd <- rowData(sce.sacc)
+rd.sacc <- rowData(sce.sacc)
+rd.amy <- rowData(sce.amy)
 ## Import gnomic features
 txdb <- makeTxDbFromGFF("/dcl01/ajaffe/data/lab/singleCell/10x_pilot/premRNA/GRCh38-3.0.0_premrna/genes/genes.gtf")
 g <- genes(txdb)
@@ -48,13 +74,26 @@ e2 <- disjoin(e)
 g$bp_length <- sum(width(e2))
 summary(g$bp_length)
 
+## Add data to sacc
 g_sacc <- g[rownames(sce.sacc),]
 summary(g_sacc$bp_length)
 table(g_sacc$bp_length > 205012)
 
 rowRanges(sce.sacc) <- g_sacc
-all(rownames(rd) == rownames(sce.sacc))
-rowData(sce.sacc)$Symbol <- rd$Symbol
+all(rownames(rd.sacc) == rownames(sce.sacc))
+rowData(sce.sacc)$Symbol <- rd.sacc$Symbol
+
+## Add data to amy data
+g_amy <- g[rownames(sce.amy),]
+summary(g_amy$bp_length)
+table(g_amy$bp_length > 205012)
+
+rowRanges(sce.amy) <- g_amy
+all(rownames(rd.amy) == rownames(sce.amy))
+rowData(sce.amy)$Symbol <- rd.amy$Symbol
 
 ## Save filtered sce object
 save(sce.sacc, file = here("deconvolution","data","sce.sacc_filtered.Rdata"))
+save(sce.amy, file = here("deconvolution","data","sce.amy_filtered.Rdata"))
+
+
