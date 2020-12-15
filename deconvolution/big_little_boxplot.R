bf37360b5f42459195776e52f88a5c65952dd9f1
lahuuki <lahuuki@gmail.com>
2020-12-15 16:40:07 -0500

    Add color var and ignore value

--- deconvolution/big_little_boxplot.R
+++ deconvolution/big_little_boxplot.R
@@ -2,12 +2,13 @@ library(ggplot2)
 library(patchwork)
 library("rlang")
 
-big_little_boxplot <- function(data, xvar, yvar, fillvar, pallet = mdd_Dx_colors,  cutoff = 0.1, title, subtitle, fn){
+big_little_boxplot <- function(data, xvar, yvar, fillvar, colorvar , pallet = mdd_Dx_colors,  cutoff = 0.1, title, subtitle){
+  ignore <- NULL
   
   data <- data %>% group_by(cell_type) %>% mutate(big = mean(prop) > 0.1)
   
   bp_big <- data %>% filter(big) %>%
-    ggplot(aes(x = !!sym(xvar), y = !!sym(yvar), fill = !!sym(fillvar))) +
+    ggplot(aes(x = !!sym(xvar), y = !!sym(yvar), fill = !!sym(fillvar), color = !!sym(colorvar))) +
     geom_boxplot()+
     scale_fill_manual(values = pallet)+
     labs(title = title,
@@ -15,7 +16,7 @@ big_little_boxplot <- function(data, xvar, yvar, fillvar, pallet = mdd_Dx_colors
     theme(legend.position = "None")
   
   bp_little <- data %>% filter(!big) %>%
-    ggplot(aes(x = !!sym(xvar), y = !!sym(yvar), fill = !!sym(fillvar))) +
+    ggplot(aes(x = !!sym(xvar), y = !!sym(yvar), fill = !!sym(fillvar), color = !!sym(colorvar))) +
     geom_boxplot()+
     scale_fill_manual(values = pallet)
   
