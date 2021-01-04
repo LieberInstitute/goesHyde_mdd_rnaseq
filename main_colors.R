ddd9b83e37d3947c0dbed5fc1c5a24cba7287914
lahuuki <lahuuki@gmail.com>
2021-01-04 16:37:48 -0500

    Add light dark Dx colors

--- main_colors.R
+++ main_colors.R
@@ -2,9 +2,20 @@
 library("RColorBrewer")
 
 mdd_Dx_colors <- brewer.pal(3, "Dark2")
-mdd_Dx_colors <- c("Control" = mdd_Dx_colors[[1]],
-                   "Bipolar" = mdd_Dx_colors[[2]],
-                   "MDD" = mdd_Dx_colors[[3]])
+mdd_Dx_colors <- c("Control" = mdd_Dx_colors[[1]], #Green
+                   "Bipolar" = mdd_Dx_colors[[2]], #Orange
+                   "MDD" = mdd_Dx_colors[[3]]) #Purple
+mdd_Dx_colors
+# Control   Bipolar       MDD 
+# "#1B9E77" "#D95F02" "#7570B3" 
+
+mdd_Dx_colors_LD <- c("Control_light" = "2EDCA7",
+                      "Control_dark" = "188C69",
+                      "Bipolar_light" = "FD9749",
+                      "Bipolar_dark" = "B65002",
+                      "MDD_light" = "9794C7",
+                      "MDD_dark" = "5A54A0")
+
 
 mdd_BrainRegion_colors <- brewer.pal(3,"YlGnBu")
 
@@ -15,6 +26,7 @@ mdd_BrainRegion_colors <- c("Amygdala" = mdd_BrainRegion_colors[[1]],
 mdd_dataset_colors <- c("psychENCODE_BP" = mdd_Dx_colors[[2]],
                         "psychENCODE_MDD" = mdd_Dx_colors[[3]])
 
-mdd_Sex_colors <- c(M = "#1E4482", `F` = "#783F58")
+# mdd_Sex_colors <- c(M = "#1E4482", `F` = "#783F58")
+mdd_Sex_colors <- c(M = "steelblue4", `F` = "maroon4")
 
 mdd_all_colors <- c(mdd_Dx_colors, mdd_BrainRegion_colors, mdd_Sex_colors, mdd_dataset_colors)
