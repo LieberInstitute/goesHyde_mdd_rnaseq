889161a4b6f122fa214edc2023233f7a4c771420
lahuuki <lahuuki@gmail.com>
2021-01-05 16:20:26 -0500

    Compare input and output props for common donors

--- deconvolution/deconvo_plots.R
+++ deconvolution/deconvo_plots.R
@@ -9,6 +9,7 @@ library(tidyverse)
 library(broom)
 library(sessioninfo)
 library(pheatmap)
+library(SingleCellExperiment)
 
 ## Load colors and plotting functions
 source(here("main_colors.R"))
@@ -84,7 +85,44 @@ walk2(est_prop, names(est_prop), function(est, n){
   dev.off()
 })
 
+#### Compare MuSiC output with sn data from same donors ####
+load(here("deconvolution","data","sce.sacc_filtered.Rdata"), verbose = TRUE)
+load(here("deconvolution","data","sce.amyg_filtered.Rdata"), verbose = TRUE)
 
+donors <- unique(sce.sacc$donor)
+
+input_cells <- list(sacc_broad = colData(sce.sacc)[,c("donor","cellType.Broad")],
+                   amyg_broad = colData(sce.amyg)[,c("donor","cellType.Broad")],
+                   sacc_specific = colData(sce.sacc)[,c("donor","cellType")],
+                   amyg_specific = colData(sce.amyg)[,c("donor","cellType")])
+
+input_prop <- map(input_cells, function(x){
+  colnames(x)[2] <- "cell_type"
+  
+  x %>% as_tibble %>%
+    group_by(donor, cell_type) %>%
+    count() %>%
+    group_by(donor) %>%
+    mutate(input_prop = n/sum(n))
+})
+
+output_prop <- map2(est_prop_long, input_prop, function(est,input){
+  est %>% select(donor = BrNum, cell_type, prop) %>%
+    inner_join(input)
+})
+
+output_prop_df <- do.call("rbind", output_prop) %>%
+  rownames_to_column(var = "dataset") %>%
+  mutate(dataset = ss(dataset,"\\."))
+
+in_out_prop <- ggplot(output_prop_df, aes(input_prop, prop, color = cell_type, shape = donor))+
+  geom_point() +
+  scale_color_manual(values = cell_colors)+
+  geom_abline()+
+  facet_wrap(~dataset) +
+  labs(y = "MuSiC estimated prop", x = "Input sn data prop")
+
+ggsave(in_out_prop, filename = "plots/prop_input_vs_ouput.png")
 
 # sgejobs::job_single('deconvo_plots', create_shell = TRUE, queue= 'bluejay', memory = '10G', command = "Rscript deconvo_plots.R")
 ## Reproducibility information
