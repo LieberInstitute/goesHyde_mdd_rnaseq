9ecaf9568fbe162789907f409f9cfc959e8aafe6
lahuuki <lahuuki@gmail.com>
2021-01-06 14:31:00 -0500

    Create sh file to run full deconvolution analysis #8

---
+++ deconvolution/deconvolution.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## deconvolution directory
+MAINDIR="/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/deconvolution"
+cd ${MAINDIR}
+
+## Filter sce data 
+rm logs/sce_data_prep.txt
+qsub sce_data_prep.sh
+
+## Find top marker genes
+rm logs/find_markers.txt
+qsub find_markers.sh
+
+## Create heatmaps of selected markers
+rm logs/deconvo_heatmaps.txt
+qsub deconvo_heatmap.sh
+
+## Run MuSiC
+rm logs/music_deconvo.txt
+qsub music_deconvo.sh
+
+## Create plot of deconvolution results
+rm logs.deconvo_plots.txt
+deconvo_plots.sh
+
+## Compare deconvolution results and qSVs 
+rm logs/deconvo_vs_qSV.txt
+deconvo_vs_qSV.sh
+
+
