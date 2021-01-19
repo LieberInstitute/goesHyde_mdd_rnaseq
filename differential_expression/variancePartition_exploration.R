f3a7c72b5174e8750b6d41aff0e916b9c0ffd862
lahuuki <lahuuki@gmail.com>
2021-01-19 16:54:01 -0500

    Run and save both linear and linear mixed models

--- differential_expression/variancePartition_exploration.R
+++ differential_expression/variancePartition_exploration.R
@@ -19,72 +19,25 @@ mod = model.matrix(~PrimaryDx*BrainRegion, pd)
 gExpr <- calcNormFactors(rse_gene)
 vobjGenes <- voom(gExpr, mod)
 
-formJoint = ~PrimaryDx*BrainRegion + AgeDeath + Sex + (1|BrNum) +
-  snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
-  mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr
+## Linear 
+formJoint <- list( linear = ~PrimaryDx*BrainRegion + AgeDeath + Sex +
+                     snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+                     mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr,
+                   
+                   mixed = ~(1|PrimaryDx) + (1|BrainRegion) + (1|PrimaryDx:BrainRegion) + AgeDeath + (1|Sex) +
+                     snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+                     mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr)
 
-#### Var Partion with Model terms ####
-# form <- ~AgeDeath + (1|BrainRegion) + (1|Sex) + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5+ 
-#   mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr
 
-# varPart <- fitExtractVarPartModel(exprObj =assays(rse_gene)$counts, formula = formJoint, data = pd)
-varPart <- fitExtractVarPartModel(exprObj =vobjGenes, formula = formJoint, data = pd)
-vp <- sortCols(varPart)
-vp_violin <- plotVarPart(vp) + labs(title = "modJoint")
+#### Var Partion with linear Model ####
+varPart <- map(formJoint, ~fitExtractVarPartModel(exprObj =vobjGenes, formula = .x, data = pd))
+vp <- map(varPart, sortCols)
 
-ggsave(plot = vp_violin, filename = "plots/vp_violin.png")
+vp_violin <- map2(vp, names(vp), ~plotVarPart(.x) + labs(title = paste("modJoint -",.y)))
 
-# #### Var Partition with cell type props ####
-# load(here("deconvolution","data","est_prop_top5.Rdata"), verbose = TRUE)
-# 
-# ## compute ilr 
-# est_prop_ilr <- map(est_prop, ~ilr(.x$Est.prop.weighted))
-# est_prop_ilr <- map(est_prop_ilr, function(x){
-#   colnames(x) <- paste0("ilr_",1:ncol(x))
-#   return(x)
-# })
-# 
-# #### sACC ####
-# rse_gene_sacc <- rse_gene[,rse_gene$BrainRegion == "sACC"]
-# pd_sacc <- cbind(as.data.frame(colData(rse_gene_sacc)),est_prop_ilr$sacc_specific)
-# 
-# mod_sacc <- model.matrix(~PrimaryDx, pd_sacc)
-# gExpr_sacc <- calcNormFactors(rse_gene_sacc)
-# vobjGenes_sacc <- voom(gExpr_sacc, mod_sacc)
-# 
-# mod_sacc = ~(1|PrimaryDx) + AgeDeath + (1|Sex) + 
-#   snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
-#   mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr +
-#   ilr_1 + ilr_2 + ilr_3 + ilr_4 + ilr_5 + ilr_6 + ilr_7 + ilr_8 +ilr_9 
-# 
-# message("VarPart sACC")
-# varPart_sacc <- fitExtractVarPartModel(exprObj = vobjGenes_sacc, formula = mod_sacc, data = pd_sacc)
-# vp_sacc <- sortCols(varPart_sacc)
-# vp_violin <- plotVarPart(vp_sacc)+ labs(title = "mod_sacc")
-# 
-# ggsave(plot = vp_violin, filename = "plots/vp_violin_sacc.png", width = 10)
-# 
-# ## Amygdala
-# rse_gene_amyg <- rse_gene[,rse_gene$BrainRegion == "Amygdala"]
-# pd_amyg <- cbind(as.data.frame(colData(rse_gene_amyg)),est_prop_ilr$amyg_specific)
-# 
-# mod_amyg <- model.matrix(~PrimaryDx, pd_amyg)
-# gExpr_amyg <- calcNormFactors(rse_gene_amyg)
-# vobjGenes_amyg <- voom(gExpr_amyg, mod_amyg)
-# 
-# mod_amyg = ~(1|PrimaryDx) + AgeDeath + (1|Sex) + 
-#   snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
-#   mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr +
-#   ilr_1 + ilr_2 + ilr_3 + ilr_4 + ilr_5 + ilr_6 + ilr_7 + ilr_8
-# 
-# message("VarPart Amyg")
-# varPart_amyg <- fitExtractVarPartModel(exprObj = vobjGenes_amyg, formula = mod_amyg, data = pd_amyg)
-# vp_amyg <- sortCols(varPart_amyg)
-# vp_violin <- plotVarPart(vp_amyg) + labs(title = "mod_amyg")
-# 
-# ggsave(plot = vp_violin, filename = "plots/vp_violin_amyg.png", width = 10)
+walk2(vp_violin, names(vp_violin), ggsave(plot = vp_violin, filename = paste0("plots/vp_violin_",.y,".png")))
+save(varPart, formJoint, file = "varPart.Rdata")
 
-save(varPart, file = "varPart.Rdata")
 
 # sgejobs::job_single('variancePartition_exploration', create_shell = TRUE, queue= 'bluejay', memory = '150G', command = "Rscript variancePartition_exploration.R")
 
