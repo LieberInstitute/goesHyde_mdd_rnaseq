064086129038b07f422eb86d65dac0101ae36d72
lahuuki <lahuuki@gmail.com>
2021-01-21 15:04:55 -0500

    Add models w/ ilr to variance parition

--- differential_expression/variancePartition_exploration.R
+++ differential_expression/variancePartition_exploration.R
@@ -19,6 +19,16 @@ mod = model.matrix(~PrimaryDx*BrainRegion, pd)
 gExpr <- calcNormFactors(rse_gene)
 vobjGenes <- voom(gExpr, mod)
 
+#### Var Partition with cell type props ####
+load(here("deconvolution","data","est_prop_top5.Rdata"), verbose = TRUE)
+
+## compute ilr for broad cell types
+est_prop_broad <- rbind(est_prop$sacc_broad$Est.prop.weighted, est_prop$amyg_broad$Est.prop.weighted)
+dim(est_prop_broad)
+est_prop_ilr <- ilr(est_prop_broad)
+colnames(est_prop_ilr) <-paste0("ilr_",1:ncol(est_prop_ilr))
+
+pd <- cbind(pd, est_prop_ilr)
 ## Linear 
 formJoint <- list( linear = ~PrimaryDx*BrainRegion + AgeDeath + Sex +
                      snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
@@ -26,7 +36,17 @@ formJoint <- list( linear = ~PrimaryDx*BrainRegion + AgeDeath + Sex +
                    
                    mixed = ~(1|PrimaryDx) + (1|BrainRegion) + (1|PrimaryDx:BrainRegion) + AgeDeath + (1|Sex) +
                      snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
-                     mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr)
+                     mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr,
+                   
+                   linear_ilr = ~PrimaryDx*BrainRegion + AgeDeath + Sex +
+                     snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+                     mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr+
+                     ilr_1 + ilr_2 + ilr_3 + ilr_4 + ilr_5,
+                   
+                   mixed_ilr = ~(1|PrimaryDx) + (1|BrainRegion) + (1|PrimaryDx:BrainRegion) + AgeDeath + (1|Sex) +
+                     snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
+                     mitoRate + rRNA_rate + totalAssignedGene + RIN + ERCCsumLogErr +
+                     ilr_1 + ilr_2 + ilr_3 + ilr_4 + ilr_5)
 
 
 #### Var Partion with linear Model ####
