58e1b5886b9b6a3b44d11b75edf493ede98647cb
lahuuki <lahuuki@gmail.com>
2021-03-15 12:28:29 -0400

    Use pan-brain object as sce input #9

--- deconvolution/sce_data_prep.R
+++ deconvolution/sce_data_prep.R
@@ -5,123 +5,61 @@ library(here)
 library(GenomicFeatures)
 library(sessioninfo)
 
-#### Load and filter data ####
-## Load rse_gene data
-load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
-rownames(rse_gene) <- rowData(rse_gene)$ensemblID
-
-## sACC data
-load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/regionSpecific_sACC-n2_cleaned-combined_SCE_MNTFeb2020.rda", verbose = TRUE)
-sce.sacc$uniqueID <- paste0(sce.sacc$donor, "_", sce.sacc$Barcode)
-colnames(sce.sacc) <- sce.sacc$uniqueID
-
-sce.sacc <- sce.sacc[,sce.sacc$cellType != "Ambig.lowNtrxts",]
-sce.sacc$cellType <- droplevels(sce.sacc$cellType)
-## Add cellType.broad
-sce.sacc$cellType.Broad <- ss(as.character(sce.sacc$cellType), "\\.", 1)
-sce.sacc$cellType.Broad <- factor(sce.sacc$cellType.Broad)
-## Match rownames
-rownames(sce.sacc) <- rowData(sce.sacc)$ID
-table(rownames(sce.sacc) %in% rownames(rse_gene))
-# FALSE  TRUE 
-# 15021 18517 
-
-## filter for common between sc and bulk data
-common_genes <- rownames(rse_gene)[rownames(rse_gene) %in% rownames(sce.sacc)]
-sce.sacc <- sce.sacc[common_genes, ]
-
-# Remove 0 genes across all nuclei
-sce.sacc <- sce.sacc[!rowSums(assay(sce.sacc, "counts"))==0, ] 
-dim(sce.sacc)
-# [1] 17785  7004
-
-## Amyg Data
-load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/regionSpecific_Amyg-n2_cleaned-combined_SCE_MNTFeb2020.rda", verbose = TRUE)
-sce.amy$uniqueID <- paste0(sce.amy$donor, "_", sce.amy$Barcode)
-colnames(sce.amy) <- sce.amy$uniqueID
-## make cellType cols the same
-sce.amy$cellType = sce.amy$cellType.split
-sce.amy <- sce.amy[,sce.amy$cellType != "Ambig.lowNtrxts",]
-sce.amy$cellType <- droplevels(sce.amy$cellType)
-## Add cellType.broad
-sce.amy$cellType.Broad <- ss(as.character(sce.amy$cellType), "\\.", 1)
-sce.amy$cellType.Broad <- factor(sce.amy$cellType.Broad)
-## Match rownames
-rownames(sce.amy) <- rowData(sce.amy)$ID
-table(rownames(sce.amy) %in% rownames(rse_gene))
-# FALSE  TRUE 
-# 15021 18517 
-
-## filter for common between sc and bulk data
-common_genes <- rownames(rse_gene)[rownames(rse_gene) %in% rownames(sce.amy)]
-sce.amy <- sce.amy[common_genes, ]
-
-# Remove 0 genes across all nuclei
-sce.amy <- sce.amy[!rowSums(assay(sce.amy, "counts"))==0, ] 
-dim(sce.amy)
-# [1] 17792  6582
-
-#### Add bp length for RPKM later #### 
-rd.sacc <- rowData(sce.sacc)
-rd.amy <- rowData(sce.amy)
-## Import gnomic features
-txdb <- makeTxDbFromGFF("/dcl01/ajaffe/data/lab/singleCell/10x_pilot/premRNA/GRCh38-3.0.0_premrna/genes/genes.gtf")
-g <- genes(txdb)
-e <- exonsBy(txdb, by = "gene")
-
-if (!all(names(e) %in% names(g))) {
-  warning("Dropping exons with gene ids not present in the gene list")
-  e <- e[names(e) %in% names(g)]
-}
-e2 <- disjoin(e)
-g$bp_length <- sum(width(e2))
-summary(g$bp_length)
-
-## Add data to sacc
-g_sacc <- g[rownames(sce.sacc),]
-summary(g_sacc$bp_length)
-table(g_sacc$bp_length > 205012)
-
-rowRanges(sce.sacc) <- g_sacc
-all(rownames(rd.sacc) == rownames(sce.sacc))
-rowData(sce.sacc)$Symbol <- rd.sacc$Symbol
-
-## Add data to amy data
-g_amy <- g[rownames(sce.amy),]
-summary(g_amy$bp_length)
-table(g_amy$bp_length > 205012)
-
-rowRanges(sce.amy) <- g_amy
-all(rownames(rd.amy) == rownames(sce.amy))
-rowData(sce.amy)$Symbol <- rd.amy$Symbol
-
-## rename amy to amyg to be consistent with rest of mdd scripts
-sce.amyg <- sce.amy
-
-## Save filtered sce object
-sce <- list(sacc = sce.sacc, amyg = sce.amyg)
-
-## Fix cellType factor order
 order_ct <- function(cellTypes){
   excit_ct <- cellTypes[grep("Excit", cellTypes)]
   inhib_ct <- cellTypes[grep("Inhib", cellTypes)]
   other_ct <- cellTypes[!cellTypes %in% c(excit_ct, inhib_ct)]
   
-  cell_order <- c(other_ct, excit_ct, inhib_ct)
+  cell_order <- c(other_ct[order(other_ct)], excit_ct[order(excit_ct)], inhib_ct[order(inhib_ct)])
   return(cell_order)
 }
 
-sce <- map(sce, function(s){
-  co <- order_ct(levels(s$cellType))
-  co.Broad <- order_ct(levels(s$cellType.Broad))
-  
-  
-  s$cellType <- factor(s$cellType,levels = co)
-  s$cellType.Broad <- factor(s$cellType.Broad,levels = co.Broad)
-  return(s)
-})
+#### Load and filter data ####
+## Load rse_gene data
+load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
+rownames(rse_gene) <- rowData(rse_gene)$ensemblID
 
-save(sce, file = here("deconvolution","data","sce_filtered.Rdata"))
+## pan-brain data
+load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/all-FACS-homogenates_n12_PAN-BRAIN-Analyses_MNTFeb2020.rda", verbose = TRUE)
+sce.all.n12$uniqueID <- paste0(sce.all.n12$donor, "_", sce.all.n12$Barcode ,"_", sce.all.n12$region)
+length(unique(sce.all.n12$uniqueID))  == ncol(sce.all.n12)
+colnames(sce.all.n12) <- sce.all.n12$uniqueID
+
+sce.all.n12 <- sce.all.n12[ ,sce.all.n12$cellType != "Ambig.hiVCAN"] # 32 nuclei
+sce.all.n12 <- sce.all.n12[ ,sce.all.n12$cellType != "Excit.4"]  # 33 nuclei
+sce.all.n12$cellType <- droplevels(sce.all.n12$cellType)
+
+## reorder cell types 
+cellType_order <- order_ct(levels(sce.all.n12$cellType))
+sce.all.n12$cellType <- factor(sce.all.n12$cellType, cellType_order)
+
+table(sce.all.n12$cellType)
+
+## Add cellType.broad
+sce.all.n12$cellType.Broad <- ss(as.character(sce.all.n12$cellType), "\\.", 1)
+table(sce.all.n12$cellType.Broad)
+
+sce.all.n12$cellType.Broad <- factor(sce.all.n12$cellType.Broad, order_ct(unique(sce.all.n12$cellType.Broad)))
+table(sce.all.n12$cellType.Broad)
+
+
+## Match rownames
+rownames(sce.all.n12) <- rowData(sce.all.n12)$ID
+table(rownames(sce.all.n12) %in% rownames(rse_gene))
+# FALSE  TRUE 
+# 15021 18517 
+
+## filter for common between sc and bulk data
+common_genes <- rownames(rse_gene)[rownames(rse_gene) %in% rownames(sce.all.n12)]
+sce.all.n12 <- sce.all.n12[common_genes, ]
+
+# Remove 0 genes across all nuclei
+sce.all.n12 <- sce.all.n12[!rowSums(assay(sce.all.n12, "counts"))==0, ] 
+dim(sce.all.n12)
+# [1] 17893 34005
+
+sce_pan <- sce.all.n12
+save(sce_pan, file = here("deconvolution","data","sce_filtered.Rdata"))
 
 # sgejobs::job_single('sce_data_prep', create_shell = TRUE, queue= 'bluejay', memory = '10G', command = "Rscript 1_sce_data_prep.R")
 ## Reproducibility information
