d24ca05b8023fd594c5dd8f97ee72c7ec0392a5d
lahuuki <lahuuki@gmail.com>
2021-03-22 13:28:37 -0400

    Move rank check to find_markers

--- deconvolution/find_markers.R
+++ deconvolution/find_markers.R
@@ -44,6 +44,62 @@ top_marker_table %>% write_csv(here("deconvolution","data",paste0("top",n_genes,
 marker_genes <- top_marker_table %>% pull("gene")
 save(marker_genes, file = here("deconvolution","data","marker_genes.Rdata"))
 
+#### Rank Plots ####
+sc_mean_expr <- rowMeans(assays(sce_pan)$logcounts)
+
+sce_rank <- tibble(sce_mean_expr = sc_mean_expr,
+                   gene = names(sc_mean_expr))
+
+rpkm <- getRPKM(rse_gene, "Length")
+bulk_mean_expr <- rowMeans(rpkm)
+
+bulk_rank <- tibble(bulk_mean_expr = bulk_mean_expr,
+                    gene = rowData(rse_gene)$ensemblID)
+
+all_rank <- left_join(sce_rank, bulk_rank, by = "gene") %>%
+  arrange(bulk_mean_expr) %>%
+  mutate(bulk_rank = row_number())%>%
+  arrange(sce_mean_expr) %>%
+  mutate(sce_rank = row_number()) %>%
+  left_join(marker_anno, by = "gene") %>%
+  mutate(marker = !is.na(rank_ratio),
+         log_bulk_mean_rpkm = log(bulk_mean_expr + 1))
+
+all_rank$Length <- rowData(rse_gene)[all_rank$gene,]$Length
+
+
+rank_plot <-  ggplot() +
+  geom_point(data = all_rank, aes(x = bulk_rank, y = sce_rank),
+             size = 0.5, alpha = 0.25, color = "grey") +
+  geom_point(data = all_rank %>% filter(marker),
+             aes(x = bulk_rank, y = sce_rank, color = cellType.target)) +
+  geom_text(data = all_rank %>% filter(marker),
+            aes(x = bulk_rank, y = sce_rank, label = Symbol),
+            vjust = 0, nudge_y = 1, size = 1.5)+
+  geom_abline(intercept = c(5000, -5000))+
+  scale_color_manual(values = cell_colors) +
+  labs(title = paste("Markers Over Mean Expr Rank"),
+       x = "Rank of Mean RPKM in Bulk", y = "Rank of Mean logcounts in SC")
+
+ggsave(rank_plot, filename = here("deconvolution","plots","expr_rank.png"))
+
+expr_plot <-  ggplot() +
+  geom_point(data = all_rank, aes(x = log_bulk_mean_rpkm, y = sce_mean_expr),
+             size = 0.5, alpha = 0.25, color = "grey") +
+  geom_point(data = all_rank %>% filter(marker),
+             aes(x = log_bulk_mean_rpkm, y = sce_mean_expr, color = cellType.target)) +
+  geom_text(data = all_rank %>% filter(marker),
+            aes(x = log_bulk_mean_rpkm, y = sce_mean_expr, label = Symbol),
+            vjust = 0,  size = 1.5)+
+  # geom_abline(intercept = c(5000, -5000))+
+  scale_color_manual(values = cell_colors) +
+  labs(title = paste("Markers Over Mean Expr"),
+       x = "Log Mean RPKM in Bulk", y = "Mean logcounts in SC")
+
+ggsave(expr_plot, filename = here("deconvolution","plots","expr_mean.png"))
+
+
+
 #### Create Ratio plots #### 
 ratio_plot <- marker_stats %>% mutate(Marker = rank_ratio <= n_genes) %>%
   ggplot(aes(x=ratio, y=std.logFC, color = Marker))+
@@ -53,7 +109,7 @@ ratio_plot <- marker_stats %>% mutate(Marker = rank_ratio <= n_genes) %>%
   # scale_color_manual(values = cell_colors) +
   theme_bw()
 
-ggsave(ratio_plot, filename = paste0("plots/expr/ratio_vs_stdFC.png"), width = 10)
+ggsave(ratio_plot, filename = here("deconvolution","plots","ratio_vs_stdFC.png"), width = 10)
 
 #### Plot expression ####
 pdf(here("deconvolution","plots",paste0("expr_top25_mean_ratio.pdf")))
