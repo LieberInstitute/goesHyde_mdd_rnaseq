3a664d758d69a19ebd5a645bf9a2d6e88b09530e
aseyedia <arta.seyedian@gmail.com>
2021-05-26 11:31:59 -0400

    moved to src dir, added shell script to create inverse normalized expression for PredictDB pipeline

---
+++ predixcan_pipeline/src/00_get_inv_quantile_norm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -N eqtl_prepare_expression
+#$ -j y
+#$ -o eqtl_prepare_expression.log
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: \${USER}"
+echo "Job id: \${JOB_ID}"
+echo "Job name: \${JOB_NAME}"
+echo "Hostname: \${HOSTNAME}"
+echo "Task id: \${TASK_ID}"
+
+mkdir processed-data/
+mkdir plots/
+mkdir code/
+
+ml htslib
+
+module use /jhpce/shared/jhpce/modulefiles/libd
+
+Rscript 00_prepare_gene_expression.R
+
+vcf="../genotype_data/topmed_mdd_602sample_090120_maf005.vcf.gz"
+
+tabix --list-chroms ${vcf} > "../processed-data/vcf_chr_list.txt"
+
+eqtl_prepare_expression.py ${tpm_gct} ${counts_gct} ${annotation_gtf} \
+    ${sample_participant_lookup} vcf_chr_list.txt ${prefix} \
+    --tpm_threshold 0.1 \
+    --count_threshold 6 \
+    --sample_frac_threshold 0.2 \
+    --normalization_method tmm
+
+echo "**** Job ends ****"
+date
