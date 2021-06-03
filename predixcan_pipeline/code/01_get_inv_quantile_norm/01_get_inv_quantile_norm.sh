eb5332f33c5c4e8c8f8dc624ca06add7c75988ab
aseyedia <arta.seyedian@gmail.com>
2021-06-03 11:03:27 -0400

    expression normalization almost complete, getting strange error in 01_get_inv_quantile_norm.sh telling me that goesHyde_mdd_rnaseq_ needs to be defined and supplied to the script when it clearly is

--- predixcan_pipeline/code/01_get_inv_quantile_norm/01_get_inv_quantile_norm.sh
+++ predixcan_pipeline/code/01_get_inv_quantile_norm/01_get_inv_quantile_norm.sh
@@ -20,9 +20,13 @@ mkdir -p processed-data/01_get_inv_quantile_norm
 mkdir plots/
 mkdir -p code/01_get_inv_quantile_norm
 
+# conda create -n eqtl_prepare_expression python=3.5
+# pip install pandas numpy scipy argparse qtl --user
+conda activate eqtl_prepare_expression
+
 ml htslib
 ml bcftools
-ml python/3.6.9
+ml conda_R/4.1
 
 module use /jhpce/shared/jhpce/modulefiles/libd
 
@@ -31,6 +35,7 @@ tpm_gct="processed-data/tpm.gct"
 counts_gct="processed-data/gene_counts.gct"
 annotation_gtf="/dcl01/lieber/ajaffe/Emily/RNAseq-pipeline/Annotation/GENCODE/GRCh38_hg38/gencode.v25.annotationGRCh38.gtf"
 prefix="goesHyde_mdd_rnaseq_"
+vcf_chr_list="processed-data/01_get_inv_quantile_norm/vcf_chr_list.txt"
 
 bcftools query -l ${vcf} > "processed-data/01_get_inv_quantile_norm/vcf_samples.txt"
 
@@ -38,11 +43,15 @@ Rscript code/01_get_inv_quantile_norm/01_prepare_gene_expression.R
 
 tabix --list-chroms ${vcf} > "processed-data/01_get_inv_quantile_norm/vcf_chr_list.txt"
 
-# adding normalization script from gtex-pipeline: https://github.com/broadinstitute/gtex-pipeline/tree/master/qtl
-wget -P "code/01_get_inv_quantile_norm/" https://raw.githubusercontent.com/broadinstitute/gtex-pipeline/master/qtl/src/eqtl_prepare_expression.py
+if [ ! -f "code/01_get_inv_quantile_norm/eqtl_prepare_expression.py" ]; then
+  # adding normalization script from gtex-pipeline: https://github.com/broadinstitute/gtex-pipeline/tree/master/qtl
+  wget -P "code/01_get_inv_quantile_norm/" https://raw.githubusercontent.com/broadinstitute/gtex-pipeline/master/qtl/src/eqtl_prepare_expression.py
+else
+  echo "eqtl_prepare_expression.py already exists."
+fi
 
-eqtl_prepare_expression.py ${tpm_gct} ${counts_gct} ${annotation_gtf} \
-    ${sample_participant_lookup} "processed-data/vcf_chr_list.txt" ${prefix} \
+./code/01_get_inv_quantile_norm/eqtl_prepare_expression.py ${tpm_gct} ${counts_gct} ${annotation_gtf} \
+    ${sample_participant_lookup} ${vcf_chr_list} ${prefix} \
     --tpm_threshold 0.1 \
     --count_threshold 6 \
     --sample_frac_threshold 0.2 \
@@ -52,3 +61,5 @@ mv "${prefix}*" "processed-data/01_get_inv_quantile_norm/"
 
 echo "**** Job ends ****"
 date
+
+# mv code/01_get_inv_quantile_norm eqtl_prepare_expression.log processed-data/01_get_inv_quantile_norm
