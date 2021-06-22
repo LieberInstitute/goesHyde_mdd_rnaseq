08e471df9bd2f5127b053d649f4f4c1f2357b18f
aseyedia <arta.seyedian@gmail.com>
2021-06-22 15:51:37 -0400

    generated tmm normalized expression; added chr to vcf chr list

--- predixcan_pipeline/code/01_get_inv_quantile_norm/01_get_inv_quantile_norm.sh
+++ predixcan_pipeline/code/01_get_inv_quantile_norm/01_get_inv_quantile_norm.sh
@@ -45,6 +45,8 @@ Rscript code/01_get_inv_quantile_norm/01_prepare_gene_expression.R -r "Amygdala"
 Amyg_vcf="processed-data/01_get_inv_quantile_norm/topmed_mdd_602sample_090120_maf005_Amygdala_sorted.vcf.gz"
 sACC_vcf="processed-data/01_get_inv_quantile_norm/topmed_mdd_602sample_090120_maf005_sACC_sorted.vcf.gz"
 
+prefix_Amygdala="goesHyde_mdd_rnaseq_Amygdala"
+
 bgzip -c "processed-data/01_get_inv_quantile_norm/topmed_mdd_602sample_090120_maf005_Amygdala_sorted.vcf" > ${Amyg_vcf}
 bgzip -c "processed-data/01_get_inv_quantile_norm/topmed_mdd_602sample_090120_maf005_sACC_sorted.vcf" > ${sACC_vcf}
 
@@ -52,7 +54,7 @@ tabix -p vcf ${Amyg_vcf}
 tabix -p vcf ${sACC_vcf}
 
 # It's just 1-23
-tabix --list-chroms ${Amyg_vcf} > "processed-data/01_get_inv_quantile_norm/vcf_chr_list.txt"
+tabix --list-chroms ${Amyg_vcf} | awk '$1="chr"$1' > "processed-data/01_get_inv_quantile_norm/vcf_chr_list.txt"
 
 if [ ! -f "code/01_get_inv_quantile_norm/eqtl_prepare_expression.py" ]; then
   # adding normalization script from gtex-pipeline: https://github.com/broadinstitute/gtex-pipeline/tree/master/qtl
@@ -65,13 +67,15 @@ fi
 # pip install pandas numpy scipy argparse qtl --user
 conda activate eqtl_prepare_expression
 
+# bgzip needs to be mounted as a module through htslib for this step. If you're
+# encountering some bgzip related error, it's probably for that reason.
 ./code/01_get_inv_quantile_norm/eqtl_prepare_expression.py ${Amyg_tpm_gct} ${Amyg_counts_gct} ${annotation_gtf} \
-    ${Amyg_sample_participant_lookup} ${vcf_chr_list} "goesHyde_mdd_rnaseq_Amygdala_" \
+    ${Amyg_sample_participant_lookup} ${vcf_chr_list} ${prefix_Amygdala} \
     --tpm_threshold 0.1 \
     --count_threshold 6 \
     --sample_frac_threshold 0.2 \
     --normalization_method tmm \
-    --output_dir " /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/01_get_inv_quantile_norm/"
+    --output_dir "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/01_get_inv_quantile_norm/"
 
 # 16:13 predixcan_pipeline $ ./code/01_get_inv_quantile_norm/eqtl_prepare_expression.py ${tpm_gct} ${counts_gct} ${annotation_gtf} \
 # >     ${sample_participant_lookup} ${vcf_chr_list} ${prefix} \
