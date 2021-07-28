8ece6d2159be88c45fef119e7882d539611f2ace
aseyedia <arta.seyedian@gmail.com>
2021-07-28 14:32:49 -0400

    renamed column headers in the snp annotation file, adding variantID column to genotype, may need to query more RSIDs

--- predixcan_pipeline/code/02_prep_inputs/01_prep_predictdb.sh
+++ predixcan_pipeline/code/02_prep_inputs/01_prep_predictdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -l bluejay,mem_free=30G,h_vmem=30G,h_fsize=100G
 #$ -N PredictDB_prep
 #$ -j y
 #$ -o /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/logs/PredictDB_prep.log
@@ -19,6 +19,18 @@ conda activate eqtl_prepare_expression
 python /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/PredictDB-Tutorial/code/parse_gtf.py \
   /dcl01/lieber/ajaffe/Emily/RNAseq-pipeline/Annotation/GENCODE/GRCh38_hg38/gencode.v25.annotationGRCh38.gtf \
   /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/gencode.v25.annotationGRCh38_PARSED.gtf
+
+module load conda_R/4.1
+
+Rscript /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/1a_process_snp_anno.R
+
+conda activate eqtl_prepare_expression
+
+mkdir /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/snp_annot
+
+python /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/PredictDB-Tutorial/code/split_snp_annot_by_chr.py \
+  /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/snp_annot_prep.txt \
+  /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/snp_annot
 
 echo "**** Job ends ****"
 date
--- predixcan_pipeline/code/02_prep_inputs/1a_process_snp_anno.R
+++ predixcan_pipeline/code/02_prep_inputs/1a_process_snp_anno.R
@@ -1,16 +1,34 @@
+library(dplyr)
+# qrsh -l mem_free=50G,h_vmem=50G
+
 load("/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/goesHyde_bipolarMdd_Genotypes.rda")
 
 snp_anno <- snpMap[, c("CHR", "POS", "SNP", "COUNTED", "ALT")]
 
-snp_anno$snp_id_originalVCF <- paste0("snp_", snp_anno$CHR, "_", snp_anno$POS)
+# snp_anno$snp_id_originalVCF <- paste0("snp_", snp_anno$CHR, "_", snp_anno$POS)
 
 snp_anno$RSID_dbSNP137 <- snp_anno$rsNumGuess
 
-snp_anno$Num_alt_per_site <- nchar(snp_anno$ALT)
+# snp_anno$Num_alt_per_site <- nchar(snp_anno$ALT)
 
-colnames(snp_anno) <- c("Chr", "Pos", "VariantID", "Ref_b37", "Alt", "snp_id_originalVCF")
+# colnames(snp_anno) <- c("Chr", "Pos", "VariantID", "Ref_b37", "Alt", "snp_id_originalVCF", "Num_alt_per_site")
+colnames(snp_anno) <- c("chromosome", "Pos", "VariantID", "ref_vcf", "alt_vcf")
+
+# snp_anno$VariantID <- snp_anno$VariantID %>% gsub(pattern = "[:]", replacement = "_") %>% gsub(pattern = "chr", replacement = "")
+
+snp_gen <- snp
+
+snp_gen$VariantID <- snp_anno$VariantID
+
+snp_gen <- snp_gen %>% 
+  relocate(VariantID)
 
 write.table(snp_anno, 
             file = here::here("predixcan_pipeline", "processed-data", "02_prep_inputs", "snp_annot_prep.txt"), 
+            quote = FALSE,
+            sep = "\t")
+
+write.table(snp_gen, 
+            file = here::here("predixcan_pipeline", "processed-data", "02_prep_inputs", "genotype.txt"), 
             quote = FALSE,
             sep = "\t")
