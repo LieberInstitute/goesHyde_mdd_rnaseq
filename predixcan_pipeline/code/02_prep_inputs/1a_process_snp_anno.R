4ce5fcc85f3cb74b6365040aad04660a0eeadb6b
aseyedia <arta.seyedian@gmail.com>
2021-07-29 15:53:10 -0400

    added the snp_id_originalVCF column to the snp annot file for compatibility reasons

--- predixcan_pipeline/code/02_prep_inputs/1a_process_snp_anno.R
+++ predixcan_pipeline/code/02_prep_inputs/1a_process_snp_anno.R
@@ -5,14 +5,18 @@ load("/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/goesHyde_bipola
 
 snp_anno <- snpMap[!is.na(snpMap$rsNumGuess),]
 
-snp_anno <- snp_anno[, c("CHR", "POS", "SNP", "COUNTED", "ALT", "rsNumGuess")]
+snp_anno <- snp_anno[, c("CHR", "POS", "SNP", "COUNTED", "ALT")]
 
-# snp_anno$snp_id_originalVCF <- paste0("snp_", snp_anno$CHR, "_", snp_anno$POS)
+snp_anno$snp_id_originalVCF <- paste0("snp_", snp_anno$CHR, "_", snp_anno$POS)
 
-# snp_anno$Num_alt_per_site <- nchar(snp_anno$ALT)
+snp_anno$rsNumGuess <- snpMap$rsNumGuess
+
+snp_anno$Num_alt_per_site <- nchar(snp_anno$ALT)
 
 # colnames(snp_anno) <- c("Chr", "Pos", "VariantID", "Ref_b37", "Alt", "snp_id_originalVCF", "Num_alt_per_site")
-colnames(snp_anno) <- c("chromosome", "pos", "varID", "ref_vcf", "alt_vcf", "rsid")
+colnames(snp_anno) <- c("chromosome", "pos", "varID", "ref_vcf", "alt_vcf", "snp_id_originalVCF","rsid")
+
+snp_anno <- snp_anno[snp_anno$chromosome != "X",]
 
 # snp_anno$VariantID <- snp_anno$VariantID %>% gsub(pattern = "[:]", replacement = "_") %>% gsub(pattern = "chr", replacement = "")
 
