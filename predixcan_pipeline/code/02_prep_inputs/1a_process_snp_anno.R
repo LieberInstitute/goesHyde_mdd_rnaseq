b5b99d80550edaaf3bc75b8beda57fe72d8e64be
aseyedia <arta.seyedian@gmail.com>
2021-07-29 14:09:55 -0400

    updated the snp annotation preprocessing R script

--- predixcan_pipeline/code/02_prep_inputs/1a_process_snp_anno.R
+++ predixcan_pipeline/code/02_prep_inputs/1a_process_snp_anno.R
@@ -3,16 +3,16 @@ library(dplyr)
 
 load("/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/goesHyde_bipolarMdd_Genotypes.rda")
 
-snp_anno <- snpMap[, c("CHR", "POS", "SNP", "COUNTED", "ALT")]
+snp_anno <- snpMap[!is.na(snpMap$rsNumGuess),]
+
+snp_anno <- snp_anno[, c("CHR", "POS", "SNP", "COUNTED", "ALT", "rsNumGuess")]
 
 # snp_anno$snp_id_originalVCF <- paste0("snp_", snp_anno$CHR, "_", snp_anno$POS)
-
-snp_anno$RSID_dbSNP137 <- snp_anno$rsNumGuess
 
 # snp_anno$Num_alt_per_site <- nchar(snp_anno$ALT)
 
 # colnames(snp_anno) <- c("Chr", "Pos", "VariantID", "Ref_b37", "Alt", "snp_id_originalVCF", "Num_alt_per_site")
-colnames(snp_anno) <- c("chromosome", "Pos", "VariantID", "ref_vcf", "alt_vcf")
+colnames(snp_anno) <- c("chromosome", "pos", "varID", "ref_vcf", "alt_vcf", "rsid")
 
 # snp_anno$VariantID <- snp_anno$VariantID %>% gsub(pattern = "[:]", replacement = "_") %>% gsub(pattern = "chr", replacement = "")
 
@@ -26,9 +26,11 @@ snp_gen <- snp_gen %>%
 write.table(snp_anno, 
             file = here::here("predixcan_pipeline", "processed-data", "02_prep_inputs", "snp_annot_prep.txt"), 
             quote = FALSE,
-            sep = "\t")
+            sep = "\t",
+            row.names = FALSE)
 
 write.table(snp_gen, 
             file = here::here("predixcan_pipeline", "processed-data", "02_prep_inputs", "genotype.txt"), 
             quote = FALSE,
-            sep = "\t")
+            sep = "\t",
+            row.names = FALSE)
