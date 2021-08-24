96df3cace38cdcfb6e721e4c3b128de64b28ce0d
lahuuki <lahuuki@gmail.com>
2021-08-24 11:41:08 -0400

    Experiment w/ snp matching between bsp1 & TopMed data

--- eqtl/genomewide/get_genomewide_SNP_list.R
+++ eqtl/genomewide/get_genomewide_SNP_list.R
@@ -9,7 +9,7 @@ dir.create(here("eqtl", "genomewide", "rdas"), showWarnings = FALSE)
 ## load SNP data
 
 ## Amyg / sACC snps
-load(here("genotype_data", "goesHyde_bipolarMdd_Genotypes_n593.rda"), verbose = TRUE) # need ~50G ram to load
+load(here("genotype_data", "goesHyde_bipolarMdd_Genotypes.rda"), verbose = TRUE) # need ~50G ram to load
 snpMap$pos_hg19 <- paste0(snpMap$CHR, ":", snpMap$POS)
 snpMap_amyg <- snpMap
 
@@ -18,8 +18,51 @@ load("/dcl01/ajaffe/data/lab/brainseq_phase1/genotype_data/brainseq_phase1_Genot
 snpMap$pos_hg19 <- paste0(snpMap$CHR, ":", snpMap$POS)
 rm(snp, mds)
 
+table(snpMap_amyg$rsNumGuess %in% snpMap$rsNumGuess)
+# FALSE    TRUE 
+# 342276 8549784 
+table(!is.na(snpMap_amyg$chr_hg38))
+snpMap_amyg <- snpMap_amyg[!is.na(snpMap_amyg$chr_hg38),]
+snpMap <- snpMap[!is.na(snpMap$chr_hg38),]
+
+snpMap_amyg$SNP_hg38 <- paste0(snpMap_amyg$chr_hg38, ":", snpMap_amyg$pos_hg38, ":", snpMap_amyg$COUNTED, ":", snpMap_amyg$ALT)
+snpMap$SNP_hg38 <- paste0(snpMap$chr_hg38, ":", snpMap$pos_hg38, ":", snpMap$COUNTED, ":", snpMap$ALT)
+
+common_snp_hg38 <- intersect(snpMap_amyg$SNP_hg38, snpMap$SNP_hg38)
+length(common_snp_hg38)
+# [1] 49852
+
+#8M
+nrow(snpMap_amyg) - length(unique(snpMap_amyg$SNP_hg38)) #[1] 3
+#6M
+nrow(snpMap) - length(unique(snpMap$SNP_hg38)) #[1] 59
+
+snpMap_amyg_match <- snpMap_amyg[snpMap_amyg$SNP_hg38 %in% snpMap$SNP_hg38,]
+nrow(snpMap_amyg_match)
+snpMap_match <- snpMap[match(snpMap_amyg_match$SNP_hg38,snpMap$SNP_hg38),]
+nrow(snpMap_match)
+## pretty sure these are the same!(except NAs)
+table(snpMap_amyg_match$rsNumGuess == snpMap_match$rsNumGuess)
+
+## whats up w/ rsNumGuess?
+table(is.na(snpMap_amyg_match$rsNumGuess))
+table(is.na(snpMap_match$rsNumGuess))
+
+common_rsNumGuess <- intersect(unique(snpMap_amyg$rsNumGuess), unique(snpMap$name))
+length(common_rsNumGuess)
+# [1] 67628
+
+snpMap_mismatch <- snpMap_amyg[
+  (snpMap_amyg$rsNumGuess %in% common_rsNumGuess) & 
+    !(snpMap_amyg$SNP_hg38 %in% common_snp_hg38) & 
+    !is.na(snpMap_amyg$rsNumGuess),]
+
+nrow(snpMap_mismatch)
+head(snpMap_mismatch)
+
 message(Sys.time(), " Get snp index")
-ind <- which(snpMap$SNP %in% snpMap_amyg$SNP)
+# ind <- which(snpMap$SNP %in% snpMap_amyg$SNP)
+ind <- which(snpMap$SNP_hg38 %in% common_snp_hg38)
 message(Sys.time(), " Filter snpMap")
 snpMap <- snpMap[ind, ]
 
