4c956845c49aa64d8415707c7e423743ef6be6c7
lahuuki <lahuuki@gmail.com>
2021-08-30 16:55:12 -0400

    Begin to update

--- eqtl/genomewide/make_boxplots_and_csv_amyg.R
+++ eqtl/genomewide/make_boxplots_and_csv_amyg.R
@@ -19,36 +19,46 @@ load(here("exprs_cutoff", "rse_tx.Rdata"), verbose = TRUE)
 pd <- colData(rse_gene)
 
 ## load SNP data
-load(here("eqtl", "genomewide", "rdas", "overlappingSNPs.rda"), verbose = TRUE) # snpMapKeep
-load(here("genotype_data", "goesHyde_bipolarMdd_Genotypes_n593.rda"), verbose = TRUE) # snp, snpMap & mds
+# load(here("eqtl", "genomewide", "rdas", "overlappingSNPs.rda"), verbose = TRUE) # snpMapKeep
+load(here("genotype_data", "goesHyde_bipolarMdd_Genotypes.rda"), verbose = TRUE) # snp, snpMap & mds
 snpMap$pos_hg19 <- paste0(snpMap$CHR, ":", snpMap$POS)
 
-#  In Bipolar code rownames are SNP, need to assign here
-# > identical(rownames(snpMap), snpMap$SNP)
-# [1] TRUE
 rownames(snp) <- rownames(snpMap) <- snpMap$SNP
-snpInd <- which(rownames(snpMap) %in% rownames(snpMapKeep) & !is.na(snpMap$pos_hg38))
-snpMap <- snpMap[snpInd, ]
-snp <- snp[snpInd, ]
+
+has_pos <- !is.na(snpMap$pos_hg38)
+snpMap <- snpMap[has_pos, ]
+snp <- snp[has_pos, ]
+
+# First 1M to test
+# snpInd <- 1:1e6
+# snpMap <- snpMap[snpInd, ]
+# snp <- snp[snpInd, ]
 
 #####################
 # filter brain region
 # make mds and snp dimensions equal to N
 # (repeat rows or columns for BrNum replicates)
+## FILTER REGION?
+regInd <- which(colData(rse_gene)$BrainRegion == "Amygdala")
+sum(regInd) #540
+
 table(pd$BrNum %in% colnames(snp))
 table(pd$BrNum %in% rownames(mds))
 
-has_genotype <- pd$BrNum %in% rownames(mds) # snp and mds are missing BR1843
-rse_gene <- rse_gene[, has_genotype]
-rse_exon <- rse_exon[, has_genotype]
-rse_jxn <- rse_jxn[, has_genotype]
-rse_tx <- rse_tx[, has_genotype]
+has_genotype <- pd$BrNum %in% rownames(mds) 
+all(has_genotype) ## All BrNum are in data
 
-pd <- pd[has_genotype, ]
+rse_gene <- rse_gene[, regInd]
+# rse_exon <- rse_exon[, region_idx]
+# rse_jxn <- rse_jxn[, region_idx]
+# rse_tx <- rse_tx[, region_idx]
+
+pd <- pd[regInd, ]
 mds <- mds[pd$BrNum, ]
 snp <- snp[, pd$BrNum]
 rownames(mds) <- colnames(snp) <- pd$RNum
-
+dim(snp)
+# [1] 1000000     540
 snpMap$maf <- rowSums(snp, na.rm = TRUE) / (2 * rowSums(!is.na(snp)))
 
 
@@ -56,12 +66,16 @@ snpMap$maf <- rowSums(snp, na.rm = TRUE) / (2 * rowSums(!is.na(snp)))
 ## load table ##
 ###############
 load("mergedEqtl_output_amyg_genomewide_4features_FDR01.rda", verbose = TRUE)
+table(allEqtlFDR01$Type)
+# Exon     Gene      Jxn       Tx 
+# 14692485  2338029  8931780  3198436 
 amyg <- allEqtlFDR01
 
 pd$PrimaryDx <- factor(pd$PrimaryDx,
     levels = c("Control", "Bipolar", "MDD")
 )
-mod <- model.matrix(~ PrimaryDx + Sex + as.matrix(mds[, 1:5]), data = pd)
+
+mod <- cbind(model.matrix(~ PrimaryDx + Sex , data = pd), as.matrix(mds[, 1:5]))
 
 #####################
 ## calculate rpkm ##
@@ -86,7 +100,7 @@ jExprs <- cleaningY(jExprs, mod, P = 1)
 tExprs <- log2(txTpm + 1)
 tExprs <- cleaningY(tExprs, mod, P = 1)
 
-
+exprsAdj <- gExprs
 exprsAdj <- rbind(gExprs, eExprs, jExprs, tExprs)
 amyg$Symbol <- as.character(amyg$Symbol)
 
@@ -99,7 +113,7 @@ amygT <- amyg[which(amyg$Type == "Tx"), ]
 ## plot ##
 #########
 message(Sys.time(), " Box plots")
-pdf("amyg_top_eqtl_adj.pdf", h = 6, w = 10)
+pdf(here("eqtl","genomewide","plots","amyg_top_eqtl_adj.pdf"), h = 6, w = 10)
 par(mfrow = c(2, 3), cex.main = 1.2, cex.lab = 1.2)
 palette(brewer.pal(8, "Spectral"))
 
@@ -108,6 +122,7 @@ for (i in 1:12) {
     symi[is.na(symi)] <- ""
     snpi <- amygG[i, "snps"]
     feati <- amygG[i, "gene"]
+    message(feati)
     p_i <- signif(amygG[i, "pvalue"], 3)
     typei <- amygG[i, "Type"]
 
@@ -122,6 +137,7 @@ for (i in 1:12) {
     )
     legend("top", paste0("p=", p_i))
 }
+dev.off()
 for (i in 1:12) {
     symi <- amygE[i, "Symbol"]
     symi[is.na(symi)] <- ""
