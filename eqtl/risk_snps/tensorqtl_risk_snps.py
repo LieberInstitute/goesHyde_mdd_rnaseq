ecb78133bcb00b42f9e6a848c969a6c2ce6c2a2b
lahuuki <lahuuki@gmail.com>
2021-09-16 17:01:59 -0400

    Try python script

---
+++ eqtl/risk_snps/tensorqtl_risk_snps.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import torch
+import tensorqtl
+from tensorqtl import genotypeio, cis, trans
+print(f'PyTorch {torch.__version__}')
+print(f'Pandas {pd.__version__}')
+
+# define paths to data
+plink_prefix_path = "LIBD_maf01_gwas_BPD"
+expression_bed = 'pheno_data/gene_Amygdala.bed.gz'
+covariates_file = 'pheno_data/covariates_gene_Amygdala.txt'
+prefix = 'gene_amygdala'
+
+# load phenotypes and covariates
+phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(expression_bed)
+covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T
+
+# PLINK reader for genotypes
+pr = genotypeio.PlinkReader(plink_prefix_path)
+genotype_df = pr.load_genotypes()
+variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
