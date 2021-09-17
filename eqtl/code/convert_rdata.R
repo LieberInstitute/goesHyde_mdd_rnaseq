30d5226ea893e1b8a513588ae78c7eeb7a5f210a
lahuuki <lahuuki@gmail.com>
2021-09-17 17:47:33 -0400

    Fix inputs formats

--- eqtl/code/convert_rdata.R
+++ eqtl/code/convert_rdata.R
@@ -1,10 +1,11 @@
 library(SummarizedExperiment)
 library(sessioninfo)
 library(tidyverse)
+library(VariantAnnotation)
 library(jaffelab)
 library(here)
 
-source(here("eqtl","risk_snps","rse_to_bed.R"))
+source(here("eqtl","code","rse_to_bed.R"))
 
 ## load
 load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
@@ -18,7 +19,7 @@ rse_gene_split <- map(regions, ~rse_gene[,rse_gene$BrainRegion == .x])
 
 #### Covariate Data ####
 # load(here("eqtl", "genomewide", "rdas", "pcs_4features_amyg.rda"), verbose = TRUE)
-load(here("eqtl", "pcs_4features_gene_temp.rda"), verbose = TRUE)
+load(here("eqtl", "data", "pcs_4features_gene_temp.rda"), verbose = TRUE)
 
 corner(genePCs)
 
@@ -32,7 +33,7 @@ covars <- map2(rse_gene_split, regions,  ~map(list(genePCs), function(pc){
   pc <- t(pc)
   pc <- as.data.frame(pc) %>% rownames_to_column("id")
   write.table(pc,
-              file= here("eqtl","risk_snps","pheno_data", paste0("covariates_gene_",.y,".txt")),
+              file= here("eqtl","data","covariates_txt", paste0("covariates_gene_",.y,".txt")),
               sep = "\t", quote = FALSE, row.names = FALSE)
   return(pc)
 }))
@@ -41,24 +42,39 @@ ss(rownames(genePCs),"_")[(ss(rownames(genePCs),"_") %in% rse_gene$RNum)]
 
 
 #### Create Phenotype Data ####
-fn_gene <- map(regions, ~paste0("pheno_data/gene_",.x,".bed"))
+fn_gene <- map(regions, ~here("eqtl","data", "expression_bed", paste0("gene_",.x,".bed")))
 
-walk2(rse_gene_split, fn_gene, function(rse,fn){
+bed <- map2(rse_gene_split, fn_gene, function(rse,fn){
   bed_gene <- rse_to_bed(rse)
   write.table(bed_gene, fn, sep = "\t", 
               quote = FALSE, row.names = FALSE)
+  return(bed_gene)
 })
 
 ## Create shell script to zip data
 commands <- map(fn_gene, ~paste0("bgzip ", .x," && tabix -p bed ", .x,".gz"))
+if(file.exists("bed_bgzip.sh")) file.remove("bed_bgzip.sh")
 sgejobs::job_single("bed_bgzip", create_shell = TRUE, queue= 'bluejay', memory = '100G', 
                     command = paste(commands, collapse = '\n'))
+## Add "module load htslib"
 
-sgejobs::job_single("tensorQTL_test", create_shell = TRUE, queue= 'bluejay', memory = '10G', 
-                    command = 
-                      "python3 -m tensorqtl LIBD_maf01_gwas_BPD pheno_data/gene_Amygdala.bed.gz gene_Amygdala \
-    --covariates pheno_data/covariates_gene_Amygdala.txt \
-    --mode cis")
+#### VCF ####
+risk_vcf <- readVcf(here("eqtl","data","risk_snps","LIBD_maf01_gwas_BPD.vcf.gz"))
+risk_vcf_split <- map(rse_gene_split, ~risk_vcf[,.x$genoSample])
+map(risk_vcf_split, dim)
+
+vcf_fn <- map(regions, ~here("eqtl","data","risk_snps",paste0("LIBD_maf01_gwas_BPD_",.x,".vcf.gz")))
+walk2(risk_vcf_split, vcf_fn, ~writeVcf(.x, .y))
+
+## plink commands
+map(vcf_fn, ~paste("plink --make-bed --output-chr chrM --vcf", .x, "--out", gsub(".vcf.gz","",.x)))
 
 
+## check 
 
+map2(bed, risk_vcf_split, ~all(colnames(.x[,5:ncol(.x)]) == colnames(.y)))
+map2(bed, covars, ~all(colnames(.x[,5:ncol(.x)]) == colnames(.y[[1]][,2:ncol(.y[[1]])])))
+
+
+vcf_test <- readVcf("../data/risk_snps/LIBD_maf01_gwas_BPD_Amygdala.vcf.gz")
+## create shell commands ##
--- eqtl/code/rse_to_bed.R
+++ eqtl/code/rse_to_bed.R
@@ -2,15 +2,17 @@
 rse_to_bed <- function(rse){
   rr_df <- as.data.frame(rowRanges(rse)) %>%
     dplyr::arrange(seqnames, start) %>%
-    dplyr::select(`#chr` = seqnames, left = start, right = end, phenotype_id = gencodeID, group_id = gene_type, strand)
+    dplyr::mutate(end = start + 1) %>%
+    dplyr::select(`#Chr` = seqnames, start, end, ID = gencodeID)
+    
   
     counts <- SummarizedExperiment::assays(rse)$counts
     colnames(counts) <- rse$genoSample
     # counts = round(counts, 3) ## round to 3 ?
     
     counts <- as.data.frame(counts) %>%
-      tibble::rownames_to_column("phenotype_id") %>%
-      dplyr::left_join(rr_df, ., by = "phenotype_id")
+      tibble::rownames_to_column("ID") %>%
+      dplyr::left_join(rr_df, ., by = "ID")
   
     return(counts)
 }
