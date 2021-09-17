ecae4ea2d51645de8d12cf3409df0b81eb54453a
lahuuki <lahuuki@gmail.com>
2021-09-17 17:48:39 -0400

    Get Amyg example working

--- eqtl/code/tensorqtl_risk_snps.py
+++ eqtl/code/tensorqtl_risk_snps.py
@@ -6,10 +6,10 @@ print(f'PyTorch {torch.__version__}')
 print(f'Pandas {pd.__version__}')
 
 # define paths to data
-plink_prefix_path = "LIBD_maf01_gwas_BPD"
-expression_bed = 'pheno_data/gene_Amygdala.bed.gz'
-covariates_file = 'pheno_data/covariates_gene_Amygdala.txt'
-prefix = 'gene_amygdala'
+plink_prefix_path = "../data/risk_snps/LIBD_maf01_gwas_BPD_Amygdala"
+expression_bed = '../data/expression_bed/gene_Amygdala.bed.gz'
+covariates_file = '../data/covariates_txt/covariates_gene_Amygdala.txt'
+prefix = '/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/eqtl/data/tensorQTL_out/Amyg_test_cis'
 
 # load phenotypes and covariates
 phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(expression_bed)
@@ -20,7 +20,27 @@ pr = genotypeio.PlinkReader(plink_prefix_path)
 genotype_df = pr.load_genotypes()
 variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
 
-cis.map_nominal(genotype_df, variant_df,
-                phenotype_df.loc[phenotype_pos_df['chr']=='chr18'],
-                phenotype_pos_df.loc[phenotype_pos_df['chr']=='chr18'],
-                prefix, covariates_df=covariates_df)
+covariates_df.shape
+genotype_df.shape
+phenotype_df.shape
+
+## Current fixes
+genotype_df.columns = phenotype_df.columns
+variant_df.chrom = "chr" + variant_df.chrom
+variant_df.chrom.value_counts()
+
+my_chrom = set(variant_df.chrom)
+chrom_filter = phenotype_pos_df.chr.isin(my_chrom)
+phenotype_pos_df.chr in my_chrom
+
+phenotype_df_filter = phenotype_df[chrom_filter]
+phenotype_pos_df_filter = phenotype_pos_df[chrom_filter]
+
+cis_df = cis.map_cis(genotype_df, variant_df, 
+                     phenotype_df_filter,
+                     phenotype_pos_df_filter,
+                     covariates_df=covariates_df,
+                     seed=9172021)
+
+cis_df.to_csv(prefix + ".csv")
+
