5ba2d14718c519173e4919765fb6771076838e9b
lahuuki <lahuuki@gmail.com>
2021-09-20 15:08:02 -0400

    Fix ID column to work with all features

--- eqtl/code/rse_to_bed.R
+++ eqtl/code/rse_to_bed.R
@@ -1,9 +1,10 @@
 
 rse_to_bed <- function(rse){
   rr_df <- as.data.frame(rowRanges(rse)) %>%
+    tibble::rownames_to_column("ID") %>%
     dplyr::arrange(seqnames, start) %>%
     dplyr::mutate(end = start + 1) %>%
-    dplyr::select(`#Chr` = seqnames, start, end, ID = gencodeID)
+    dplyr::select(`#Chr` = seqnames, start, end, ID)
     
   
     counts <- SummarizedExperiment::assays(rse)$counts
