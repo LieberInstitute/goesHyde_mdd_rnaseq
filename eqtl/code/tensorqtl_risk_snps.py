6cceb90cde0e6403984ffdd7e815c2e18c1ad4be
lahuuki <lahuuki@gmail.com>
2021-09-21 16:04:25 -0400

    Fix output prefix, do full run

--- eqtl/code/tensorqtl_risk_snps.py
+++ eqtl/code/tensorqtl_risk_snps.py
@@ -7,17 +7,15 @@ def get_input_paths(feature, region):
     plink_prefix_path = "../data/risk_snps/LIBD_maf01_gwas_BPD_" + region
     expression_bed = '../data/expression_bed/' + feature + '_' + region + '.bed.gz'
     covariates_file = '../data/covariates_txt/covariates_' + feature + '_' + region + '.txt'
-    prefix = '/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/eqtl/data/tensorQTL_out/' + feature + '_' + region + '_cis'
+    prefix = '/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/eqtl/data/tensorQTL_out/' + feature + '_' + region + '_cis_risk_bpd'
     return plink_prefix_path, expression_bed, covariates_file, prefix
 
 
 
-for feature in ["gene"]:
-## for feature in ["gene", "exon","jxn","tx"]:
-    for region in ["sACC"]:
-    ##for region in ["Amygdala", "sACC"]:
+
+for feature in ["gene", "exon","jxn","tx"]:
+    for region in ["Amygdala", "sACC"]:
          plink, expres, covar, prefix = get_input_paths(feature, region)
-         ##print(expres + " - " + str(os.path.exists(expres)))
          my_tensorqtl_run(plink, expres, covar, prefix)
 
 
