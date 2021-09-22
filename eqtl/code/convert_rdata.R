b42e1f117250a423790837d9c1b3137d082a83eb
lahuuki <lahuuki@gmail.com>
2021-09-22 17:14:01 -0400

    Save out cell fractions as csv, update covariates table #15

--- eqtl/code/convert_rdata.R
+++ eqtl/code/convert_rdata.R
@@ -23,26 +23,42 @@ samples_split <- map(rse_gene_split, colnames)
 
 #### Covariate Data ####
 load(here("eqtl", "data", "pcs_4features.Rdata"), verbose = TRUE)
+load(here("eqtl", "data", "pcs_4features_gene_temp.rda"), verbose = TRUE)
 corner(genePCs)
 dim(genePCs)
 
 all(rownames(genePCs) == colnames(rse_gene))
 
-covars <- map2(list(genePCs, exonPCs, jxnPCs, txPCs), features,
+covar_format <- function(data, rn){
+  data <- as.data.frame(data) 
+  rownames(data) <- rn
+  data <- t(data)
+  data <- as.data.frame(data) %>% rownames_to_column("id")
+  return(data)
+}
+
+# covars <- map2(list(genePCs, exonPCs, jxnPCs, txPCs), features,
+covars <- map2(list(gene = genePCs),features[1],
                 function(pc, feat){
                   map2(rse_gene_split, regions, function(rse, region){
                     message(paste(feat, region))
+                    ## Phenodata
+                    pd <- colData(rse)[,c("PrimaryDx", "Sex", paste0("snpPC", 1:5))]
+                    pd <- covar_format(pd, rse$genoSample)
+                    
+                    ## PC data
                     pc <- pc[colnames(rse),]
-                    rownames(pc) <- rse$genoSample
-                    pc <- t(pc)
-                    pc <- as.data.frame(pc) %>% rownames_to_column("id")
-                    write.table(pc,
+                    pc <- covar_format(pc, rse$genoSample)
+                    
+                    ##bind and save
+                    covars <- rbind(pd, pc)
+                    write.table(covars,
                                 file= here("eqtl","data","covariates_txt", paste0("covariates_",feat,"_",region,".txt")),
                                 sep = "\t", quote = FALSE, row.names = FALSE)
-                    return(pc)
+                    return(covars)
                   })
                 })
-
+# corner(covars$gene$amyg)
 
 
 #### Expression Data ####
@@ -85,9 +101,16 @@ map(vcf_fn, ~paste("plink --make-bed --output-chr chrM --vcf", .x, "--out", gsub
 map2(bed, risk_vcf_split, ~all(colnames(.x[,5:ncol(.x)]) == colnames(.y)))
 map2(bed, covars, ~all(colnames(.x[,5:ncol(.x)]) == colnames(.y[[1]][,2:ncol(.y[[1]])])))
 
-
 vcf_test <- readVcf("../data/risk_snps/LIBD_maf01_gwas_BPD_Amygdala.vcf.gz")
-## create shell commands ##
 
+## prep interaction csv
+walk2(rse_gene_split, regions, function(rse, region){
+  cell_fractions <- colData(rse)[,c("Astro","Endo","Macro","Micro","Mural","Oligo","OPC","Tcell","Excit","Inhib")]
+  cell_fractions <- as.data.frame(cell_fractions)
+  rownames(cell_fractions) <- rse$genoSample
+  write.csv(cell_fractions, file = here("eqtl","data","interaction",paste0("cell_fraction_",region,".csv")))
+})
+
+## create shell commands ##
 sgejobs::job_single("tensorqtl_risk_snps", create_shell = TRUE, queue= 'bluejay', memory = '50G', 
                     command = "python tensorqtl_risk_snps.py")
