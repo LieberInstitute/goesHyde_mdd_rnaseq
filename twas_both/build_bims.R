c9ec1c7c0b8fc08a52045dab0e5daa20f4c5e8e7
aseyedia <arta.seyedian@gmail.com>
2021-09-24 11:15:47 -0400

    changed BrNum to famKey, reformatted build_bims

--- twas_both/build_bims.R
+++ twas_both/build_bims.R
@@ -23,16 +23,38 @@ setDTthreads(threads = 1)
 ## Flags that are supplied with RScript
 ## Specify parameters
 ## Tissues: Amygdala, SACC
-spec <- matrix(c(
-    'region', 'r', 1, 'character', 'Either Amygdala or SACC',
-    'cores', 'c', 1, 'integer', 'Number of cores to use. Use a small number',
-	'help' , 'h', 0, 'logical', 'Display help'
-), byrow=TRUE, ncol=5)
+spec <- matrix(
+    c(
+        'region',
+        'r',
+        1,
+        'character',
+        'Either Amygdala or SACC',
+        'cores',
+        'c',
+        1,
+        'integer',
+        'Number of cores to use. Use a small number',
+        'help' ,
+        'h',
+        0,
+        'logical',
+        'Display help'
+    ),
+    byrow = TRUE,
+    ncol = 5
+)
 opt <- getopt(spec)
 
 ## For an interactive test
 if (FALSE) {
-    opt <- list('region' = 'sACC', "feature" = "gene", "cores" = 1, "test" = TRUE)
+    opt <-
+        list(
+            'region' = 'Amygdala',
+            "feature" = "gene",
+            "cores" = 1,
+            "test" = TRUE
+        )
 }
 
 # opt$region <- tolower(opt$region)
@@ -70,42 +92,55 @@ load_rse <- function(feat, reg) {
     message(paste(Sys.time(), "loading expression data"))
     
     # expmnt data
-    load(Sys.glob(here("twas_both", "filter_data", paste0(opt$region, "_rda"), "MDD_*_hg38_rseGene_rawCounts_allSamples_n*.Rdata")), verbose = TRUE)
+    load(Sys.glob(
+        here(
+            "twas_both",
+            "filter_data",
+            paste0(opt$region, "_rda"),
+            paste0(opt$region, "_hg38_rseGene_rawCounts_allSamples_n*.Rdata")
+        )
+    ), verbose = TRUE)
     ## Could be more complicated later on for exon, jxn, tx
     rse <- rse_gene
     assays(rse)$raw_expr <- assays(rse_gene)$RPKM
-
-    load(here("twas_both", "filter_data", paste0(opt$region, "_rda"), "genePCs.Rdata"), verbose = TRUE)
+    
+    load(here(
+        "twas_both",
+        "filter_data",
+        paste0(opt$region, "_rda"),
+        "genePCs.Rdata"
+    ),
+    verbose = TRUE)
     pcs <- genePCs
-
+    
     pd = colData(rse)
-
+    
     colnames(pd)
-
+    
     colData(rse) <- cbind(colData(rse), pcs)
     print(head(rse))
     
-    mod <- model.matrix(~PrimaryDx + Sex + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 + pcs,
-    	data = colData(rse))
+    mod <-
+        model.matrix( ~ PrimaryDx + Sex + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 + pcs,
+                      data = colData(rse))
     
     message(paste(Sys.time(), 'cleaning expression'))
-    assays(rse) <- List(
-        'raw_expr' = assays(rse)$raw_expr,
-        'clean_expr' = cleaningY(log2(assays(rse)$raw_expr + 1), mod, P = 3)
-    )
-
+    assays(rse) <- List('raw_expr' = assays(rse)$raw_expr,
+                        'clean_expr' = cleaningY(log2(assays(rse)$raw_expr + 1), mod, P = 3))
+    
     ## Regress out effects. If we had a diagnosis variable (Dx), we would use it
     ## first, then use P = 2 in cleaningY()
-
+    
     message(paste(Sys.time(), "switch column names to BrNum"))
     stopifnot(!any(duplicated(colnames(rse))))
     colnames(rse) <- colData(rse)$BrNum
-
+    
     return(rse)
-
+    
 }
 
-rse_file <- file.path(paste0(opt$region, "_", opt$feature), 'working_rse.RData')
+rse_file <-
+    file.path(paste0(opt$region, "_", opt$feature), 'working_rse.RData')
 
 if (file.exists(rse_file) == FALSE) {
     rse <- load_rse(opt$feature, opt$region)
@@ -127,9 +162,9 @@ bim_file <-
         "twas_both",
         "filter_data",
         paste0(opt$region, "_unique_snps_bim"),
-        paste0("LIBD_merged_h650_1M_Omni5M_Onmi2pt5_Macrogen_QuadsPlus_dropBrains_maf01_hwe6_geno10_hg38_filtered_", opt$region, "_MDD_uniqueSNPs")
+        paste0("mdd_bpd_maf01.rsid", opt$region, "_uniqueSNPs")
     )
-                
+
 message(paste(Sys.time(), "reading the bim file", bim_file))
 bim <- fread(
     paste0(bim_file, ".bim"),
@@ -137,18 +172,17 @@ bim <- fread(
 )
 
 bim$chr <- as.character(bim$chr)
-bim[chr == "23", ]$chr <- "X"
+bim[chr == "23",]$chr <- "X"
 
-bim_gr <- GRanges(
-    paste0("chr", bim$chr),
-    IRanges(bim$basepair, width = 1)
-)
+bim_gr <- GRanges(paste0("chr", bim$chr),
+                  IRanges(bim$basepair, width = 1))
 
-mcols(bim_gr) <- bim[, -c("chr", "basepair")]
+mcols(bim_gr) <- bim[,-c("chr", "basepair")]
 
 ## Based on http://gusevlab.org/projects/fusion/#computing-your-own-functional-weights
 ## they restrict to 500kb on each side
-rse_window <- resize(rowRanges(rse), width(rowRanges(rse)) + 500000 * 2, fix = "center")
+rse_window <-
+    resize(rowRanges(rse), width(rowRanges(rse)) + 500000 * 2, fix = "center")
 mcols(rse_window) <- NULL
 
 ## Number of SNPs per feature window
@@ -157,8 +191,8 @@ mcols(rse_window) <- NULL
 ## Keep only those feature windows with some SNPs nearby
 keep_feat <- which(countOverlaps(rse_window, bim_gr) > 0)
 message(paste(Sys.time(), "number of features kept:", length(keep_feat)))
-rse <- rse[keep_feat, ]
-rse_window <- rse_window[keep_feat, ]
+rse <- rse[keep_feat,]
+rse_window <- rse_window[keep_feat,]
 stopifnot(nrow(rse) == length(rse_window))
 
 message(Sys.time(), " number of genes per chr")
@@ -167,10 +201,15 @@ table(seqnames(rse))
 print("Final RSE feature dimensions:")
 print(dim(rse))
 
-rse_file_subset <- file.path(paste0(opt$region, "_", opt$feature),"subsetted_rse.RData")
+rse_file_subset <-
+    file.path(paste0(opt$region, "_", opt$feature), "subsetted_rse.RData")
 
 if (!file.exists(rse_file_subset)) {
-    message(paste(Sys.time(), "saving the subsetted rse file for later at", rse_file_subset))
+    message(paste(
+        Sys.time(),
+        "saving the subsetted rse file for later at",
+        rse_file_subset
+    ))
     save(rse, file = rse_file_subset)
 } else {
     message(paste(Sys.time(), "subsetted rse file already exists:", rse_file))
@@ -194,7 +233,9 @@ if (opt$test == TRUE) {
 
 ## For checking if the triplet of bim files exists
 check_bim <- function(x) {
-    all(file.exists(paste0(x, c(".fam", ".bed", ".bim"))))
+    all(file.exists(paste0(x, c(
+        ".fam", ".bed", ".bim"
+    ))))
 }
 
 ## For matching, due to complicated BrNums
@@ -220,7 +261,14 @@ print(length(i))
 print(length(i.names))
 
 ## Save the ids
-write.table(data.frame(i, i.names), file = "input_ids.txt", row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
+write.table(
+    data.frame(i, i.names),
+    file = "input_ids.txt",
+    row.names = FALSE,
+    col.names = FALSE,
+    sep = "\t",
+    quote = FALSE
+)
 # rm(b, q, feat_id, base, j, filt_fam, m)
 # setwd("NAc_gene/")
 # load("pre-bimFilesFxn.RData")
@@ -233,56 +281,95 @@ write.table(data.frame(i, i.names), file = "input_ids.txt", row.names = FALSE, c
 
 bim_files <- bpmapply(function(i, feat_id, clean = TRUE) {
     if (i == 1 || i %% 1000 == 0) {
-        message("*******************************************************************************")
-        message(paste(Sys.time(), "building bim file for i =", i, "corresponding to feature", feat_id))
+        message(
+            "*******************************************************************************"
+        )
+        message(
+            paste(
+                Sys.time(),
+                "building bim file for i =",
+                i,
+                "corresponding to feature",
+                feat_id
+            )
+        )
     }
-
+    
     base <- paste0(opt$region, "_", opt$feature, "_", i)
     filt_snp <- paste0("filtered_snps_", base, ".txt")
-
+    
     # change this file
     filt_bim <- gsub(".txt", "", filt_snp)
     filt_snp <- file.path("snp_files", filt_snp)
     dir.create(file.path("bim_files", base), showWarnings = FALSE)
     filt_bim <- file.path("bim_files", base, filt_bim)
-
+    
     ## Re-use the bim file if it exists already
-    if (check_bim(filt_bim)) {
-        return(TRUE)
-    }
-
+    # if (check_bim(filt_bim)) {
+    #     return(TRUE)
+    # }
+    # 
     j <- subjectHits(findOverlaps(rse_window[i], bim_gr))
-
-    fwrite(
-        bim[j, "snp"], # %>% unique()
-        file = filt_snp,
-        sep = "\t", col.names = FALSE
+    
+    fwrite(bim[j, "snp"],
+           # %>% unique()
+           file = filt_snp,
+           sep = "\t",
+           col.names = FALSE)
+    
+    system(
+        paste(
+            "plink --bfile",
+            bim_file,
+            "--extract",
+            filt_snp,
+            "--make-bed --out",
+            filt_bim,
+            "--memory 5000 --threads 1 --silent"
+        )
     )
-
-    system(paste(
-        "plink --bfile", bim_file, "--extract", filt_snp,
-        "--make-bed --out", filt_bim, "--memory 5000 --threads 1 --silent"
-    ))
-
+    
     ## Edit the "phenotype" column of the fam file
-    filt_fam <- fread(paste0(filt_bim, ".fam"),
-        col.names = c("famid", "w_famid", "w_famid_fa", "w_famid_mo", "sex_code", "phenotype")
+    filt_fam <- fread(
+        paste0(filt_bim, ".fam"),
+        col.names = c(
+            "famid",
+            "w_famid",
+            "w_famid_fa",
+            "w_famid_mo",
+            "sex_code",
+            "phenotype"
+        )
     )
-
+    
+    filt_fam$famKey <- paste0(filt_fam$famid, "_", filt_fam$w_famid)
+    
     ## Note BrNums might be duplicated, hence the use of match()
-    m <- match(brnumerical(filt_fam$famid), brnumerical(colData(rse)$BrNum))
+    m <-
+        match(filt_fam$famKey,
+              colData(rse)$genoSample)
     stopifnot(all(!is.na(m)))
-
+    
     ## Use cleaned expression for now. Could be an argument for the code.
     filt_fam$phenotype <- assays(rse)$clean_expr[i, m]
-
+    
     ## Ovewrite fam file (for the phenotype info)
-    fwrite(filt_fam, file = paste0(filt_bim, ".fam"), sep = " ", col.names = FALSE)
+    fwrite(
+        filt_fam,
+        file = paste0(filt_bim, ".fam"),
+        sep = " ",
+        col.names = FALSE
+    )
     ## Clean up
-    if (clean) unlink(filt_snp)
-
+    if (clean)
+        unlink(filt_snp)
+    
     return(check_bim(filt_bim))
-}, i, i.names, BPPARAM = MulticoreParam(workers = opt$cores), SIMPLIFY = FALSE)
+},
+i,
+i.names,
+BPPARAM = MulticoreParam(workers = opt$cores),
+SIMPLIFY = FALSE)
 
 message(paste(Sys.time(), "finished creating the bim files"))
 stopifnot(all(unlist(bim_files)))
