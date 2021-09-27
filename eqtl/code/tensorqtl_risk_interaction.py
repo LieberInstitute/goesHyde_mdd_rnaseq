50f1ef232cebc0c63c4a0ef7a2504fbc75ea0fdb
lahuuki <lahuuki@gmail.com>
2021-09-27 17:12:47 -0400

    Run tensorwtl with model.matrix style covars

--- eqtl/code/tensorqtl_risk_interaction.py
+++ eqtl/code/tensorqtl_risk_interaction.py
@@ -9,7 +9,7 @@ out_path = "../data/tensorQTL_out/nominal_bpd_risk/"
 for region in ["Amygdala", "sACC"]:
     plink, express, covar, prefix = my_tensorqtl_run.get_input_paths("gene", region)
     genotype_df, variant_df, phenotype_df_filter, phenotype_pos_df_filter, covariates_df = my_tensorqtl_run.load_data(plink, express, covar)
-    
+    covar = covar.replace(".txt","_mm.txt")
     cell_fraction = pd.read_csv("../data/interaction/cell_fraction_"+ region +".csv", index_col = 0)
     
     for cell_type in cell_fraction.columns:
@@ -18,10 +18,10 @@ for region in ["Amygdala", "sACC"]:
         ##out_path_ct = out_path + tag + "/"
         cf_interaction = pd.Series(cell_fraction[cell_type])
         
-        nominal_test = cis.map_nominal(genotype_df, variant_df, phenotype_df_filter, phenotype_pos_df_filter,
+        nominal_out = cis.map_nominal(genotype_df, variant_df, phenotype_df_filter, phenotype_pos_df_filter,
         prefix = tag, covariates_df=covariates_df, output_dir= out_path + "parquet_data/", 
         interaction_s = cf_interaction, maf_threshold_interaction=0, group_s=None, window=500000, 
         run_eigenmt=True, write_top = False)
         
-        nominal_test.to_csv('../data/tensorQTL_out/nominal_bpd_risk/' + tag + ".csv")
-
+        ##nominal_out.to_csv('../data/tensorQTL_out/nominal_bpd_risk/' + tag + ".csv")
+        nominal_out.to_csv('../data/tensorQTL_out/nominal_bpd_risk/' + tag + "_mm.csv")
