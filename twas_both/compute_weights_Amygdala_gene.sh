3fffada89900362543e6ab42d77476452ea6b5c3
aseyedia <arta.seyedian@gmail.com>
2021-09-29 14:49:38 -0400

    fixed compute_weights to show job scheduler data

--- twas_both/compute_weights_Amygdala_gene.sh
+++ twas_both/compute_weights_Amygdala_gene.sh
@@ -9,11 +9,11 @@
 echo "**** Job starts ****"
 date
 echo "**** JHPCE info ****"
-echo "User: \${USER}"
-echo "Job id: \${JOB_ID}"
-echo "Job name: \${JOB_NAME}"
-echo "Hostname: \${HOSTNAME}"
-echo "Task id: \${TASK_ID}"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${TASK_ID}"
 
 ## Load dependencies
 module load plink/1.90b6.6
