d08271d3b4d47968ae174c2d22247f5f0e4d452d
lahuuki <lahuuki@gmail.com>
2021-10-19 11:25:31 -0400

    Add vcf subset script

---
+++ eqtl/code/subset_risk_snps2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=12G,h_vmem=12G,h_fsize=200G
+#$ -N subset_signif_snps
+#$ -o logs/subset_signif_snps2.txt
+#$ -e logs/subset_signif_snps2.txt
+
+
+module load bcftools
+
+
+
+## Locate file and ids
+
+bcftools view --include 'RS=@../data/risk_snps/MDD_risk.txt' /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/mdd_bpd/maf01/mdd_bpd_01.vcf.gz > ../data/risk_snps/LIBD_maf01_gwas_MDD.vcf.gz
+
