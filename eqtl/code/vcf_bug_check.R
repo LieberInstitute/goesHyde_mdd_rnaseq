7ff221fdc6c0304add4ece95e038c70e019b94b4
lahuuki <lahuuki@gmail.com>
2021-10-21 17:16:00 -0400

    Create list of snp IDs for VCF subset

--- eqtl/code/vcf_bug_check.R
+++ eqtl/code/vcf_bug_check.R
@@ -44,3 +44,23 @@ dim(risk_vcf_good)
 writeVcf(risk_vcf_good ,here("eqtl","data","risk_snps","LIBD_maf01_gwas_BPD.vcf"))
 # module load htslib
 # bgzip
+
+#### MDD risk vcf ####
+
+## table has GRCh38 annotations get SNPids to subset
+mdd_snps <- read.delim(here("eqtl", "data", "risk_snps", "PGC_depression_genome-wide_significant_makers.txt"))
+head(mdd_snps)
+# chr       bp markername a1 a2   freq   logor stderrlogor         p
+# 1   1 17795514  rs4141983  T  C 0.6740  0.0264      0.0046 9.692e-09
+# 2   1 49062731 rs17105472  A  C 0.0960 -0.0409      0.0073 2.355e-08
+# 3   1 49088804   rs589436  T  C 0.0960 -0.0409      0.0073 2.333e-08
+# 4   1 49097909   rs655065  T  C 0.0963 -0.0412      0.0073 1.708e-08
+# 5   1 49115334   rs590503  T  C 0.9037  0.0416      0.0073 1.321e-08
+# 6   1 49209604   rs354155  C  G 0.0923 -0.0449      0.0075 1.751e-09
+
+mdd_snps <- mdd_snps %>%
+  mutate(snpID = paste0("chr",chr,":" ,bp, ":",a1, ":",a2))
+
+cat(mdd_snps$snpID, file = here("eqtl", "data", "risk_snps","MDD_risk_snps.txt"), sep = "\n")
+
+
