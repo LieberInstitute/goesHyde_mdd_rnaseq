52688a2651a4dbd5c121749a077e8f1a5ca69c17
lahuuki <lahuuki@gmail.com>
2021-10-21 17:14:36 -0400

    Fix MDD risk VCF subset

--- eqtl/code/subset_risk_snps2.sh
+++ eqtl/code/subset_risk_snps2.sh
@@ -12,5 +12,5 @@ module load bcftools
 
 ## Locate file and ids
 
-bcftools view --include 'RS=@../data/risk_snps/MDD_risk.txt' /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/mdd_bpd/maf01/mdd_bpd_01.vcf.gz > ../data/risk_snps/LIBD_maf01_gwas_MDD.vcf.gz
+bcftools view --include 'ID=@../data/risk_snps/MDD_risk_snps.txt' /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/mdd_bpd/maf01/mdd_bpd_01.vcf.gz > ../data/risk_snps/LIBD_maf01_gwas_MDD.vcf.gz
 
