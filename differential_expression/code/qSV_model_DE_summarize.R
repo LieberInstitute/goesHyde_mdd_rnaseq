38b937af6c82ad7c1e1873860b75882b878a81f1
lahuuki <lahuuki@gmail.com>
2021-11-01 16:33:59 -0400

    Summarize significant DE results

---
+++ differential_expression/code/qSV_model_DE_summarize.R
@@ -0,0 +1,64 @@
+library(jaffelab)
+library(SummarizedExperiment)
+library(VennDiagram)
+library(tidyverse)
+library(sessioninfo)
+library(here)
+library(EnhancedVolcano)
+
+#### Load Data ####
+data_type <- c("gene","exon","jxn","tx")
+names(data_type) <- data_type
+
+paths <- map(data_type, ~here("differential_expression","data",paste0("qSVA_MDD_", .x, "_DEresults.rda")))
+
+load(paths$gene, verbose = TRUE)
+load(paths$exon, verbose = TRUE)
+load(paths$jxn, verbose = TRUE)
+load(paths$tx, verbose = TRUE)
+
+#### Summarize Counts ####
+#model, region, Dx
+summarize_DE <- function(outFeature, feature_name){
+  feature_df <- map_dfr(outFeature, function(outModel){
+    model_df <- map_dfr(outModel, function(outRegion){
+      region_df <- map_dfr(outRegion, ~sum(.x$adj.P.Val < 0.05, na.rm = TRUE))
+    })
+    model_df$region <- names(outModel)
+    return(model_df)
+  })
+  feature_df$model <- rep(names(outFeature), each = 2)
+  feature_df <- feature_df %>%
+    pivot_longer(!c(region, model), names_to = "Dx", values_to =  feature_name)
+  
+  return(feature_df)
+  }
+
+model_counts <- summarize_DE(outGene, "Gene")  %>%
+  left_join(summarize_DE(outExon, "Exon"))%>%
+  left_join(summarize_DE(outJxn, "Jxn"))%>%
+  left_join(summarize_DE(outTx, "Tx"))
+
+# region model  Dx     Gene  Exon   Jxn    Tx
+# <chr>  <chr>  <chr> <int> <int> <int> <int>
+# 1 amyg   sep    MDD       3     4    47    66
+# 2 amyg   sep    BPD      82   159    23     7
+# 3 sacc   sep    MDD     392   430   470    43
+# 4 sacc   sep    BPD     326   119    34    18
+# 5 amyg   sep_cf MDD       4     0    47     9
+# 6 amyg   sep_cf BPD      12    44    16     0
+# 7 sacc   sep_cf MDD      95    20   409    26
+# 8 sacc   sep_cf BPD      21     5    37     1
+ 
+write_csv(model_counts, here("differential_expression","data","model_counts.csv"))
+
+##### Overlap Between Models ####
+
+#### Volcano Plots ####
+
+pdf(here("differential_expression","plots","qSV_model_volcano.pdf"))
+walk2(outGene, names(outGene))
+
+
+
+
