bd8223dee6ce79bd66b7e37bd37261369db8febb
lahuuki <lahuuki@gmail.com>
2021-11-09 15:37:24 -0500

    Add model term boxplot exploration

---
+++ differential_expression/code/model_term_exploration.R
@@ -0,0 +1,27 @@
+library(SummarizedExperiment)
+library(tidyverse)
+library(here)
+library(sessioninfo)
+
+load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
+load(here("data", "MDD_colors.Rdata"), verbose = TRUE)
+
+mdd_Sex_colors
+
+pd <- as.data.frame(colData(rse_gene))
+pd_long <- pd %>% select(PrimaryDx, Experiment, BrainRegion, 
+                         #AgeDeath,  snpPC1 ,snpPC2, snpPC3, snpPC4, snpPC5,
+                           mitoRate, rRNA_rate,totalAssignedGene, RIN, ERCCsumLogErr) %>%
+  mutate(ERCCsumLogErr_abs = abs(ERCCsumLogErr)) %>%
+  pivot_longer(!c(PrimaryDx, Experiment, BrainRegion), names_to = "Metric")
+
+
+qc_boxplots <- ggplot(pd_long, aes(x = BrainRegion, y = value, fill = PrimaryDx, color = Experiment)) +
+  geom_boxplot() +
+  facet_wrap(~Metric, scales = "free_y", nrow = 2) +
+  scale_color_manual(values = c(psychENCODE_MDD = "black", psychENCODE_BP = "skyblue")) +
+  scale_fill_manual(values = mdd_Dx_colors) +
+  theme_bw() +
+  theme(text = element_text(size=15)) 
+
+ggsave(qc_boxplots, file = here("differential_expression","plots","model_terms_boxplots.png"), width = 15)
