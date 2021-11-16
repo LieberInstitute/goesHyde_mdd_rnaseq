0ecf65759924529e9c157f1c6aa5d695e7608620
lahuuki <lahuuki@gmail.com>
2021-11-16 14:57:25 -0500

    Filter to top 1500, update signif snps, add sh + reproducibility info

--- eqtl/code/summarize_genomewide_eqtl.R
+++ eqtl/code/summarize_genomewide_eqtl.R
@@ -30,7 +30,8 @@ regions <- c(amyg = "Amygdala", sacc = "sACC")
 
 #### genomewide results ####
 ## load tables
-eqtl_tensor <- map(features[1], function(f){
+eqtl_tensor <- map2(features, names(features), function(f, n){
+    message(n , " ", Sys.time())
     eqtl <- map(c(amyg = "amyg", sacc = "sacc"), 
                 ~ read.csv(here("eqtl", "data", "tensorQTL_out", "cis_genomewide_nominal", paste0(f, "_", .x, "_FDR01.csv"))) %>%
     rename(gencodeID = phenotype_id))
@@ -40,9 +41,22 @@ eqtl_tensor <- map(features[1], function(f){
 head(eqtl_tensor$gene$amyg)
 summary(eqtl_tensor$gene$amyg$tss_distance)
 summary(eqtl_tensor$gene$amyg$FDR)
-summary(eqtl_tensor$tx$sacc$tss_distance)
 
-map_depth(eqtl_tensor, 2, nrow)
+t(as.data.frame(map_depth(eqtl_tensor, 2, nrow)))
+# gene.amyg gene.sacc exon.amyg exon.sacc jxn.amyg jxn.sacc tx.amyg tx.sacc
+#    625776    649396   5599118   5449203  7422175  7935775 1365378 1444336
+
+## most significant
+eqtl_tensor_top <- map_depth(eqtl_tensor, 2, ~.x %>% arrange(FDR) %>% head(1500))
+
+
+## top 1500
+# significant_snps <- map_depth(eqtl_tensor_top, 2, variant_id)
+# significant_snps <- unique(unlist(significant_snps))
+# length(significant_snps) #[1] 5765
+# significant_snps <- significant_snps[order(significant_snps)]
+# cat(significant_snps, sep = "\n", file = here("eqtl", "data", "signif_snps",'significant_snps.txt'))
+# ## run subset_signif_snps.sh 
 
 #### get residual expression ####
 ## build model
@@ -50,7 +64,8 @@ pd <- colData(rse_gene) %>% as.data.frame()
 mod <- model.matrix(~ PrimaryDx + Sex + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5, data = pd)
 colnames(mod)
 
-resid_expres_split <- map(c(gene = rse_gene, exon = rse_exon, jxn = rse_jxn, tx = rse_tx)[1], function(rse) map(splitit(rse$BrainRegion), ~ get_resid_expres(rse[, .x], mod[.x, ])))
+# resid_expres_split <- map(c(gene = rse_gene), function(rse) map(splitit(rse$BrainRegion), ~ get_resid_expres(rse[, .x], mod[.x, ])))
+resid_expres_split <- map(c(gene = rse_gene, exon = rse_exon, jxn = rse_jxn, tx = rse_tx), function(rse) map(splitit(rse$BrainRegion), ~ get_resid_expres(rse[, .x], mod[.x, ])))
 map_depth(resid_expres_split, 2, dim)
 
 resid_expres_split_long <- map_depth(resid_expres_split, 2, 
@@ -69,15 +84,16 @@ head(genoDx)
 
 #### Load VCF ####
 signif_vcf <- readVcf(here("eqtl", "data", "signif_snps", "LIBD_Brain_merged_maf_005_topmed_051120_mdd_signif.vcf.gz"))
+dim(signif_vcf)
 
+## connect variant id to rs number
 rs_id <- info(signif_vcf) %>%
     as.data.frame() %>%
     dplyr::select(RS) %>%
     mutate(RS = paste0("rs", RS)) %>%
     rownames_to_column("variant_id")
 
-head(rs_id)
-
+## get genotype for samples in long format
 geno_long <- as.data.frame(geno(signif_vcf)$GT) %>%
     rownames_to_column("variant_id") %>%
     pivot_longer(!variant_id, names_to = "genoSample", values_to = "Genotype") %>%
@@ -91,17 +107,24 @@ geno_long <- as.data.frame(geno(signif_vcf)$GT) %>%
     )
 
 #### Filter and Annotate eQTL ####
-names(eqtl_tensor)
-eqtl_tensor_anno <- map_depth(eqtl_tensor, 2, function(e) {
+test <- eqtl_tensor_top$gene$amyg %>%
+    left_join(rs_id)
+test %>% count(is.na(RS))
+
+
+eqtl_tensor_top$gene$amyg %>% count(gencodeID) %>% arrange(n) %>% left_join(rd)
+
+## annotation for boxplots - order eqtl pairs by FDR
+eqtl_tensor_anno <- map_depth(eqtl_tensor_top, 2, function(e) {
     e_anno <- e %>%
         arrange(FDR)  %>%
-        left_join(rd) %>%
-        left_join(rs_id) %>%
+        left_join(rd) %>% ## add gene symbol
+        left_join(rs_id) %>% ## add rs #
         mutate(
             eqtl = paste0(str_pad(row_number(), 4), ". ", gencodeID, " - ", variant_id),
-            eqtl_anno = paste("Gene:", Symbol, "\nSNP:", RS, "\nFDR= ", scales::scientific(FDR, didgits = 3))
+            eqtl_anno = paste("\n Gene:", Symbol, "\n SNP:", RS, "\n FDR= ", scales::scientific(FDR, didgits = 3))
         ) %>%
-        slice(c(1:10, 501:510, 1001:1010))
+        slice(c(1:50, 201:250, 401:450, 601:650, 801:850, 1001:1050))
 
     e_anno$eqtl <- factor(e_anno$eqtl)
     e_anno$eqtl <- fct_reorder(e_anno$eqtl, e_anno$FDR, min)
@@ -110,8 +133,8 @@ eqtl_tensor_anno <- map_depth(eqtl_tensor, 2, function(e) {
 })
 map_depth(eqtl_tensor_anno, 2, head)
 map_depth(eqtl_tensor_anno, 2, nrow)
-eqtl_tensor_anno$gene$amyg
 
+## merge genotype and expression data for boxplots
 express_geno <- map2(
     eqtl_tensor_anno, resid_expres_split_long, function(eqtl, expres){
         map2(eqtl, expres, ~ .x %>%
@@ -121,33 +144,8 @@ express_geno <- map2(
     }
 )
 
-eqtl_tensor_anno$gene$amyg %>% head()
-resid_expres_split_long$gene$Amygdala %>% head()
-
-map_depth(express_geno_cf,2, head)
-express_geno_cf$gene$amyg %>%
-    count(eqtl)
-
-genomewide_box <- map2(
-    express_geno_cf, eqtl_tensor_anno, function(express_geno, anno){
-        map2(express_geno, anno, ~ ggplot(.x, aes(x = Genotype, y = expression)) +
-                 geom_boxplot(aes(fill = PrimaryDx)) +
-                 facet_wrap(~eqtl, nrow = 2) +
-                 scale_fill_manual(values = mdd_Dx_colors) +
-                 geom_text(
-                     data = .y,
-                     aes(label = eqtl_anno),
-                     x = -Inf, y = Inf, vjust = "inward", hjust = "inward", size = 3, nudge_y = -0.1
-                 ) +
-                 theme_bw() +
-                 coord_cartesian(clip = "off"))
-    }
-)
-
-walk2(genomewide_box, names(genomewide_box), ~ ggsave(.x, filename = here("eqtl", "plots", paste0("gene_genomewide_boxplot_", .y, ".png")), width = 12))
-
 pwalk(
-    list(expres = express_geno_cf, anno = eqtl_tensor_anno, feat_name = names(express_geno_cf)), function(expres, anno, feat_name){
+    list(expres = express_geno, anno = eqtl_tensor_anno, feat_name = names(express_geno)), function(expres, anno, feat_name){
         pwalk(
             list(expres2 = expres, anno2 = anno, region_name = names(expres)), function(expres2, anno2, region_name){
                 label = paste0(feat_name, "_", region_name)
@@ -159,27 +157,31 @@ pwalk(
                     geom_text(
                         data = anno2,
                         aes(label = eqtl_anno),
-                        x = -Inf, y = Inf, vjust = "inward", hjust = "inward", size = 3, nudge_y = -0.1
+                        x = -Inf, y = Inf, vjust = "inward", hjust = "inward" , size = 3
                     ) +
                     theme_bw() +
                     coord_cartesian(clip = "off")
         
         required_n_pages <- n_pages(eqtl_box + facet_wrap_paginate(~ eqtl, scales = "free", nrow = 2, ncol = 2))
-        message(required_n_pages)
+        message(paste("Plotting", required_n_pages, "pages"))
         
         pdf(here("eqtl", "plots", paste0("eqtl_genomewide_", label, ".pdf")), width = 10)
         for (i in 1:required_n_pages) {
             print( eqtl_box +
                       facet_wrap_paginate(~ eqtl, scales = "free", nrow = 2, ncol = 2, page = i)
-                  # +
-                  #     geom_text(
-                  #         data = anno2,
-                  #         aes(label = eqtl_anno), x = -Inf, y = Inf, vjust = "inward", hjust = "inward", size = 3
-                  #     )
                   )
         }
         dev.off()
         
             })
 })
+
+
+#sgejobs::job_single('summarize_genomewide_eqtl', create_shell = TRUE, memory = '100G', command = "Rscript summarize_genomewide_eqtl.R")
+## Reproducibility information
+print('Reproducibility information:')
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
 
