b8eeaf4e3dc66ffc8ae188def63979d51bcb37e3
lahuuki <lahuuki@gmail.com>
2021-11-22 14:03:43 -0500

    Explore qSVs over Experiment

--- differential_expression/code/model_term_exploration.R
+++ differential_expression/code/model_term_exploration.R
@@ -5,8 +5,8 @@ library(sessioninfo)
 
 load(here("exprs_cutoff", "rse_gene.Rdata"), verbose = TRUE)
 load(here("data", "MDD_colors.Rdata"), verbose = TRUE)
+load(here("differential_expression","data","qSV_mat.Rdata"), verbose = TRUE)
 
-mdd_Sex_colors
 
 pd <- as.data.frame(colData(rse_gene))
 pd_long <- pd %>% select(PrimaryDx, Experiment, BrainRegion, 
@@ -25,3 +25,22 @@ qc_boxplots <- ggplot(pd_long, aes(x = BrainRegion, y = value, fill = PrimaryDx,
   theme(text = element_text(size=15)) 
 
 ggsave(qc_boxplots, file = here("differential_expression","plots","model_terms_boxplots.png"), width = 15)
+
+#### qSV plots ####
+qSV_long <- pd %>% select(PrimaryDx, Experiment, BrainRegion) %>%
+  cbind(qSV_mat) %>%
+  pivot_longer(!c(PrimaryDx, Experiment, BrainRegion), names_to = "qSV")
+
+qSV_long$qSV <- factor(qSV_long$qSV, levels = paste0("PC", 1:26))
+levels(qSV_long$qSV)
+
+qSV_boxplots <- ggplot(qSV_long, aes(x = BrainRegion, y = value, fill = PrimaryDx, color = Experiment)) +
+  geom_boxplot() +
+  facet_wrap(~qSV, scales = "free_y", nrow = 3) +
+  scale_color_manual(values = c(psychENCODE_MDD = "black", psychENCODE_BP = "skyblue")) +
+  scale_fill_manual(values = mdd_Dx_colors) +
+  theme_bw() +
+  theme(text = element_text(size=15), legend.position="top") 
+
+ggsave(qSV_boxplots, file = here("differential_expression","plots","model_qSV_boxplots.png"), width = 20, height = 15)
+
