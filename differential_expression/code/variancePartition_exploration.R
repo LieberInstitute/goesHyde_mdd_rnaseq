6a3b6398e35b47efc250523d2c0dcc2aa2dd8813
lahuuki <lahuuki@gmail.com>
2021-12-22 16:59:46 -0500

    Add qSV terms to form

--- differential_expression/code/variancePartition_exploration.R
+++ differential_expression/code/variancePartition_exploration.R
@@ -15,7 +15,8 @@ test_dx = ss(args,"_",2)
 message(paste("Running", test_dx, "vs. Control -", test_region))
 #load objects
 load(here('exprs_cutoff','rse_gene.Rdata'), verbose=TRUE)
-
+load(here("differential_expression","data","qSV_mat.Rdata"), verbose = TRUE)
+cbind(colData(rse_gene),qSV_mat)
 #### Var Partition with cell type props ####
 message("Subset data")
 rse_gene <- rse_gene[,rse_gene$BrainRegion == test_region & rse_gene$PrimaryDx %in% c("Control", test_dx)]
@@ -26,14 +27,25 @@ mod <- model.matrix(~PrimaryDx, pd)
 gExpr <- calcNormFactors(rse_gene)
 vobjGenes <- voom(gExpr, mod)
 
+# linear mixed model where all categorical variables are modeled as random
+# effects and all continuous variables are fixed effects. The function lmer
+# from lme4 is used to fit this model.
+
 form = list(Sep = ~(1|PrimaryDx) + AgeDeath + (1|Sex) + 
               snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
-              mitoRate + rRNA_rate + totalAssignedGene + RIN + abs(ERCCsumLogErr),
+              mitoRate + rRNA_rate + totalAssignedGene + RIN + abs(ERCCsumLogErr) +
+              PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + PC11 + 
+              PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + 
+              PC21 + PC22 + PC23 + PC24 + PC25 + PC26,
             
             Sep_cf = ~(1|PrimaryDx) + AgeDeath + (1|Sex) + 
               snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5 +
               mitoRate + rRNA_rate + totalAssignedGene + RIN + abs(ERCCsumLogErr) +
-              Astro + Endo + Macro + Micro + Mural + Oligo + OPC + Tcell + Excit)
+              Astro + Endo + Macro + Micro + Mural + Oligo + OPC + Tcell + Excit +
+              PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10 + PC11 + 
+              PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + 
+              PC21 + PC22 + PC23 + PC24 + PC25 + PC26)
+
 
 
 message("VarPart Amyg")
