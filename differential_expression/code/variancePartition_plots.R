76ef0bfaa15192a96b53d6f89d94bcbcdbe7c530
lahuuki <lahuuki@gmail.com>
2022-01-05 15:20:41 -0500

    Use mget to load data, plot varPart violins to pdf, experiment with tile plot summary

--- differential_expression/code/variancePartition_plots.R
+++ differential_expression/code/variancePartition_plots.R
@@ -6,42 +6,78 @@ library(here)
 
 ## Load and sort data
 files <- list.files(path = here("differential_expression","data","variance_partition"), full.names = TRUE)
-# results <- sapply(files, function(x) mget(load(x)), simplify = TRUE) 
+varPart <- lapply(files, function(x) mget(load(x, verbose = TRUE)))
+varPart <- map(varPart, ~pluck(.x, 1))
+names(varPart) <- gsub(".Rdata","",gsub("varPart_","",basename(files)))
 
-## for now
-load(files[[4]], verbose = TRUE)
-varPartSort <- map(varPart, sortCols)
-map(varPartSort, colnames)
+## Sort columns
+varPartSort <- map_depth(varPart, 2, sortCols)
+map_depth(varPartSort, 2, colnames)
 
 ## Violin Plots
-varPart_violin <- map2(varPartSort, names(varPartSort), ~plotVarPart(.x) + labs(title = paste("Model -",.y)))
-
-map2(varPart_violin, names(varPart_violin), 
-     ~ggsave(.x, 
-             filename = here("differential_expression","plots",
-                             paste0("variancePartition_",.y,".png")), 
-             width = 10)
-     )
+varPart_violin <- map2(varPartSort, names(varPartSort),  
+                       function(data, data_name){
+                         map2(data, names(data), ~plotVarPart(.x) +
+                                labs(title = data_name,
+                                     subtitle = paste("Model -",.y)) + 
+                                theme(axis.text.x = element_text(angle = 90)))
+                       })
 
 pdf(here("differential_expression","plots", "variancePartition.pdf"))
-walk(varPart_violin, print)
+map_depth(varPart_violin,2, print)
 dev.off()
+# 
+# ## Density Plots
+# varPartLong <- map(varPartSort, ~.x %>% 
+#   as.data.frame() %>%
+#   rownames_to_column("Gene") %>% 
+#   pivot_longer(!Gene, names_to = "Term", values_to = "Variance"))
+# 
+# varPart_density <- map(varPartLong, ~ggplot(data = .x, aes(x = Variance, color = Term)) +
+#                          geom_density() +
+#                          )
+# 
+# map2(varPart_density, names(varPart_density), 
+#      ~ggsave(.x, 
+#              filename = here("differential_expression","plots",
+#                              paste0("variancePartition_density_",.y,".png")), 
+#              width = 10)
+# )
 
-## Density Plots
-varPartLong <- map(varPartSort, ~.x %>% 
-  as.data.frame() %>%
-  rownames_to_column("Gene") %>% 
-  pivot_longer(!Gene, names_to = "Term", values_to = "Variance"))
+## Compare Mean Variance 
+varPart_t <- transpose(varPart)
+names(varPart_t)
 
-varPart_density <- map(varPartLong, ~ggplot(data = .x, aes(x = Variance, color = Term)) +
-                         geom_density() +
-                         )
+meanVar <- map(varPart_t, function(vp){
+  
+  meanVar <- do.call("cbind", map(vp, ~colMeans(.x))) %>%
+    as.data.frame() %>%
+    rownames_to_column("term") %>%
+    pivot_longer(!term, values_to = "meanVar", names_to = "DE_analysis")
+  
+  term_order <- meanVar %>% 
+    group_by(term) %>% 
+    summarize(max = max(meanVar)) %>%
+    arrange(-max) %>%
+    pull(term)
+  
+  meanVar$term <- factor(meanVar$term, levels = term_order)
+  return(meanVar)
+})
 
-map2(varPart_density, names(varPart_density), 
-     ~ggsave(.x, 
-             filename = here("differential_expression","plots",
-                             paste0("variancePartition_density_",.y,".png")), 
-             width = 10)
-)
+## tile plots 
+walk2(meanVar, names(meanVar), function(mV, name){
+  
+  meanVar_tile <- mV %>% 
+    filter(term != "Residuals") %>%
+    ggplot(aes(x = DE_analysis , y = term, fill = meanVar)) +
+    geom_tile()  +
+    scale_fill_continuous(type = "viridis") +
+    labs(title = paste("Model -", name))
+  
+  ggsave(meanVar_tile, filename = here("differential_expression","plots", paste0("meanVar_tile_",name,".png")))
+  
+})
 
+map(meanVar, ~.x %>% filter(term == "Residuals"))
 
