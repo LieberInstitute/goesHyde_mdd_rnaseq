1f4c66f6559b63ce773591968d55266af9c48688
lahuuki <lahuuki@gmail.com>
2022-01-14 13:39:39 -0500

    Run GO for region x Dx, unique genes from all features

--- differential_expression/code/GO_enrichment.R
+++ differential_expression/code/GO_enrichment.R
@@ -9,36 +9,37 @@ library(clusterProfiler)
 library(purrr)
 library(here)
 
-#load objects
-load(here('exprs_cutoff','rse_gene.Rdata'), verbose=TRUE)
-load(here('exprs_cutoff','rse_exon.Rdata'), verbose=TRUE)
-load(here('exprs_cutoff','rse_jxn.Rdata'), verbose=TRUE)
-load(here('exprs_cutoff','rse_tx.Rdata'), verbose=TRUE)
+# #load objects
+# load(here('exprs_cutoff','rse_gene.Rdata'), verbose=TRUE)
+# load(here('exprs_cutoff','rse_exon.Rdata'), verbose=TRUE)
+# load(here('exprs_cutoff','rse_jxn.Rdata'), verbose=TRUE)
+# load(here('exprs_cutoff','rse_tx.Rdata'), verbose=TRUE)
+# 
+# #### Build Expression Map ####
+# ## modify rowRanges
+# rowRanges(rse_gene)$Type <- "Gene"
+# rowRanges(rse_exon)$Type <- "Exon"
+# 
+# # jxn "newGeneID"     "newGeneSymbol"
+# colnames(mcols(rowRanges(rse_jxn)))[13:14] <- c("gencodeID", "Symbol")
+# rowRanges(rse_jxn)$Type <- "Junction"
+# 
+# # Exon "gene_id"   "gene_name"
+# colnames(mcols(rowRanges(rse_tx)))[c(5,8)] <- c("gencodeID", "Symbol")
+# rowRanges(rse_tx)$Class <- "InGen"
+# rowRanges(rse_tx)$Type <- "Transcript"
+# 
+# ## Expression Map
+# name <- c("gencodeID", "Symbol", "Type", "Class")
+# exprsMap <- rbind(as.data.frame(rowRanges(rse_gene))[,name],
+#                  as.data.frame(rowRanges(rse_exon))[,name],
+#                  as.data.frame(rowRanges(rse_jxn))[,name],
+#                  as.data.frame(rowRanges(rse_tx))[,name])
+# exprsMap <- DataFrame(exprsMap)
+# 
+# nrow(exprsMap)
+# # [1] 810029
 
-#### Build Expression Map ####
-## modify rowRanges
-rowRanges(rse_gene)$Type <- "Gene"
-rowRanges(rse_exon)$Type <- "Exon"
-
-# jxn "newGeneID"     "newGeneSymbol"
-colnames(mcols(rowRanges(rse_jxn)))[13:14] <- c("gencodeID", "Symbol")
-rowRanges(rse_jxn)$Type <- "Junction"
-
-# Exon "gene_id"   "gene_name"
-colnames(mcols(rowRanges(rse_tx)))[c(5,8)] <- c("gencodeID", "Symbol")
-rowRanges(rse_tx)$Class <- "InGen"
-rowRanges(rse_tx)$Type <- "Transcript"
-
-## Expression Map
-name <- c("gencodeID", "Symbol", "Type", "Class")
-exprsMap <- rbind(as.data.frame(rowRanges(rse_gene))[,name],
-                 as.data.frame(rowRanges(rse_exon))[,name],
-                 as.data.frame(rowRanges(rse_jxn))[,name],
-                 as.data.frame(rowRanges(rse_tx))[,name])
-exprsMap <- DataFrame(exprsMap)
-
-nrow(exprsMap)
-# [1] 810029
 
 #### Load DE results ####
 data_type <- c("gene", "exon", "jxn","tx")
@@ -48,33 +49,94 @@ names(files) <- data_type
 
 
 allOut <- lapply(files, function(x) get(load(x, verbose = TRUE)))
-map_depth(allOut, 4, ~"EntrezID" %in% colnames(.x))
 
-allEntrez <- map_depth(allOut, 4, ~.x$EntrezID)
-allEntrez_ul <- unlist(allEntrez)
-u <- as.character(allEntrez_ul[!is.na(allEntrez_ul)])
-length(u)
+## sep results only
+allOut <- transpose(allOut)
+allOut <- allOut$sep
 
-sigOut <- map_depth(allOut, 4, ~.x[.x$adj.P.Val < 0.05,])
-map_depth(sigOut, 4, nrow)
+map_depth(allOut, 3, nrow)
+map_depth(allOut, 3, colnames)
 
-sigGenes <- map_depth(sigOut, 4, ~.x$EntrezID)
+## Define Universe
+all_gencode <- map_depth(allOut, 3, "common_gene_id")
+map_depth(all_gencode, 3, length)
+head(all_gencode$gene$amyg$MDD)
 
-g_test <- map(sigGenes, ~pluck(.x, "sep","sacc","MDD"))
-g_test2 <- map(g_test, ~as.character(unique(.x[!is.na(.x)])))
-lengths(g_test2[1:2])
-# gene exon 
-# 678  906
-is.character(g_test2[[1]])
-is.character(g_test2)
+length(unlist(all_gencode))
+# [1] 3240116
 
-go = compareCluster(geneClusters = g_test2[[1]], 
+## all ENSEMBL
+all_ensembl <- unique(ss(unlist(all_gencode),"\\."))
+length(all_ensembl)
+# [1] 34635
+
+all_entrez <- bitr(all_ensembl, fromType = "ENSEMBL", toType ="ENTREZID", OrgDb="org.Hs.eg.db")
+# 32.71% of input gene IDs are fail to map...
+nrow(all_entrez)
+# [1] 23468
+u <- all_entrez$ENTREZ
+
+## Extract gene sets
+get_signif <- function(outFeature, colname = "common_feature_id", cutoff = 0.05, return_unique = FALSE){
+  signif <- outFeature[[colname]][outFeature$adj.P.Val < cutoff]
+  if(return_unique) signif <- unique(signif)
+  signif <- signif[!is.na(signif)]
+  return(signif)
+}
+my_flatten <- function (x, use.names = TRUE, classes = "ANY") {
+  #' Source taken from rlist::list.flatten
+  len <- sum(rapply(x, function(x) 1L, classes = classes))
+  y <- vector("list", len)
+  i <- 0L
+  items <- rapply(x, function(x) {
+    i <<- i + 1L
+    y[[i]] <<- x
+    TRUE
+  }, classes = classes)
+  if (use.names && !is.null(nm <- names(items))) 
+    names(y) <- nm
+  y
+}
+
+signif_genes <- map_depth(allOut, 3, ~get_signif(.x, colname = "common_gene_id", return_unique = TRUE))
+map_depth(signif_genes, 3, length)
+
+## transpose and flatten so features are combined
+signif_genes2 <- map(transpose(signif_genes),transpose)
+names(signif_genes2$amyg$MDD)
+
+signif_genes3 <- my_flatten(map_depth(signif_genes2, 2, unlist))
+map_int(signif_genes3, length)
+# amyg.MDD amyg.BPD sacc.MDD sacc.BPD 
+# 1141      198     2715      331 
+
+## Extract unique and convert to entrez
+gene_sets <- map(signif_genes3, function(g){
+  e <- unique(ss(g, "\\."))
+  entrez <- bitr(e, fromType = "ENSEMBL", toType ="ENTREZID", OrgDb="org.Hs.eg.db")
+  return(entrez$ENTREZ)
+})
+
+map_int(gene_sets, length)
+# amyg.MDD amyg.BPD sacc.MDD sacc.BPD 
+# 936      138     1668      264 
+
+map(gene_sets, class)
+
+ck <- compareCluster(geneCluster = gene_sets, 
+                     fun = enrichGO, 
+                     OrgDb = "org.Hs.eg.db", univ = u)
+dotplot(ck)
+
+go = compareCluster(geneClusters = gene_sets, 
                     univ = u,
                     OrgDb = "org.Hs.eg.db", 
-                    ont = "ALL",
-                    readable = TRUE, 
-                    pvalueCutoff = 1,
-                    qvalueCutoff = 1)
+                    # ont = "ALL",
+                    # readable = TRUE, 
+                    # pvalueCutoff = 1,
+                    # qvalueCutoff = 1,
+                    fun = enrichGO)
 
-# Error in (function (cl, name, valueClass)  : 
-#            assignment of an object of class “NULL” is not valid for @‘fun’ in an object of class “compareClusterResult”; is(value, "character") is not TRUE
+dotplot(go)
+
+save(go, file = here("differential_expression", "data", "go.rda"))
