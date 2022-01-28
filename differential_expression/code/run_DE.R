fccd3bcd4ccd2d48734f533eb467779e2f169675
lahuuki <lahuuki@gmail.com>
2022-01-28 16:43:13 -0500

    Update messages in DE function

--- differential_expression/code/run_DE.R
+++ differential_expression/code/run_DE.R
@@ -49,19 +49,19 @@ run_DE <- function(rse, model, run_voom = TRUE, save_eBayes = FALSE, coef){
 get_eBayes <- function(rse, model, run_voom = TRUE){
   
   if(run_voom){
-    message("\\nCalc Norm Factors: ", Sys.time())
+    message("Calc Norm Factors: ", Sys.time())
     dge_out = DGEList(counts = assays(rse)$counts,
                       genes = rowData(rse))
     dge_norm = calcNormFactors(dge_out)
     voom_out = voom(dge_norm, model, plot=FALSE)
     
-    message("\\nLimma: ", Sys.time())
+    message("Limma: ", Sys.time())
     fit = lmFit(voom_out)
   } else {
-    message("\\nUsing tpm")
+    message("Using tpm")
     log_tmp = log2(assays(rse)$tpm + 1)
     
-    message("\\nLimma: ", Sys.time())
+    message("Limma: ", Sys.time())
     fit = lmFit(log_tmp, model)
   }
   
