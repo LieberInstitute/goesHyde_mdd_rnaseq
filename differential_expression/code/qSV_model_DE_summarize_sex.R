bd2527bfd6738e734d549dd9dec9cdfe5648320c
lahuuki <lahuuki@gmail.com>
2022-02-08 12:45:18 -0500

    Use new function to plot M & F vs. ALL t-stats

--- differential_expression/code/qSV_model_DE_summarize_sex.R
+++ differential_expression/code/qSV_model_DE_summarize_sex.R
@@ -12,9 +12,11 @@ library(VennDiagram)
 source(here("differential_expression","code","utils.R"))
 
 #### Load Data ####
+load(here('exprs_cutoff','rse_gene.Rdata'), verbose=TRUE)
 load(here("differential_expression","data","qSVA_MDD_gene_sex_DEresults.rda"), verbose = TRUE)
+outGene_sex = outGene
 
-# ## Annotate Risk allels ##
+## Annotate Risk allels ##
 risk_vcf <- readVcf(here("eqtl", "data", "risk_snps", "LIBD_maf01_gwas_MDD.vcf.gz"))
 mdd_snps <- read.delim(here("eqtl", "data", "risk_snps", "PGC_MDD_genome-wide_significant_Jan2022.txt")) %>%
   filter(!is.na(bp))
@@ -28,18 +30,11 @@ length(risk_genes$gencodeID)
 
 #### Summarize Counts ####
 ## Extract lists
-signifFeat <- map_depth(outGene, 3, get_signif)
-signifGene <- map_depth(outGene, 3, get_signif, colname = "common_gene_id", return_unique = TRUE)
-signifFC <- map_depth(outGene, 3, get_signif, colname = "logFC")
-signifSymb <- map_depth(outGene, 3, get_signif, colname = "common_gene_symbol")
+signifGene <- map_depth(outGene_sex, 3, get_signif, colname = "common_gene_id", return_unique = TRUE)
+signifFC <- map_depth(outGene_sex, 3, get_signif, colname = "logFC")
+signifSymb <- map_depth(outGene_sex, 3, get_signif, colname = "common_gene_symbol")
 
 ## Summarize counts
-# signifFeat_n <- map_depth(signifFeat, 3, length) %>%
-#   as.data.frame() %>%
-#   t() %>%
-#   as.data.frame() %>%
-#   rename(n_features = V1)
-
 signifGene_n <- map_depth(signifGene, 3, length) %>%
   as.data.frame() %>%
   t() %>%
@@ -78,37 +73,28 @@ signifRiskSymbol <- map_depth(signifSymb, 3, ~paste(unique(.x[.x %in% risk_genes
 
 model_counts <- do.call("cbind",list(signifGene_n, signifGene_n_up, signifGene_n_down,signifRiskGene_n, signifRiskSymbol, signifSymbol)) %>%
   rownames_to_column("data") %>%
-  separate(data, into = c("Region", "Dx", "Sex"), sep = "\\.")
+  separate(data, into = c("BrainRegion", "PrimaryDx", "Sex"), sep = "\\.")
 
 model_counts[,1:7]
 
 write_csv(model_counts, here("differential_expression","data","model_counts_gene_sex.csv"))
 
+## Summary table for n signif genes
+gene_counts <- model_counts %>%
+  dplyr::select(BrainRegion, PrimaryDx, Sex, n_genes) %>%
+  pivot_wider(values_from = "n_genes", names_from = "Sex") %>%
+  rename(`Control vs.` = PrimaryDx)
+
+write_csv(gene_counts, here("differential_expression","data","model_counts_gene_sex_compare.csv"))
+
 ##### Overlap Between Models ####
-my_flatten <- function (x, use.names = TRUE, classes = "ANY") 
-{
-  #' Source taken from rlist::list.flatten
-  len <- sum(rapply(x, function(x) 1L, classes = classes))
-  y <- vector("list", len)
-  i <- 0L
-  items <- rapply(x, function(x) {
-    i <<- i + 1L
-    y[[i]] <<- x
-    TRUE
-  }, classes = classes)
-  if (use.names && !is.null(nm <- names(items))) 
-    names(y) <- nm
-  y
-}
-
 signifGene_flat <- map(signifGene,my_flatten)
-
 
 #### Four Way Venns ####
 ## 1 Venn for each Feature + Model
 
 walk2(signifGene_flat, names(signifGene_flat), function(r_data, r_name){
-    filename = paste0("venn_sex_", r_name,'.jpg')
+    filename = paste0("venn_sex_", r_name,'.tiff')
     message(filename)
     
     names(r_data) <- gsub("\\.", " ", names(r_data))
@@ -120,11 +106,11 @@ walk2(signifGene_flat, names(signifGene_flat), function(r_data, r_name){
   })
 
 #### Male vs. Female t-stats ####
-outGene_sex_compare <- map_depth(outGene, 2, do.call, what = "cbind")
+outGene_sex_compare <- map_depth(outGene_sex, 2, do.call, what = "cbind")
 head(outGene_sex_compare$sacc$MDD)
 
-sig_colors <- c(RColorBrewer::brewer.pal(3, "Set1"),"dark grey")
-names(sig_colors) <- c("sig_Both", "sig_M", "sig_F", "None")
+sig_colors <- c(RColorBrewer::brewer.pal(4, "Set1"),"dark grey")
+names(sig_colors) <- c("sig_Both", "sig_M", "sig_F", "sig_ALL", "None")
 
 model_compare_plot <- function(model_df, title, save_png = FALSE, prefix = ""){
 
@@ -155,65 +141,50 @@ model_compare_plot <- function(model_df, title, save_png = FALSE, prefix = ""){
 }
 
 pdf(here("differential_expression","plots","sex_tstat_scatter.pdf"))
-  walk2(outGene_sex_compare, c("Amygdala", "sACC"), function(region_df, region_name){
-    walk2(region_df, names(region_df), ~model_compare_plot(.x, 
-                                                           title = paste(region_name,.y,"vs. Control"),
-                                                           save_png = TRUE,
-                                                           prefix = paste("t-stat_MvF", region_name, .y, sep = "_")))
-  })
+walk2(outGene_sex_compare, c("Amygdala", "sACC"), function(region_df, region_name){
+  walk2(region_df, names(region_df), 
+        ~model_compare_plot(.x, 
+                            title = paste(region_name,.y,"vs. Control"),
+                            save_png = TRUE,
+                            prefix = paste("t-stat_MvF", region_name, .y, sep = "_")))
+})
 dev.off()
 
-#### Compare Male-only with All data
-outGene_sex <- outGene
-names(outGene_sex$sacc$MDD)
-
-outGene_M <- transpose(map(outGene_sex, transpose))[["M"]]
-names(outGene_M)
-
+#### Compare Male-only and Female - only with All data
 ## load all data outGene
 load(here("differential_expression","data","qSVA_MDD_gene_DEresults.rda"), verbose = TRUE)
 names(outGene$sep)
 
-all(rownames(outGene_M$amyg$MDD) == rownames(outGene$sep$amyg$MDD))
+all(rownames(outGene_sex$amyg$MDD$M) == rownames(outGene$sep$amyg$MDD))
 
-compare_sex_all <- map2(outGene_M, outGene$sep, function(region_M, region_all){
-  map2(region_M, region_all, function(dx_M, dx_all){
-    colnames(dx_M) <- paste0("M.", colnames(dx_M))
-    return(cbind(dx_all, dx_M))
-  })
-})
-head(compare_sex_all$sacc$MDD)
+source(here("differential_expression","code","utils.R"))
+test <- compare_tstat_plot(DE_out_x = outGene$sep$amyg$MDD,
+                           DE_out_y = outGene_sex$amyg$MDD$M, 
+                           name_x = "M", 
+                           name_y = "All")
 
-sig_colors <- c(RColorBrewer::brewer.pal(3, "Set1"),"dark grey")
-names(sig_colors) <- c("sig_Both", "sig_M", "sig_all", "None")
+ggsave(test, filename = here("differential_expression","plots","test.png"))
 
-M_compare_plot <- function(model_df, title, save_png = FALSE, prefix = ""){
-  
-  t_cor <- model_df %>%
-    summarize(cor = cor(t, M.t, method = "spearman")) %>%
-    mutate(cor_anno = paste0("rho = ", format(round(cor, 2), nsmall = 2)))
-  
-  model_df <- model_df %>% 
-    mutate(Signif = case_when(M.adj.P.Val < 0.05 & adj.P.Val < 0.05 ~"sig_Both",
-                              M.adj.P.Val < 0.05 ~ "sig_M",
-                              adj.P.Val < 0.05 ~ "sig_all",
-                              TRUE ~ "None"))
-  
-  model_plot <- ggplot(model_df, aes(x = t, y = M.t, color = Signif)) +
-    geom_point(alpha = 0.7, size = 0.5) +
-    labs(x = "t-stats All", y = "t-stats Male-only", 
-         title = title, subtitle = t_cor$cor_anno) +
-    scale_color_manual(values = sig_colors)+
-    theme_bw()
-  
-  if(save_png){
-    fn = here("differential_expression","plots",paste0(prefix,".png"))
-    ggsave(model_plot, filename = fn)              
-  }
-  
-  message(title)  
-  print(model_plot)
-}
+pdf(here("differential_expression","plots","sex_vs_All_tstat_scatter.pdf"))
+
+pwalk(list(all_region = outGene$sep, sex_region = outGene_sex, name_region = c("Amygdala", "sACC")), 
+      function(all_region, sex_region, name_region){
+        pwalk(list(all_dx = all_region, sex_dx = sex_region, name_dx = names(all_region)), 
+              function(all_dx, sex_dx, name_dx){
+                title1 = paste(name_region, "-", name_dx, "vs. Control,")
+                
+                walk2(sex_dx, names(sex_dx),
+                      ~print(compare_tstat_plot(DE_out_x = all_dx,
+                                          DE_out_y = .x, 
+                                          name_x = "All", 
+                                          name_y = .y,
+                                          title = paste(title1, .y))))
+              })
+      })
+
+dev.off()
+
+
 
 pdf(here("differential_expression","plots","sexM_vs_All_tstat_scatter.pdf"))
 walk2(compare_sex_all, c("Amygdala", "sACC"), function(region_df, region_name){
