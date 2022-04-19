25fa9e181032369d7a693b4bfa2325e135b12a71
lahuuki <lahuuki@gmail.com>
2022-04-19 17:00:57 -0400

    Up memory

--- eqtl/code/tensorqtl_genomewide_nominal_gpu.sh
+++ eqtl/code/tensorqtl_genomewide_nominal_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l caracol,mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -l caracol,mem_free=100G,h_vmem=100G,h_fsize=100G
 #$ -pe local 1
 #$ -N tensorqtl_genomewide_nominal_gpu
 #$ -o logs/tensorqtl_genomewide_nominal_gpu.$TASK_ID.txt
