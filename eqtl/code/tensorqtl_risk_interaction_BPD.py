cf7e13238d736ba713eb95914e8bba6c62ce3b13
lahuuki <lahuuki@gmail.com>
2022-04-26 11:07:01 -0400

    Upgrade BPD risk interation to match MDD

--- eqtl/code/tensorqtl_risk_interaction_BPD.py
+++ eqtl/code/tensorqtl_risk_interaction_BPD.py
@@ -4,13 +4,14 @@ import os
 from tensorqtl import genotypeio, cis, trans
 import pandas as pd
 
-out_path = "../data/tensorQTL_out/nominal_bpd_risk/"
+out_path = "../data/tensorQTL_out/risk_nominal_bpd/"
 
 for region in ["Amygdala", "sACC"]:
-    plink, express, covar, prefix = my_tensorqtl_run.get_input_paths("gene", region)
-    genotype_df, variant_df, phenotype_df_filter, phenotype_pos_df_filter, covariates_df = my_tensorqtl_run.load_data(plink, express, covar)
+    express, covar = my_tensorqtl_run.get_input_paths("gene", region)
+    plink = "../data/risk_snps/LIBD_maf01_gwas_BPD"
+    genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df = my_tensorqtl_run.load_data(plink, express, covar, add_chr = True)
 
-    cell_fraction = pd.read_csv("../data/interaction/cell_fraction_"+ region +".csv", index_col = 0)
+    cell_fraction = pd.read_csv("../data/tensorQTL_input/interaction/cell_fraction_"+ region +".csv", index_col = 0)
     
     for cell_type in cell_fraction.columns:
         tag = 'gene_' + region +'_' + cell_type 
@@ -18,12 +19,10 @@ for region in ["Amygdala", "sACC"]:
         
         cf_interaction = pd.Series(cell_fraction[cell_type])
         
-        nominal_out = cis.map_nominal(genotype_df, variant_df, phenotype_df_filter, phenotype_pos_df_filter,
-        prefix = tag, covariates_df=covariates_df, output_dir= out_path + "parquet_data/", 
-        interaction_s = cf_interaction, maf_threshold_interaction=0, group_s=None, window=500000, 
-        run_eigenmt=True, write_top = False)
+        nominal_out = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df,
+        prefix = tag, covariates_df=covariates_df, output_dir = out_path, 
+        interaction_s = cf_interaction, maf_threshold_interaction = 0.05, group_s = None, window = 500000, 
+        run_eigenmt = False, write_stats = True)
         
-        nominal_out.to_csv('../data/tensorQTL_out/nominal_bpd_risk/' + tag + ".csv")
-    
     print("DONE " + region)
 
