1aeda683d7bea2116f74a248ca1869a8aefe84ca
lahuuki <lahuuki@gmail.com>
2022-05-25 12:53:05 -0400

    Export qqnorms as *.gz files #19

--- leafcutter/code/extract_qqnorm_data.R
+++ leafcutter/code/extract_qqnorm_data.R
@@ -18,11 +18,21 @@ region_idx <- splitit(rse_gene$BrainRegion)
 names(region_idx)
 
 ## separate regions and save
-amy <- all[,br_amy]
-write.table(amy, file="/users/schadinh/lieber/qqnorm/amygdala_qqnorm_20190329.txt", quote=F, sep="\t", col.names=T, row.names=F)
+amy <- all[,region_idx$Amygdala]
+dim(amy)
+# [1]  10 540
+amy_fn <- here("leafcutter", "data", "qqnorm", "qqnorm_Amygdala.txt")
+write.table(amy, file= amy_fn,
+            quote=F, sep="\t", col.names=T, row.names=F)
+system(paste("gzip", amy_fn))
 
-sacc <- all[,br_sacc]
-write.table(sacc, file="/users/schadinh/lieber/qqnorm/sacc_qqnorm_20190329.txt", quote=F, sep="\t", col.names=T, row.names=F)
+sacc <- all[,region_idx$sACC]
+dim(sacc)
+# [1]  10 551
+sacc_fn <- here("leafcutter", "data", "qqnorm", "qqnorm_sACC.txt")
+write.table(sacc, file = sacc_fn,
+            quote=F, sep="\t", col.names=T, row.names=F)
+system(paste("gzip", sacc_fn))
 
 # sgejobs::job_single('extract_qqnorm_data', create_shell = TRUE, queue= 'bluejay', memory = '5G', command = "Rscript extract_qqnorm_data.R")
 
