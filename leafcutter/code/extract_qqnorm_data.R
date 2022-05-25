a67e7f527e24b512c93acc577f4dc39330194592
lahuuki <lahuuki@gmail.com>
2022-05-25 12:13:51 -0400

    Create sep files for amyg & sacc, use genoSamples as colnames to match VCF #19

--- leafcutter/code/extract_qqnorm_data.R
+++ leafcutter/code/extract_qqnorm_data.R
@@ -1,22 +1,27 @@
 library("SummarizedExperiment")
 library("here")
+library("jaffelab")
 
 load(here('exprs_cutoff','rse_gene.Rdata'), verbose=TRUE)
 
-all <- read.table("/users/schadinh/lieber/jxn_counts/BP_RNAseq_perind.counts.chrom.gz.qqnorm_all",header=T,comment.char="?")
-cols <- colnames(all)
-cols <- gsub("_junctions_primaryOnly_regtools","",cols)
-br_all <- as.character(lapply(cols, function(x) ifelse(x %in% pd$SAMPLE_ID, pd[pd$SAMPLE_ID == x,"SampleID"],x)))
-colnames(all) <- br_all
+all <- read.table(here("leafcutter", "data", "clusters", "leafcutter_perind.counts.gz.PCs"),
+                  header=T,comment.char="?")
 
-br_amy <- c(1, 2, 3, 4, grep("Amygdala",br_all))
+colnames(all) <- ss(colnames(all), "_")
+
+## Use genoSample as col names to match VCF
+all <- all[,rse_gene$RNum]
+all(rse_gene$RNum == colnames(all))
+colnames(all) <- rse_gene$genoSample
+
+region_idx <- splitit(rse_gene$BrainRegion)
+names(region_idx)
+
+## separate regions and save
 amy <- all[,br_amy]
-colnames(amy) <- gsub("_Amygdala","",colnames(amy))
 write.table(amy, file="/users/schadinh/lieber/qqnorm/amygdala_qqnorm_20190329.txt", quote=F, sep="\t", col.names=T, row.names=F)
 
-br_sacc <- c(1, 2, 3, 4, grep("sACC",br_all))
 sacc <- all[,br_sacc]
-colnames(sacc) <- gsub("_sACC","",colnames(sacc))
 write.table(sacc, file="/users/schadinh/lieber/qqnorm/sacc_qqnorm_20190329.txt", quote=F, sep="\t", col.names=T, row.names=F)
 
 # sgejobs::job_single('extract_qqnorm_data', create_shell = TRUE, queue= 'bluejay', memory = '5G', command = "Rscript extract_qqnorm_data.R")
