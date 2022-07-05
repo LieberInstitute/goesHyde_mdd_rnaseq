00ab752ed0ae4ddfc5fad9f3e4a5b1ba0ff70804
lahuuki <lahuuki@gmail.com>
2022-07-05 11:03:43 -0400

    Change input list for subset, ended up not using risk VCF anyway

--- eqtl/code/subset_risk_snps_MDD.sh
+++ eqtl/code/subset_risk_snps_MDD.sh
@@ -7,10 +7,11 @@
 
 
 module load bcftools
-
+module load plink
 
 
 ## Locate file and ids
 
-bcftools view --include 'ID=@../data/risk_snps/MDD_risk_snps.txt' /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/mdd_bpd/maf01/mdd_bpd_01.vcf.gz > ../data/risk_snps/LIBD_maf01_gwas_MDD.vcf.gz
+bcftools view --include 'ID=@../data/risk_snps/Variants_within_MDD_loci_all_052722.txt' /dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/genotype_data/mdd_bpd/maf01/mdd_bpd_01.vcf.gz > ../data/risk_snps/LIBD_maf01_gwas_MDD.vcf.gz
+plink2 --vcf ../data/risk_snps/LIBD_maf01_gwas_MDD.vcf.gz --make-bed --out ../data/risk_snps/LIBD_maf01_gwas_MDD
 
