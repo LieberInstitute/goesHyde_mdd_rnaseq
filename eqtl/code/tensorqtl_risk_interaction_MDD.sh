c8d303024bc833f6f347c044c419e1cfeff2a56a
lahuuki <lahuuki@gmail.com>
2023-06-23 14:36:36 -0400

    up mem

--- eqtl/code/tensorqtl_risk_interaction_MDD.sh
+++ eqtl/code/tensorqtl_risk_interaction_MDD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l caracol,mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -l caracol,mem_free=100G,h_vmem=100G,h_fsize=100G
 #$ -N tensorqtl_risk_interaction
 #$ -o logs/tensorqtl_risk_interaction_MDD.txt
 #$ -e logs/tensorqtl_risk_interaction_MDD.txt
