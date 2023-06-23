1f010c6a3498bf8a4405579432b85dfc4bc87ecc
lahuuki <lahuuki@gmail.com>
2023-06-23 14:24:06 -0400

    Change FDR values, plot 'April plots'

--- eqtl/code/subset_signif_snps.R
+++ eqtl/code/subset_signif_snps.R
@@ -1,12 +1,13 @@
 library("tidyverse")
 library("here")
-library("readxl")
+library("xlsx")
 
 ## list all the SNPs of interest + signif to subset the VCF via plink
 
 ## SNPs of Intrest
-twas_table <- read_excel(here("eqtl","data","plot_data","TWAS_sig_PP4_coloc08.xlsx")) %>%
-  rename(variant_id_gwas = "chr_bp_ref_alt...6", variant_id_eqtl = "chr_bp_ref_alt...9")
+twas_table <- read.xlsx(here("eqtl","data","plot_data","TWAS_sig_PP4_coloc08.xlsx")) %>%
+  rename(variant_id_gwas = "chr_bp_ref_alt...6", variant_id_eqtl = "chr_bp_ref_alt...9") %>%
+  select(-`...7`)
 
 snps_twas <- unique(c(twas_table$variant_id_gwas, twas_table$variant_id_eqtl))
 length(snps_twas)
@@ -14,10 +15,10 @@ length(snps_twas)
 
 
 #### Load the FDR01 output ####
-fn_FDR01 <- list.files(here("eqtl", "data", "tensorQTL_FDR01"), recursive = TRUE, pattern = "*.csv", full.names = TRUE)
-names(fn_FDR01) <- gsub("_FDR01.csv","", basename(fn_FDR01))
+fn_FDR <- list.files(here("eqtl", "data", "tensorQTL_FDR05"), recursive = TRUE, pattern = "*.csv", full.names = TRUE)
+names(fn_FDR) <- gsub("_FDR01.csv","", basename(fn_FDR))
 
-eqtl_FDR01 <- map(fn_FDR01, read.csv)
+eqtl_FDR01 <- map(fn_FDR, read.csv)
 map_int(eqtl_FDR01, nrow)
 # cis_gene_amyg         cis_gene_sacc independent_gene_amyg independent_gene_sacc     nominal_exon_amyg 
 # 1326                  1367                  1805                  1880               5599118 
@@ -44,4 +45,11 @@ length(all_snps)
 
 ## write to txt file
 cat(all_snps, sep = "\n", file = here("eqtl", "data", "signif_snps",'significant_snps.txt'))
+## run subset_signif_snps.sh to subset vcf
+
+#### April plots ####
+april_plots <- read.xlsx(here("eqtl","data","plot_data","Genes_for_boxplot_042622.xlsx"), sheetIndex = "eqtl", header=TRUE)
+
+## write to txt file
+cat(april_plots$variant_id, sep = "\n", file = here("eqtl", "data", "plot_data",'april_snps.txt'))
 ## run subset_signif_snps.sh to subset vcf
