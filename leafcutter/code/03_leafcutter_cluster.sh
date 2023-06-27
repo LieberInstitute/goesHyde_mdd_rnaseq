9c421ddf0684455f1bc7eab92fe124870c294d91
lahuuki <lahuuki@gmail.com>
2023-06-27 13:00:37 -0400

    Run processed not all jxn

--- leafcutter/code/03_leafcutter_cluster.sh
+++ leafcutter/code/03_leafcutter_cluster.sh
@@ -2,9 +2,12 @@
 #$ -cwd
 #$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
 #$ -N leafcutter_cluster
-#$ -o logs/leafcutter_cluster.txt
-#$ -e logs/leafcutter_cluster.txt
-#$ -hold_jid reformat_junc
+#$ -o logs/leafcutter_cluster_processed.txt
+#$ -e logs/leafcutter_cluster_process.txt
+#$ -hold_jid cluster
+#$ -m be
+#$ -M fgoes1@jhmi.edu
+
 
 echo "**** Job starts ****"
 date
@@ -18,7 +21,8 @@ echo "Task id: ${SGE_TASK_ID}"
 
 module load python/2.7
 
-python leafcutter_cluster.py -j ../data/all_jxn_filenames.txt -m 50 -l 500000 -r ../data/clusters/
+#python leafcutter_cluster.py -j ../data/all_jxn_filenames.txt -m 50 -l 500000 -r ../data/clusters/
+python leafcutter_cluster.py -j ../data/processed_junc_count_files.txt -m 50 -l 500000 -r ../data/clusters/
 
 echo "**** Job ends ****"
 date
