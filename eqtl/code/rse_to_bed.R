d9fe5bc454535e5e2aaba63926ee244e62540c4a
lahuuki <lahuuki@gmail.com>
2023-07-27 11:29:50 -0400

    save rse_to_bed as own function, add assay_name = 'logcounts' argument

---
+++ eqtl/code/rse_to_bed.R
@@ -0,0 +1,28 @@
+#' Convert assay from a SummarizedExperiment to tensorQTL compatible bed file
+#'
+#' @param rse 
+#' @param assay_name
+#' @return A `data.frame()` that can be saved with `write.table(sep = "\t",quote = FALSE, row.names = FALSE)` to import to tensorQTL
+#' @export
+#'
+#' @examples bed <- rse_to_bed(rse_gene)
+#' @importFrom tibble rownames_to_column
+#' @importFrom dplyr arrange mutate select left_join
+#' @importFrom SummarizedExperiment assays
+rse_to_bed <- function(rse, assay_name = "logcounts") {
+  rr_df <- as.data.frame(rowRanges(rse)) %>%
+    tibble::rownames_to_column("ID") %>%
+    dplyr::arrange(seqnames, start) %>%
+    dplyr::mutate(end = start + 1) %>%
+    dplyr::select(`#Chr` = seqnames, start, end, ID)
+  
+  
+  counts <- SummarizedExperiment::assays(rse)[[assay_name]]
+  colnames(counts) <- rse$genoSample
+  
+  counts <- as.data.frame(counts) %>%
+    tibble::rownames_to_column("ID") %>%
+    dplyr::left_join(rr_df, ., by = "ID")
+  
+  return(counts)
+}
