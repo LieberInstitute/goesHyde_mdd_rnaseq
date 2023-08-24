537f4d866835314b66b2a899aa68c3682dc37c0e
lahuuki <lahuuki@gmail.com>
2023-08-24 17:31:27 -0400

    Use start for TSS for +, and end for TSS for - strand

--- eqtl/code/rse_to_bed.R
+++ eqtl/code/rse_to_bed.R
@@ -10,18 +10,19 @@
 #' @importFrom dplyr arrange mutate select left_join
 #' @importFrom SummarizedExperiment assays
 rse_to_bed <- function(rse, assay_name = "logcounts") {
-  rr_df <- as.data.frame(rowRanges(rse)) %>%
-    tibble::rownames_to_column("ID") %>%
-    dplyr::arrange(seqnames, start) %>%
-    dplyr::mutate(end = start + 1) %>%
+  rr_df <- as.data.frame(rowRanges(rse_gene)) |>
+    tibble::rownames_to_column("ID") |>
+    dplyr::arrange(seqnames, start) |>
+    dplyr::mutate(start = ifelse(strand == "+", start, end),
+                  end = start + 1) |>
     dplyr::select(`#Chr` = seqnames, start, end, ID)
   
   message(assay_name, " to bed format")
   counts <- SummarizedExperiment::assays(rse)[[assay_name]]
   colnames(counts) <- rse$genoSample
   
-  counts <- as.data.frame(counts) %>%
-    tibble::rownames_to_column("ID") %>%
+  counts <- as.data.frame(counts) |>
+    tibble::rownames_to_column("ID") |>
     dplyr::left_join(rr_df, ., by = "ID")
   
   return(counts)
