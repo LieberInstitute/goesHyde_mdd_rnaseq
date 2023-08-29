fa9e28c03b391e8e18eef92e3722f3cef788207e
lahuuki <lahuuki@gmail.com>
2023-08-29 09:49:08 -0400

    Save ouput as qs files for fast loading

--- eqtl/code/04_filter_genomewide_nominal.R
+++ eqtl/code/04_filter_genomewide_nominal.R
@@ -1,18 +1,17 @@
-library(tidyverse)
-library(jaffelab)
-library(miniparquet)
-library(sessioninfo)
-library(here)
+library("tidyverse")
+library("jaffelab")
+library("miniparquet")
+library("sessioninfo")
+library("here")
+library("qs")
 
+## set up lists
 features <- c("gene", "exon", "jxn", "tx")
 names(features) <- features
 
 regions <- c(amyg = "Amygdala", sacc = "sACC")
 
-## just for test
-features <- features["gene"]
-regions <- regions["amyg"]
-  
+## loop throught datasets
 parquet_files <- map(
     features,
     function(f) {
@@ -32,15 +31,13 @@ read_adj_filter <- function(parquet_files, cutoff = 0.05) {
     eqtl_out <- do.call("rbind", map(parquet_files, parquet_read)) %>%
         mutate(FDR = p.adjust(pval_nominal, "fdr")) 
     message("n pairs: ", nrow(eqtl_out))
-    ## filter 
+    # filter
     eqtl_out <- eqtl_out %>%
         filter(FDR < cutoff)
     message("n pairs FDR<", cutoff, ": ", nrow(eqtl_out))
-    # significant_snps <- c(significant_snps, eqt_outl$variant_id)
+    significant_snps <- c(significant_snps, eqt_outl$variant_id)
     return(eqtl_out)
 }
-
-# parquet_files <- parquet_files['gene']
 
 # significant_snps <- c()
 eqtl_out_filtered <- map2(parquet_files, names(parquet_files), function(parq_feat, names_feat) {
@@ -48,11 +45,14 @@ eqtl_out_filtered <- map2(parquet_files, names(parquet_files), function(parq_fea
         message(paste("Reading:", names_feat, names_region, "- "), Sys.time())
 
         eqtl_out_filtered <- read_adj_filter(parquet_files = parq_region)
+        
+        fn <- here("eqtl", "data", "tensorQTL_FDR05", "genomewide_nominal",
+          paste0(names_feat, "_", names_region, "_FDR05"))
 
-        write_csv(eqtl_out_filtered, file = here(
-            "eqtl", "data", "tensorQTL_FDR05", "genomewide_nominal",
-            paste0(names_feat, "_", names_region, "_FDR05.csv")
-        ))
+        write_csv(eqtl_out_filtered, file = paste0(fn, ".csv"))
+        
+        ## also save with qs to make loading faster later
+        qsave(eqtl_out_filtered, file = paste0(fn, ".qs"))
     })
 })
 # significant_snps <- unique(significant_snps)
