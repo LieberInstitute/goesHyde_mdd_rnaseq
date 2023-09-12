ebd5049a91aed4d3e036970a54d534944175798f
lahuuki <lahuuki@gmail.com>
2023-09-12 14:50:58 -0400

    rm rse_to_bed, its in own script

--- eqtl/code/utils.R
+++ eqtl/code/utils.R
@@ -26,21 +26,3 @@ read_adj_filter <- function(parquet_files, cutoff = 0.05, pval_name = "pval_nomi
 }
 
 
-rse_to_bed <- function(rse) {
-    rr_df <- as.data.frame(rowRanges(rse)) %>%
-        tibble::rownames_to_column("ID") %>%
-        dplyr::arrange(seqnames, start) %>%
-        dplyr::mutate(end = start + 1) %>%
-        dplyr::select(`#Chr` = seqnames, start, end, ID)
-    
-    
-    counts <- SummarizedExperiment::assays(rse)$counts
-    colnames(counts) <- rse$genoSample
-    # counts = round(counts, 3) ## round to 3 ?
-    
-    counts <- as.data.frame(counts) %>%
-        tibble::rownames_to_column("ID") %>%
-        dplyr::left_join(rr_df, ., by = "ID")
-    
-    return(counts)
-}
