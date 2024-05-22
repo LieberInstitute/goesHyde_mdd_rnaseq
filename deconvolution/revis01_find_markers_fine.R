b3c83f5289b7afec4f879bf5e845ceacc607a0a9
lahuuki <lahuuki@gmail.com>
2024-05-22 16:02:13 -0400

    Start marker finding for region specific data

--- deconvolution/revis01_find_markers_fine.R
+++ deconvolution/revis01_find_markers_fine.R
@@ -1,9 +1,32 @@
 
 library("DeconvoBuddies")
+library("SingleCellExperiment")
+library("purrr")
+library("here")
+library("sessioninfo")
 
 ## sce Data
 load("/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/deconvolution_bsp2/data/sce_pan.v2.Rdata", verbose = TRUE)
 
-sce_amyg
+sce_region <- map(c(sacc = "sacc", amy = "amy"), function(r){
+  sce <- sce_pan[,sce_pan$region == r]
+  sce$cellType <- droplevels(sce$cellType)
+  return(sce)
+})
+rm(sce_pan)
 
-sce_sacc
+map_int(sce_region, ncol)
+# sacc   amy 
+# 15323 14006 
+map(sce_region, ~table(.x$cellType))
+map_int(sce_region, ~length(levels((.x$cellType))))
+# sacc  amy 
+# 24   19
+marker_stats <- map(sce_region, ~get_mean_ratio(.x, 
+                               cellType_col = "cellType",
+                               gene_ensembl = "gene_id",
+                               gene_name = "gene_name")
+)
+                  
+                    
+                    
